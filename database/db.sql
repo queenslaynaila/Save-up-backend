be32ab8649f2040363fc71fe1928a1eb77c5c605
queenslaynaila <queenslayjema@gmail.com>
2024-02-09 10:04:03 +0300

    Feat:databse

---
+++ database/db.sql
@@ -0,0 +1,50 @@
+BEGIN TRANSACTION;
+
+CREATE TABLE IF NOT EXISTS users (
+      id SERIAL PRIMARY KEY,
+      username VARCHAR(255) NOT NULL,
+      email VARCHAR(255) NOT NULL UNIQUE,
+      phone_no VARCHAR(255) NOT NULL UNIQUE,
+      password_hash VARCHAR(255) NOT NULL,
+      created_at TIMESTAMP DEFAULT NOW(),
+      updated_at TIMESTAMP DEFAULT NOW()
+);
+
+CREATE TABLE IF NOT EXISTS savings (
+      id SERIAL PRIMARY KEY,
+      user_id INT REFERENCES users(id) ON DELETE CASCADE,
+      description VARCHAR(255) NOT NULL,
+      category VARCHAR(255),
+      targetAmount DECIMAL(10, 2) NOT NULL,
+      contributedAmount DECIMAL(10, 2) DEFAULT 0.00,
+      priority VARCHAR(255),
+      status VARCHAR(255) DEFAULT 'In Progress',
+      targetDate DATE,
+      startDate DATE DEFAULT NOW(),
+      created_at TIMESTAMP DEFAULT NOW(),
+      updated_at TIMESTAMP DEFAULT NOW()
+);
+
+CREATE TABLE IF NOT EXISTS contributions (
+      id SERIAL PRIMARY KEY,
+      saving_id INT REFERENCES savings(id) ON DELETE CASCADE,
+      amount DECIMAL(10, 2) NOT NULL,
+      date DATE NOT NULL,
+      created_at TIMESTAMP DEFAULT NOW(),
+      updated_at TIMESTAMP DEFAULT NOW()
+);
+
+CREATE TABLE IF NOT EXISTS expenses (
+      id SERIAL PRIMARY KEY,
+      user_id INT REFERENCES users(id) ON DELETE CASCADE,
+      category VARCHAR(255) NOT NULL,
+      amount DECIMAL(10, 2) NOT NULL,
+      date DATE NOT NULL,
+       created_at TIMESTAMP DEFAULT NOW(),
+      updated_at TIMESTAMP DEFAULT NOW()
+);
+
+
+
+
+COMMIT
