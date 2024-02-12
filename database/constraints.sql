d6359e40f5523945dd010bea787724ba2dd17a7a
queenslaynaila <queenslayjema@gmail.com>
2024-02-12 12:47:18 +0300

    Fix query error

--- database/constraints.sql
+++ database/constraints.sql
@@ -7,7 +7,6 @@ ALTER TABLE users
 ADD CONSTRAINT email_format_check
 CHECK (email ~* '^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$')
 
-ALTER TABLE users
 ADD CONSTRAINT phone_no_format_check
 CHECK (phone_no ~* '^[0-9]{10}$')
 
