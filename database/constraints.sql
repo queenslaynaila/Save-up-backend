c6ab61e3dbdfc7a8b5e88789968268c01a21b377
queenslaynaila <queenslayjema@gmail.com>
2024-02-12 12:50:12 +0300

    Feat(savings):restrict status to either in progress or complete

--- database/constraints.sql
+++ database/constraints.sql
@@ -10,5 +10,10 @@ CHECK (email ~* '^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$')
 ADD CONSTRAINT phone_no_format_check
 CHECK (phone_no ~* '^[0-9]{10}$')
 
+ALTER TABLE savings
+ADD CONSTRAINT status_check
+CHECK (status IN ('In Progress', 'Complete'));
+
+
 
 COMMIT
