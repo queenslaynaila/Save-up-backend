b0e44a09e3d0dc7b7077936c8b52c11fcca58eed
queenslaynaila <queenslayjema@gmail.com>
2024-02-15 10:32:23 +0300

    Index databse on query params

---
+++ database/index.sql
@@ -0,0 +1,4 @@
+CREATE INDEX idx_savings_user_id ON savings (user_id);
+CREATE INDEX idx_savings_category ON savings (category);
+CREATE INDEX idx_savings_priority ON savings (priority);
+CREATE INDEX idx_savings_status ON savings (status);
