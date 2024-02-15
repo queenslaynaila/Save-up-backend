c7158f25140b32544b5718528b07a98c405a7583
queenslaynaila <queenslayjema@gmail.com>
2024-02-15 11:00:23 +0300

    Run prettier

--- routes/expenses.ts
+++ routes/expenses.ts
@@ -10,22 +10,22 @@ router.get('/', (req: Request, res: Response) => {
   expenseController.getAllExpenses(req, res);
 });
 
-router.get('/:id',authenticateToken, (req: Request, res: Response) => {
+router.get('/:id', authenticateToken, (req: Request, res: Response) => {
   expenseController.getExpenseById(req, res);
 });
 
-router.patch('/:id',authenticateToken, (req: Request, res: Response) => {
+router.patch('/:id', authenticateToken, (req: Request, res: Response) => {
   expenseController.updateExpense(req, res);
 });
 
-router.delete('/:id',authenticateToken, (req: Request, res: Response) => {
+router.delete('/:id', authenticateToken, (req: Request, res: Response) => {
   expenseController.deleteExpense(req, res);
 });
 
-router.get('/category/:category',authenticateToken, (req: Request, res: Response) => {
+router.get('/category/:category', authenticateToken, (req: Request, res: Response) => {
   expenseController.getExpenseByCategory(req, res);
 });
-router.get('/month/:month',authenticateToken, (req: Request, res: Response) => {
+router.get('/month/:month', authenticateToken, (req: Request, res: Response) => {
   expenseController.getExpensesByMonth(req, res);
 });
 
