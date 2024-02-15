378a0fd9c2f8fd52802cde2388e9d6ad9de188d1
queenslaynaila <queenslayjema@gmail.com>
2024-02-15 11:29:57 +0300

    Add indexes for frequently queried

--- database/alter.sql
+++ database/alter.sql
@@ -2,3 +2,5 @@ ALTER TABLE users
 ADD COLUMN total_targeted_amount DECIMAL(10, 2) DEFAULT 0,
 ADD COLUMN total_contributions_amount DECIMAL(10, 2) DEFAULT 0,
 ADD COLUMN total_expenses_amount DECIMAL(10, 2) DEFAULT 0;
+ALTER TABLE expenses
+ADD COLUMN month_extracted INTEGER GENERATED ALWAYS AS (EXTRACT(MONTH FROM date)) STORED;
