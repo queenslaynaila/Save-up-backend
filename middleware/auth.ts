c8b2b7a6cd6a87212f7d33aec1cdbe7878885b89
queenslaynaila <queenslayjema@gmail.com>
2024-02-15 10:59:24 +0300

    RUn prettier

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,34 +1,34 @@
-import  { NextFunction, Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 import jwt, { Secret } from 'jsonwebtoken';
 import { User } from '../types';
 
 declare module 'express-serve-static-core' {
-    interface Request {
-      user?: User; 
-    }
+  interface Request {
+    user?: User;
   }
+}
 
 const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
-      const token = req.headers['authorization'];
-  
-      if (!token) {
-          return res.status(401).json({ message: "Token does not exist" });
+  const token = req.headers['authorization'];
+
+  if (!token) {
+    return res.status(401).json({ message: 'Token does not exist.Login first' });
+  }
+
+  const tokenValue = token.split(' ')[1];
+  jwt.verify(tokenValue, process.env.JWT_SECRET as Secret, (err, decoded) => {
+    if (err) {
+      if (err.name === 'TokenExpiredError') {
+        return res.status(401).json({ message: 'Token expired,please log in again' });
+      } else {
+        return res.status(401).json({ message: 'Invalid token,please log in again' });
       }
-  
-      const tokenValue = token.split(' ')[1];
-      jwt.verify(tokenValue, process.env.JWT_SECRET as Secret, (err, decoded) => {
-          if (err) {
-              if (err.name === 'TokenExpiredError') {
-                  return res.status(401).json({ message: 'Token expired,please log in again' });
-              } else {
-                  return res.status(401).json({ message: 'Invalid token,please log in again' });
-              }
-          }
-          
-          req.user = decoded as User;
-          next();
-      });
-  };
-  
-  export default authenticateToken;
-  
+    }
+    
+
+    req.user = decoded as User;
+    next();
+  });
+};
+
+export default authenticateToken;
