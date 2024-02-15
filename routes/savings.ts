837c9813e79307aeebd26e3e757259f4fe904df9
queenslaynaila <queenslayjema@gmail.com>
2024-02-15 10:56:52 +0300

    Update savings qury routes

--- routes/savings.ts
+++ routes/savings.ts
@@ -1,42 +1,26 @@
 import express, { Request, Response } from 'express';
 import * as savingsController from '../controllers/savingsController';
-import authenticateToken from '../middleware/auth';
+ import authenticateToken from '../middleware/auth';
 const router = express.Router();
 
 router.post('/', (req: Request, res: Response) => {
   savingsController.createSaving(req, res);
 });
 
-router.get('/', (req: Request, res: Response) => {
+router.get('/all', (req: Request, res: Response) => {
   savingsController.getAllSavings(req, res);
 });
 
-router.get('/:id', (req: Request, res: Response) => {
-  savingsController.getSavingById(req, res);
-});
-
-router.patch('/:id',authenticateToken, (req: Request, res: Response) => {
+router.patch('/:id',authenticateToken,(req: Request, res: Response) => {
   savingsController.updateSaving(req, res);
 });
 
-router.delete('/:id',authenticateToken, (req: Request, res: Response) => {
+router.delete('/:id', authenticateToken, (req: Request, res: Response) => {
   savingsController.deleteSaving(req, res);
 });
 
-router.get('/category/:category',authenticateToken, (req: Request, res: Response) => {
-  savingsController.getSavingsByCategory(req, res);
-});
-
-router.get('/status/:status',authenticateToken, (req: Request, res: Response) => {
-  savingsController.getSavingsByStatus(req, res);
-});
-
-router.get('/priority/:priority',authenticateToken, (req: Request, res: Response) => {
-  savingsController.getSavingsByPriority(req, res);
-});
-
-router.get('/user/:id',authenticateToken, (req: Request, res: Response) => {
-  savingsController.getUserSavings(req, res);
+router.get('/', async (req: Request, res: Response) => {
+   savingsController.getSavings(req, res);
 });
 
 export default router;
