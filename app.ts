86cd104dc73bbc229881329fdb516abb46dbcb13
queenslaynaila <queenslayjema@gmail.com>
2024-02-21 15:30:56 +0300

    allow exposed headers

--- app.ts
+++ app.ts
@@ -16,7 +16,9 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(morgan('dev'));
-app.use(cors());
+app.use(cors({
+  exposedHeaders: ['Authorization', 'X-Auth-Token']
+}));
 
 // Routes
 usersRoutes(app);
