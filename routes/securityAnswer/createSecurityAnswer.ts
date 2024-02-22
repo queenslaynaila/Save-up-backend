4fdac753e714ad6fc7f688136f43c88cb2cd65cc
queenslaynaila <queenslayjema@gmail.com>
2024-02-22 08:33:34 +0300

    fEAT:create security answer controller

--- routes/securityAnswer/createSecurityAnswer.ts
+++ routes/securityAnswer/createSecurityAnswer.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import { createSecurityAnswerSchema } from '../../types';
 import { HttpError } from '../../middleware/errorMiddleware';
 import pool from '../../db';
+import bcrypt from 'bcrypt'
 
 export default (router: Router) => {
   router.post('/', async (req, res) => {
@@ -10,11 +11,12 @@ export default (router: Router) => {
       throw new HttpError(422, 'Invalid data');
     }
     const { question_id, user_id, answer } = validationResult.data;
+    const hashedAnswer = await bcrypt.hash(answer, 10)
     const securityQuery = `
           INSERT INTO security_answers (question_id, user_id, answer, created_at, updated_at) 
           VALUES ($1, $2, $3, NOW(), NOW())
           RETURNING *`;
-    const securityValues = [question_id, user_id, answer];
+    const securityValues = [question_id, user_id, hashedAnswer];
     const answerResult = await pool.query(securityQuery, securityValues);
     if (answerResult.rows.length === 0) {
       throw new HttpError(500, 'Failed to create security answer');
