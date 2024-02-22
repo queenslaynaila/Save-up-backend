28b10db5f16fe6eb817c8091c52c68811803850b
queenslaynaila <queenslayjema@gmail.com>
2024-02-22 09:13:51 +0300

    Refcator security answer creation

--- routes/securityAnswer/createSecurityAnswer.ts
+++ routes/securityAnswer/createSecurityAnswer.ts
@@ -2,7 +2,7 @@ import { Router } from 'express';
 import { createSecurityAnswerSchema } from '../../types';
 import { HttpError } from '../../middleware/errorMiddleware';
 import pool from '../../db';
-import bcrypt from 'bcrypt'
+import bcrypt from 'bcrypt';
 
 export default (router: Router) => {
   router.post('/', async (req, res) => {
@@ -11,17 +11,15 @@ export default (router: Router) => {
       throw new HttpError(422, 'Invalid data');
     }
     const { question_id, user_id, answer } = validationResult.data;
-    const hashedAnswer = await bcrypt.hash(answer, 10)
+    const hashedAnswer = await bcrypt.hash(answer, 10);
     const securityQuery = `
           INSERT INTO security_answers (question_id, user_id, answer, created_at, updated_at) 
-          VALUES ($1, $2, $3, NOW(), NOW())
-          RETURNING *`;
+          VALUES ($1, $2, $3, NOW(), NOW())`;
     const securityValues = [question_id, user_id, hashedAnswer];
     const answerResult = await pool.query(securityQuery, securityValues);
-    if (answerResult.rows.length === 0) {
+    if (answerResult.rowCount === 0) {
       throw new HttpError(500, 'Failed to create security answer');
     }
-    const newAnswer = answerResult.rows[0];
-    res.json(newAnswer);
+    res.json({ message: 'Security answer created successfully' });
   });
 };
