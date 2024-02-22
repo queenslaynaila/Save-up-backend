d2cdc964468eb369e67f2a639135eafb1ee4bbf9
queenslaynaila <queenslayjema@gmail.com>
2024-02-22 09:14:41 +0300

    Update password reset to involve security answers

--- routes/password/initiatePasswordReset.ts
+++ routes/password/initiatePasswordReset.ts
@@ -4,17 +4,27 @@ import { UserRole } from '../../types';
 import pool from '../../db';
 import sendSms from '../../services/twilio';
 import jwt, { Secret } from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
+
 export default (router: Router) => {
-  router.get(
-    '/:id',
+  router.post(
+    '/',
     authMiddleware({ roles: [UserRole.ADMIN, UserRole.USER] }),
     async (req, res) => {
-      const { phoneNo } = req.body;
+      const { phoneNo,securityAnswer } = req.body;
       const userQuery = 'SELECT * FROM users WHERE phone_no = $1';
       const userResult = await pool.query(userQuery, [phoneNo]);
       if (userResult.rows.length === 0) {
-        return res.status(400).json({ message: 'User with provided phone number does not exist' });
+        return res.status(400).json({ message: 'User does not exist' });
       }
+      
+      const securityAnswerQuery = 'SELECT answer FROM security_answers WHERE user_id = $1';
+      const securityResult = await pool.query(securityAnswerQuery, [userResult.rows[0].id]);
+
+      if (securityResult.rows.length === 0 || !await bcrypt.compare(securityAnswer, securityResult.rows[0].answer)) {
+        return res.status(401).json({ message: 'Incorrect security answer or security answer not found for the user' });
+      }
+
       const resetToken = jwt.sign({ phoneNo }, process.env.JWT_SECRET as Secret, {
         expiresIn: '10m',
       });
--- routes/password/resetPassword.ts
+++ routes/password/resetPassword.ts
@@ -7,36 +7,27 @@ import bcrypt from 'bcrypt';
 import jwt, { Secret } from 'jsonwebtoken';
 
 export default (router: Router) => {
-  router.patch(
-    '/:id',
+  router.post(
+    '/reset',
     authMiddleware({ roles: [UserRole.ADMIN, UserRole.USER] }),
     async (req, res) => {
       const { newPassword, resetToken } = req.body;
-
-      try {
         const decodedToken = jwt.verify(resetToken, process.env.JWT_SECRET as Secret) as {
           phone_no: string;
         };
         const phoneNo = decodedToken.phone_no;
         const hashPassword = bcrypt.hashSync(newPassword, 10);
 
-        await pool.query('UPDATE users SET password_hash = $1 WHERE phone_no = $2', [
+        const result = await pool.query('UPDATE users SET password = $1 WHERE phone_no = $2', [
           hashPassword,
           phoneNo,
         ]);
 
-        res.json({ message: 'Password updated successfully. Login' });
-      } catch (err) {
-        if (err instanceof jwt.TokenExpiredError) {
-          return res.status(400).json({
-            error: 'The reset token has expired. Please generate a new one.',
-          });
-        } else {
-          return res.status(400).json({
-            error: 'Invalid reset token.',
-          });
+        if (result.rowCount === 0) {
+          return res.status(400).json({ message: 'User does not exist' });
         }
-      }
+        res.json({ message: 'Password updated successfully. Login' });
+      
     }
   );
 };
