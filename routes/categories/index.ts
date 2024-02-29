440c0673dffb19d8f6530998ab897c198cb283c3
queenslaynaila <queenslayjema@gmail.com>
2024-02-29 13:10:56 +0300

    REFACTOR CODE

--- routes/categories/index.ts
+++ routes/categories/index.ts
@@ -2,14 +2,14 @@ import express from 'express';
 import createCategory from './createCategory';
 import deleteCategories from './deleteCategory';
 import getAllCategories from './getAllCategories';
-import updateCategories from './updateCategory';
+import updateCategory from './updateCategory';
 import getCategoriesByUserId from './getCategoriesByUserId';
 export default (baseRouter: express.Router) => {
   const router = express.Router();
   createCategory(router);
   getAllCategories(router);
   deleteCategories(router);
-  updateCategories(router);
+  updateCategory(router);
   getCategoriesByUserId(router);
 
   baseRouter.use('/categories', router);
--- routes/categories/updateCategory.ts
+++ routes/categories/updateCategory.ts
@@ -1,49 +1,50 @@
 import { Router } from 'express';
+import { z } from 'zod';
 import authMiddleware from '../../middleware/auth';
-import { UpdateCategorySchema, idSchema } from '../../types';
+import { UpdateCategorySchema } from '../../types';
 import { HttpError } from '../../middleware/errorMiddleware';
 import { sql } from '../../db';
 
-let query = 'UPDATE categories SET ';
-const SQL_UPDATE_CATEGORY = sql<
-  { name?: string; description?: string; id: string; user_id: string },
-  Record<string, never>
->(query);
+const SQL_UPDATE_CATEGORY = (updatedQuery: string) =>sql<z.infer<typeof UpdateCategorySchema> & { id: string; user_id: string }, Record<string, never>>(updatedQuery);
 
 export default (router: Router) => {
   router.patch('/:id', authMiddleware(), async (req, res) => {
-    const validationResultId = idSchema.safeParse(req.params.id);
-    if (!validationResultId.success) {
-      throw new HttpError(400, 'Invalid category ID');
-    }
-    const id = validationResultId.data;
-    const loggedInUserId = req.user!.id;
-    const validationResultBody = UpdateCategorySchema.safeParse(req.body);
-    if (!validationResultBody.success) {
+    const categoryId = req.params.id;
+    const userId = req.user!.id;
+
+    const validatedCategory = UpdateCategorySchema.safeParse(req.body);
+    if (!validatedCategory.success) {
       throw new HttpError(422, 'Invalid category data');
     }
-    const { name, description } = validationResultBody.data;
 
-    const values = [];
+    const { name, description } = validatedCategory.data;
+
+    const values: z.infer<typeof UpdateCategorySchema> & { user_id: string; category_id: string; id: string  } = {
+      user_id: userId,
+      category_id: categoryId,
+      id:categoryId
+    };
+
+    const query = 'UPDATE categories SET ';
+    let param = '';
 
     if (name) {
-      query += `name = :name, `;
-      values.push(name);
+      param += `name = :name, `;
+      values.name = name;
     }
     if (description) {
-      query += `description = :description, `;
-      values.push(description);
+      param += `description = :description, `;
+      values.description = description;
     }
 
-    query += 'updated_at = NOW() WHERE id = :id AND user_id = :user_id RETURNING *';
-    values.push(id, loggedInUserId);
+    param = param.slice(0, -2);
 
-    const result = await SQL_UPDATE_CATEGORY({
-      name,
-      description,
-      id,
-      user_id: loggedInUserId,
-    }).one(new HttpError(404, 'Category not found'));
-    res.json(result);
+    const updatedQuery = `${query}${param} WHERE user_id = :user_id AND id = :category_id RETURNING *`;
+    console.log(updatedQuery)
+    console.log(values)
+
+    const result = await SQL_UPDATE_CATEGORY(updatedQuery)(values).one(new HttpError (400, 'Category not found'));
+
+    return res.json(result);
   });
 };
