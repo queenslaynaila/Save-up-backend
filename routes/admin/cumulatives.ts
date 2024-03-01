d555e1924afa0da5b241342e91b081eb316c3223
queenslaynaila <queenslayjema@gmail.com>
2024-03-01 11:05:27 +0300

    Refactor cumulative code

--- routes/admin/cumulatives.ts
+++ routes/admin/cumulatives.ts
@@ -3,6 +3,8 @@ import { sql } from '../../db';
 import authMiddleware from '../../middleware/auth';
 import { HttpError } from '../../middleware/errorMiddleware';
 
+const SQL_GET_CUMULATIVES = (query: string) => sql<{ operator: string;tableName: string }, { totals: number }>(query);
+
 export default (router: Router) => {
   router.get('/cumulatives', authMiddleware(), async (req, res) => {
     let operator = req.query.operator as string;
@@ -16,13 +18,12 @@ export default (router: Router) => {
     if (!validOperators.includes(operator)) {
       throw new HttpError(400, 'Invalid operator');
     }
+    const query =`SELECT COALESCE(${operator}(amount), 0) AS ${operator}
+    FROM  ${tableName}`
+    const values: { operator: string;tableName: string} = { operator, tableName };
+    
 
-    const SQL_GET_TOTALS = sql<{ operator: string;tableName: string }, { totals: number }>(`
-      SELECT COALESCE(${operator}(amount), 0) AS ${operator}
-      FROM  ${tableName}
-    `);
-
-    const result = await SQL_GET_TOTALS({ operator, tableName }).one();
+    const result = await SQL_GET_CUMULATIVES(query)(values).one();
     res.json(result);
   });
 };
