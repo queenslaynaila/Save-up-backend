796d9048ef117fd9a78db32ddba946626f55c72e
queenslaynaila <queenslayjema@gmail.com>
2024-03-05 12:44:36 +0300

    Optimise code

---
+++ routes/savings/getSavingsByConditions.ts
@@ -0,0 +1,42 @@
+import { Router, Request, Response } from 'express';
+import { sql } from '../../db';
+import { savingInterface } from './index';
+import authMiddleware from '../../middleware/auth';
+import { convertToTitleCase,isValidValue } from '../../middleware/caseNormalization';
+import { HttpError } from '../../middleware/errorMiddleware';
+
+const ACCEPTED_STATUS_VALUES = ['In Progress', 'Dormant', 'Completed'];
+const ACCEPTED_PRIORITY_VALUES = ['High', 'Intermediate', 'Low'];
+const SQL_GET_SAVINGS = sql<{ userId?: string; priority?: string; status?: string; category_id?: string }, savingInterface>(`SELECT * FROM savings `);
+
+export default (router: Router) => {
+  router.get('/:savingsIdentifier', authMiddleware(), async (req: Request, res: Response) => {
+    const { savingsIdentifier } = req.params; 
+    const { category_id, priority, status,user_id } = req.query as {status: string;category_id?: string;priority?: string; user_id:string};
+    const queryParams: { user_id?: string; priority?: string; category_id?: string; status?: string } = {};
+    const filters: string[] = [];
+
+    const convertedStatus = status ? convertToTitleCase(status) : undefined;
+    const convertedPriority = priority ? convertToTitleCase(priority) : undefined;
+    const isStandardUser = req.user?.role === 'user';
+
+    if (savingsIdentifier === 'me') {
+      queryParams.user_id = req.user!.id;
+      filters.push(`user_id = '${queryParams.user_id}'`)
+    } else if(savingsIdentifier === 'all' && !isStandardUser) {
+      //if its all and user is admin or moderator basequery stays the same so nothing
+      null
+    } else if (savingsIdentifier === 'all' && isStandardUser) {
+      throw new HttpError(400, 'Unauthorized');
+    } else {
+      throw new HttpError(400, 'Invalid ');
+    }
+
+    if (user_id) filters.push(`user_id = '${user_id}'`);
+    if (category_id) filters.push(`category_id = '${category_id}'`);
+    if (convertedPriority && isValidValue(convertedPriority, ACCEPTED_PRIORITY_VALUES)) filters.push(`priority = '${convertedPriority}'`);
+    if (convertedStatus && isValidValue(convertedStatus, ACCEPTED_STATUS_VALUES)) filters.push(`status = '${convertedStatus}'`);
+    const expenses = await SQL_GET_SAVINGS(queryParams).extend(` WHERE ${filters.join(' AND ')}` ,queryParams).many();
+    res.json(expenses);
+  });
+};
