49aa2239ac745873e79188040624c8aeabc943c7
queenslaynaila <queenslayjema@gmail.com>
2024-03-06 15:03:25 +0300

    remove delted file

--- routes/password/index.ts
+++ routes/password/index.ts
@@ -1,10 +1,10 @@
 import express from 'express';
-import initiatePasswordReset from './initiatePasswordReset';
+
 import resetPassword from './resetPassword';
 
 export default (baseRouter: express.Router) => {
   const router = express.Router();
-  initiatePasswordReset(router);
+
   resetPassword(router);
 
   baseRouter.use('/password', router);
