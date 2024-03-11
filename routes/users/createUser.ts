1d2c12f491b90574f4ef628f3b5dfca1d703359b
queenslaynaila <queenslayjema@gmail.com>
2024-03-11 11:50:41 +0300

    Add catch block

--- routes/users/createUser.ts
+++ routes/users/createUser.ts
@@ -36,7 +36,9 @@ export default (router: Router) => {
         password: passwordHash,
       })
         .one()
-       
+        .catch(() => {
+          throw new HttpError(400, 'An account with the provided phone number already exists');
+        });
 
       const token = generateToken(newUser.id, newUser.role, '1h');
       const refreshToken = generateToken(newUser.id, newUser.role, '1h');
