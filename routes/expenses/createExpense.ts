db31631768e5308665fbd70096271cd41c834f05
queenslaynaila <queenslayjema@gmail.com>
2024-03-18 12:20:52 +0300

    Revisions

--- routes/expenses/createExpense.ts
+++ routes/expenses/createExpense.ts
@@ -20,7 +20,7 @@ export default (router: Router) => {
       if (!validationResult.success) {
         throw new HttpError(400, 'Invalid expense data provided');
       }
-      const { description, category_id, amount, date, user_id } = validationResult.data;
+      const { description, category_id, amount, expense_date, user_id } = validationResult.data;
       const loggedInUserId = req.user!.id;
       const authenticatedUserId = req.user?.id;
       if (authenticatedUserId !== user_id) {
@@ -30,7 +30,7 @@ export default (router: Router) => {
         description,
         category_id,
         amount,
-        date,
+        expense_date,
         user_id: loggedInUserId,
       })
         .one()
--- routes/password/ForgetPasswordRoutes.ts
+++ routes/password/ForgetPasswordRoutes.ts
@@ -1,52 +1,64 @@
 import { Router } from 'express';
-import { generateRandomToken } from '../../middleware/generateRandomToken';
+import { NextFunction, Request, Response } from 'express';
+import jwt, { Secret } from 'jsonwebtoken';
 import bcrypt from 'bcrypt';
 import { HttpError } from '../../middleware/errorMiddleware';
+import { generateRandomToken } from '../../middleware/generateRandomToken';
+import sendSms from '../../services/twilio';
 import { UserSchema } from '../../types';
 import { sql } from '../../db';
-import sendSms from '../../services/twilio';
+
+declare module 'express-serve-static-core' {
+  interface Request {
+    userId?: string;
+    isTokenGenerated: boolean;
+  }
+}
+
+const SQL_GET_USER = sql<{ phone_number: string }, Pick<UserSchema, 'id' | 'first_name' | 'last_name' | 'role' | 'created_at' | 'updated_at'>>(
+  `SELECT id, first_name, last_name, role, created_at, updated_at FROM users WHERE phone_number = :phone_number`
+);
+
+const SQL_SAVE_TOKEN = sql<{ user_id: string; token: string }, { token: string }>(
+  `INSERT INTO reset_tokens (user_id, token) VALUES (:user_id, :token) RETURNING token`
+);
+
+const SQL_UPDATE_TOKEN_USAGE = sql<{ user_id: string; reset_token: string }, Record<string, never>>(
+  `UPDATE reset_tokens SET used = TRUE, used_at = CURRENT_TIMESTAMP WHERE user_id = :user_id AND token = :reset_token`
+);
+
+const SQL_RESET_PASSWORD = sql<{ password: string; id: string }, { phone_number: string }>(
+  `UPDATE users SET password = $1 WHERE  id = :id`
+);
 
 const SQL_GET_SECURITY_ANSWERS = sql<{ user_id: string }, { question_id: string; answer: string }>(
   `SELECT question_id, answer FROM security_answers WHERE user_id = :user_id`
 );
 
-const SQL_GET_SECURITY_QUESTIONS = sql<{ user_id: string },{ question: string; question_id: string }>(`
-  SELECT sq.id AS question_id, sq.question FROM security_answers sa INNER JOIN security_questions sq ON sa.question_id = sq.id WHERE sa.user_id = :user_id
-`);
+const SQL_GET_SECURITY_QUESTIONS = sql<{ user_id: string }, { question: string; question_id: string }>(
+  `SELECT sq.id AS question_id, sq.question FROM security_answers sa INNER JOIN security_questions sq ON sa.question_id = sq.id WHERE sa.user_id = :user_id`
+);
 
-const SQL_GET_USER = sql<{ phone_number: string },Pick<UserSchema, 'id' | 'first_name' | 'last_name' | 'role' | 'created_at' | 'updated_at'>>(`
-  SELECT id, first_name, last_name, role, created_at, updated_at FROM users WHERE phone_number = :phone_number
-`);
-
-const SQL_RESET_PASSWORD = sql<{ password: string; id: string }, { phone_number: string }>(`
-  UPDATE users SET password = $1 WHERE  id = :id
-`);
-
-const SQL_SAVE_TOKEN = sql<{ user_id: string; token: string }, { token: string }>(`
-  INSERT INTO reset_tokens (user_id, token) VALUES (:user_id, :token) RETURNING token
-`);
-
-const SQL_CHECK_TOKEN_EXISTS = sql<{ user_id: string; reset_token: string },{ token_exists: boolean }>(`
-  SELECT EXISTS(SELECT * FROM reset_tokens WHERE user_id = :user_id AND token = :reset_token ) AS token_exists
-`);
-
-const SQL_UPDATE_TOKEN_USAGE = sql<{ user_id: string; reset_token: string }, Record<string, never>>(`
-  UPDATE reset_tokens SET used = TRUE, used_at = CURRENT_TIMESTAMP WHERE user_id = :user_id AND token = :reset_token
-`);
-
-const SQL_GET_TOKEN_INFO = sql<{ user_id: string; reset_token: string }, { used: boolean }>(`
-  SELECT used FROM reset_tokens WHERE user_id = :user_id AND token = :reset_token
-`);
-
-interface SecurityAnswersRequest {
-  message: string;
-  user_id: string;
-  answers: { question_id: string; answer: string }[];
-}
+const verifyResetToken = (req: Request, res: Response, next: NextFunction) => {
+  const token = req.headers['X-Reset-Token'] as string;
+  if (!token) {
+    return res.status(403).json({ message: 'Access denied' });
+  }
+  const resetTokenValue = token.split(' ')[1];
+  jwt.verify(resetTokenValue, process.env.JWT_SECRET as Secret, (err, decoded) => {
+    if (err) {
+      return next(err);
+    } else {
+      const { userId } = decoded as { userId: string };
+      req.userId = userId;
+      next();
+    }
+  });
+};
 
 export const initiatePasswordReset = (router: Router) => {
-  router.post<string,Record<string, never>,{ message: string,userId:string },{ phone_number: string },Record<string, never>>(
-    '/forget-password-request', 
+  router.post<string, Record<string, never>, { message: string }, { phone_number: string }, Record<string, never>>(
+    '/forget-password-request',
     async (req, res) => {
       const { phone_number } = req.body;
       const user = await SQL_GET_USER({ phone_number }).one(new HttpError(404, 'User not found.'));
@@ -55,49 +67,45 @@ export const initiatePasswordReset = (router: Router) => {
         new HttpError(500, 'Error saving token')
       );
       const actualToken = token.token;
+      const accessToken = jwt.sign({ userId: user.id }, 'process.env.JWT_SECRET as Secret', { expiresIn: "15m" });
       sendSms(
         phone_number,
         `Your password reset token is: ${actualToken}. It expires in 10 minutes. Do not share with anyone.`
       );
-      res.json({ message: 'Password reset token generated and sent successfully.', userId: user.id });
+      res.setHeader('X-Reset-Token', accessToken).json({ message: 'Password reset token generated and sent successfully.' });
     });
 };
+
 interface securityQuestions{
   securityQuestions: { question_id: string; question: string }[]
 }
+
 export const verifyPasswordResetToken = (router: Router) => {
   router.post<Record<string, never>, securityQuestions, { user_id: string; reset_token: string }, Record<string, never>, Record<string, never>>(
-    '/verify-token', 
+    '/verify-token',
+    verifyResetToken,
     async (req, res) => {
-      const { user_id, reset_token } = req.body;
-
-      const tokenExists = await SQL_CHECK_TOKEN_EXISTS({ user_id, reset_token }).one();
-      if (!tokenExists) {
-        throw new HttpError(401, 'Invalid token');
-      }
-      console.log(tokenExists)
-      const tokenInfo = await SQL_GET_TOKEN_INFO({ user_id, reset_token }).one();
-      if (tokenInfo.used) {
-        throw new HttpError(401, 'Token has already been used');
-      }
+      const { reset_token } = req.body;
+      const user_id = req.userId!;
       await SQL_UPDATE_TOKEN_USAGE({ user_id, reset_token }).exec();
-      const securityQuestions = await SQL_GET_SECURITY_QUESTIONS({ user_id })
-        .many()
-        .catch(() => {
-          throw new HttpError(
-            404,
-            'No security questions found for you. Contact Support for assistance'
-          );
-        });
+      const securityQuestions = await SQL_GET_SECURITY_QUESTIONS({ user_id }).many()
       res.json({ securityQuestions });
     });
 };
 
+interface SecurityAnswersRequest {
+  message: string;
+  user_id: string;
+  answers: { question_id: string; answer: string }[];
+}
+
 export const verifySecurityAnswers = (router: Router) => {
-  router.post<string,Record<string, never>,{ message: string },SecurityAnswersRequest,Record<string, never>>(
-    '/verify-security-answers', 
+  router.post<string, Record<string, never>, { message: string }, SecurityAnswersRequest, Record<string, never>>(
+    '/verify-security-answers',
+    verifyResetToken,
     async (req, res) => {
-      const { answers, user_id } = req.body;
+      const { answers } = req.body;
+      const user_id = req.userId!;
       const userSecurityAnswers = await SQL_GET_SECURITY_ANSWERS({ user_id }).many();
       const incorrectAnswers: string[] = [];
       answers.forEach(({ question_id, answer }: { question_id: string; answer: string }) => {
@@ -118,12 +126,14 @@ export const verifySecurityAnswers = (router: Router) => {
 };
 
 export const resetPassword = (router: Router) => {
-  router.post<string,Record<string, never>,{ message: string },{ new_password: string; id: string },Record<string, never>>(
-    '/reset', 
+  router.post<string, Record<string, never>, { message: string }, { new_password: string; id: string }, Record<string, never>>(
+    '/reset',
+    verifyResetToken,
     async (req, res) => {
-      const { new_password, id } = req.body;
+      const { new_password } = req.body;
+      const user_id = req.userId!;
       const hashPassword = bcrypt.hashSync(new_password, 10);
-      await SQL_RESET_PASSWORD({ id: id, password: hashPassword }).exec();
+      await SQL_RESET_PASSWORD({ id: user_id, password: hashPassword }).exec();
       res.json({ message: 'Password updated successfully. Login' });
     });
 };
