ac32a969d77f4c53a6fccb9fba30aba4789fa5a4
queenslaynaila <queenslayjema@gmail.com>
2024-04-04 14:59:00 +0300

    Add types

--- types/index.ts
+++ types/index.ts
@@ -165,3 +165,165 @@ export interface UserSchema {
   role: UserRole;
   created_at: Date;
 }
+
+// NEXT OF KIN SCHEMA
+// ---------------------------------------------------------------------------------------------------------
+export const NextOfKinSchema = z.object({
+  user_id:z.number(),
+  full_name: z.string(),
+  relationship: z.string(),
+  email: z.string().email()
+});
+
+export const ExtendedNextOfKinSchema= NextOfKinSchema.extend({
+  created_at: z.string(),
+  updated_at: z.string()
+})
+
+// GROUPS SCHEMA
+// ---------------------------------------------------------------------------------------------------------
+
+export const CreateGroupSchema = z.object({
+  name: z.string(),
+  description: z.string(), 
+  created_by:z.number(),
+});
+export type CreateGroupInterface = z.infer<typeof CreateGroupSchema>;
+
+export const CreateGroupResponse = z.object({
+  id:z.number(),
+  name: z.string(),
+  description: z.string(), 
+  created_by:z.string(),
+  created_at:z.string(),
+  updated_at:z.string()
+});
+
+export type CreateGroupResponseInterface = z.infer<typeof CreateGroupResponse>;
+
+
+export const CreateGroupGoalSchema = z.object({
+  group_id: z.string(),
+  category_id:z.number(),
+  description: z.string(), 
+  target_amount:z.number(),
+  priority: z.string(),
+  target_at:z.date(),
+});
+
+export type CreateGroupGoalInterface = z.infer<typeof CreateGroupGoalSchema>;
+
+export const CreateGroupGoalResponse = z.object({
+  id:z.number(),
+  name: z.string(),
+  description: z.string(), 
+  created_by:z.string(),
+  created_at:z.string(),
+  updated_at:z.string()
+});
+
+export type CreateGroupGoalResponseInterface = z.infer<typeof CreateGroupGoalResponse>;
+
+export const  GetGroupGoalsSchema = CreateGroupGoalSchema.pick({
+  group_id: true,
+});
+
+export const GetGroupGoalsResponse = z.object({
+  id: z.number(),
+  owner_id: z.number(),
+  category_id: z.number(),
+  description: z.string(),
+  target_amount: z.number(),
+  priority: z.string(),
+  target_at: z.string(),
+});
+
+export type GetGroupGoalsResponseInterface = z.infer<typeof GetGroupGoalsResponse>;
+
+export type GetGroupGoalsInterface = z.infer<typeof GetGroupGoalsSchema>;
+
+export const ExitGroupSchema = z.object({
+  group_id: z.number(),
+  user_id: z.number(),
+});
+
+export type ExitGroupInterface = z.infer<typeof ExitGroupSchema>;
+
+
+export const UpdateGroupSchema = z.object({
+  group_id: z.number(),
+  name: z.string().optional(),
+  description: z.string().optional(),
+});
+
+export type UpdateGroupInterface = z.infer<typeof UpdateGroupSchema>;
+
+// INVITE SCHEMA
+// ---------------------------------------------------------------------------------------------------------
+
+export const SendInviteSchema = z.object({
+  receiver_id:z.number(),
+  sender_id:z.number(),
+  group_id:z.number(),
+});
+
+export type SendInviteInterface = z.infer<typeof SendInviteSchema>;
+
+export const InviteSchema = z.object({
+  group_id:z.number(),
+  sender_id:z.number(),
+  receiver_id:z.number(),
+});
+
+export const InviteResponseSchema = InviteSchema.extend({
+  status:z.string()
+}).omit({sender_id: true});
+
+export type InviteResponseInterface = z.infer<typeof InviteResponseSchema>;
+
+export const ElectionSchema = z.object({
+  group_id:z.number(),
+  caller_id:z.number(),
+  election_name:z.string(),
+  start_at:z.date(),
+  end_at:z.date()
+});
+
+export type ElectionInterface = z.infer<typeof ElectionSchema>;
+
+export interface ElectionResponse {
+  id: number;
+  group_id: number;
+  election_name: string;
+  caller_id: number;
+  start_at: Date;
+  end_at: Date;
+  created_at: Date;
+}
+
+
+export const ElectionVoteSchema = z.object({
+  election_id: z.number(), 
+  candidate_id: z.number(), 
+  voter_id: z.number(),
+  group_id: z.number(), 
+});
+
+export type ElectionVoteInterface = z.infer<typeof ElectionVoteSchema>;
+
+export interface ElectionCandidateSchema {
+  election_id: number;
+  candidate_id: number;
+}
+
+export const  ElectionCandidateSchema = z.object({
+  election_id: z.number(),
+  candidate_id: z.number()
+});
+
+export interface ElectionCandidateResponse {
+  election_id: number;
+  candidate_id: number;
+  votes: number;
+  created_at: Date;
+}
