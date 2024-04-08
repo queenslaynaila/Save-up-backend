218ab5ad2a732b6469e6fbd3d51ba5f58c27e979
queenslaynaila <queenslayjema@gmail.com>
2024-04-08 12:45:01 +0300

    Add socket io for real time chats

--- app.ts
+++ app.ts
@@ -24,6 +24,7 @@ cron.schedule('0 3 1 */6 *', sendSecurityReminderSMS);
 
 // Middleware
 const app = express();
+
 app.use(express.urlencoded({ extended: false, limit: '10mb' }));
 app.use(express.json());
 app.use(morgan('dev'));
--- index.ts
+++ index.ts
@@ -1,10 +1,32 @@
 import dotenv from 'dotenv';
+import http from 'http';
 import app from './app';
+import { Server as SocketIOServer } from 'socket.io';
 
 dotenv.config();
 
 const port: number = parseInt(process.env.PORT as string, 10);
+const server = http.createServer(app);
+const io = new SocketIOServer(server);
 
-app.listen(port, () => {
-  console.log(`app listening on port ${port}`);
+io.on('connection', (socket) => {
+  console.log('A user connected');
+
+  socket.on('joinGroup', ({ room }, callback) => {
+    socket.join(room);
+    callback();
+  });
+
+  socket.on('sendMessage', (message, callback) => {
+    io.to(message.room).emit('message', { user: message.user, text: message.text });
+    callback();
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+  });
+});
+
+server.listen(port, () => {
+  console.log(`App listening on port ${port}`);
 });
