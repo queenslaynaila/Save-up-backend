8b56a7e13947a6ef75440f21f8e90058e4051855
queenslaynaila <queenslayjema@gmail.com>
2024-04-09 14:51:57 +0300

    Update answer routes

--- routes/securityAnswer/index.ts
+++ routes/securityAnswer/index.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 import createSecurityAnswer from './createSecurityAnswer';
+import deleteSecurityAnswer from './deleteSecurityAnswer';
+
 export default (baseRouter: Router) => {
   const router = Router();
-
   createSecurityAnswer(router);
-
+  deleteSecurityAnswer(router);
   baseRouter.use('/security-answers', router);
 };
