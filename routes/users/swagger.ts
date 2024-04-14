e3e7cf049bd220ccb837715e59bddd79b2e117f1
queenslaynaila <queenslayjema@gmail.com>
2024-04-14 18:25:15 +0300

    Fix

--- routes/users/swagger.ts
+++ routes/users/swagger.ts
@@ -1,5 +1,12 @@
 /**
  * @swagger
+ * tags:
+ *   name: Users
+ *   description: The user management API endpoints
+ */
+
+/**
+ * @swagger
  * components:
  *   schemas:
  *     User:
@@ -19,13 +26,18 @@
  *           enum: [Male, Female, Prefer not to say]
  *           description: The gender of the user.
  *         national_id:
- *           type: number
- *           description: The national ID of the user.
+ *           type: integer
+ *           minimum: 10000000
+ *           maximum: 99999999
+ *           description: The national ID of the user, an 8-digit integer.
  *         phone_number:
  *           type: string
- *           description: The phone number of the user.
+ *           pattern: '^\+254\d{9}$'
+ *           description: Phone number with the format +254XXXXXXXXX (12 digits).
  *         pin:
- *           type: number
+ *           type: string
+ *           minLength: 4
+ *           maxLength: 4
  *           description: The PIN of the user.
  *       example:
  *         full_name: John Doe
@@ -33,13 +45,6 @@
  *         national_id: 12345678
  *         phone_number: "+254713518356"
  *         pin: 1234
- */
-
-/**
- * @swagger
- * tags:
- *   name: Users
- *   description: The user management API endpoints
  */
 
 /**
@@ -52,7 +57,7 @@
  *       required: true
  *       content:
  *         application/json:
- *           schema:
+  *           schema:
  *             type: object
  *             required:
  *               - full_name
@@ -79,7 +84,7 @@
  *                 type: string
  *                 pattern: '^\d{4}$'
  *     responses:
- *       200:
+ *       '200':
  *         description: Account created successfully. Proceed to login.
  *         content:
  *           application/json:
@@ -89,7 +94,7 @@
  *                 message:
  *                   type: string
  *                   description: Message indicating successful account creation.
- *       400:
+ *       '400':
  *         description: Account with this Phone number already exists.
  *         content:
  *           application/json:
@@ -99,7 +104,7 @@
  *                 message:
  *                   type: string
  *                   description: Error message indicating the reason for failure.
- *       500:
+ *       '500':
  *         description: Internal Server Error.
  *         content:
  *           application/json:
@@ -124,8 +129,8 @@
  *           schema:
  *             type: object
  *             required:
- *                  - phone_number
- *                  - pin
+ *               - phone_number
+ *               - pin
  *             properties:
  *               phone_number:
  *                 type: string
@@ -135,7 +140,8 @@
  *                 type: string
  *                 pattern: '^\d{4}$'
  *     responses:
- *       200:
+ *       '200':
+ *         description: User logged in successfully.
  *         content:
  *           application/json:
  *             schema:
@@ -161,13 +167,13 @@
  *           Refresh-token:
  *             description: The refresh token for the user session.
  *             schema:
- *               type: <Bearer> string
+ *               type: string
  *           Authorization:
  *             description: The access token for the user session.
  *             schema:
- *               type: <Bearer>string
- *       400:
- *         description: Invalid phone number or password combination..
+ *               type: string
+ *       '400':
+ *         description: Invalid phone number or password combination.
  *         content:
  *           application/json:
  *             schema:
@@ -176,7 +182,7 @@
  *                 message:
  *                   type: string
  *                   description: Error message indicating the reason for failure.
- *       404:
+ *       '404':
  *         description: User not found.
  *         content:
  *           application/json:
@@ -188,7 +194,6 @@
  *                   description: Error message indicating the reason for failure.
  */
 
-
 /**
  * @swagger
  * /users/signout:
@@ -196,15 +201,14 @@
  *     summary: Logout 
  *     tags: [Users]
  *     security:
- *       - AuthToken: []
+ *       - AuthorizationToken: []
  *       - RefreshToken: []
  *     responses:
- *       200:
+ *       '200':
  *         description: User logged out successfully.
- *       500:
+ *       '500':
  *         description: Internal Server Error.
  */
-
 
 /**
  * @swagger
@@ -241,9 +245,9 @@
  *             pin: "1234"
  *             phone_number: "+254713518356"
  *     responses:
- *       200:
+ *       '200':
  *         description: Phone number updated. For continued security, please log in again with your new phone number.
- *       404:
+ *       '404':
  *         description: Not found.
  *         content:
  *           application/json:
@@ -253,7 +257,7 @@
  *                 message:
  *                   type: string
  *                   description: Error message indicating the reason for failure.
- *       401:
+ *       '401':
  *         description: Invalid password.
  *         content:
  *           application/json:
@@ -278,26 +282,43 @@
  *           type: string
  *         required: true
  *         description: |
- *           Defines the scope of users to retrieve.Can be one of the following:
+ *           Defines the scope of users to retrieve. Can be one of the following:
  *           - "me": Retrieves information about the currently logged-in user.
  *           - "all": Retrieves information about all users (requires Admin or Moderator role).
- *           - A positive integer: Retrieves information about a specific user based on their ID (requires Admin or Moderator role).
+ *           - A user ID: Retrieves information about a specific user based on their ID.
+ *             Standard users can only request their own user ID. Admins and moderators can request any user ID.
  *       - in: query
  *         name: role
  *         schema:
  *           type: string
- *         description: |
- *           Filter users by role. Can be one of the following: 'User', 'Admin', 'Moderator' (case-sensitive).Optiona;
+ *         description: Filter users by role. Can be 'User', 'Admin', 'Moderator' (case-sensitive).
  *     responses:
- *       200:
+ *       '200':
  *         description: Array of users matching the specified conditions.
  *         content:
  *           application/json:
  *             schema:
  *               type: array
- *               items:
- *                 $ref: '#/components/schemas/User'  # Assuming you have a User schema defined
- *       400:
+ *               items: 
+ *                 type: object
+ *                 properties:
+ *                   id:
+ *                     type: string
+ *                     description: The unique identifier of the user.
+ *                   full_name:
+ *                     type: string
+ *                     description: The full name of the user.
+ *                   gender:
+ *                     type: string
+ *                     description: The gender of the user.
+ *                   role:
+ *                     type: string
+ *                     description: The role of the user.
+ *                   created_at:
+ *                     type: string
+ *                     format: date-time
+ *                     description: The date and time when the user account was created.
+ *       '400':
  *         description: Bad request. Invalid targetUser value or missing required filter parameters.
  *         content:
  *           application/json:
@@ -307,7 +328,7 @@
  *                 message:
  *                   type: string
  *                   description: Error message indicating the reason for failure.
- *       401:
+ *       '401':
  *         description: Unauthorized. Access denied due to insufficient permissions.
  *         content:
  *           application/json:
@@ -317,7 +338,7 @@
  *                 error:
  *                   type: string
  *                   description: Error message indicating the reason for failure.
- *       403:
+ *       '403':
  *         description: Forbidden. User lacks permission to retrieve all users.
  *         content:
  *           application/json:
@@ -327,22 +348,4 @@
  *                 error:
  *                   type: string
  *                   description: Error message indicating the reason for failure.
- *     requestBody:
- *       required: false  # Remove required attribute if optional filtering is allowed
- *       content:
- *         application/json:
- *           schema:
- *             type: object
- *             properties:
- *               pin:
- *                 type: string
- *                 minLength: 4
- *                 maxLength: 4
- *               phone_number:
- *                 type: string
- *                 pattern: '^\+254\d{9}$'
- *                 description: Phone number with the format +254XXXXXXXXX (12 digits).
- *             example:
- *               pin: "1234"
- *               phone_number: "+254713518356"
  */
