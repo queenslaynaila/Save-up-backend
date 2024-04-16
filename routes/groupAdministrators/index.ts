b5d0382a2e3f4105fb4ea4a6c101de7dd5d5e2d0
queenslaynaila <queenslayjema@gmail.com>
2024-04-16 17:08:34 +0300

    Remove ompute grp admin from app layer

--- routes/groupAdministrators/index.ts
+++ routes/groupAdministrators/index.ts
@@ -2,7 +2,6 @@ import express from 'express';
 import proposeGroupAdmins from './proposeGroupAdmins';
 import getProposedGroupAdmins from './getProposedGroupAdmins';
 import approveProposedGroupAdmin from './approveProposedGroupAdmin';
-import computeAdminApprovalResults from './computeAdminApprovalResults'
 import makeGroupAdmin from './makeGroupAdmin';
 
 export default (baseRouter: express.Router) => {
@@ -11,7 +10,6 @@ export default (baseRouter: express.Router) => {
   proposeGroupAdmins(router);
   getProposedGroupAdmins(router)
   approveProposedGroupAdmin(router);
-  computeAdminApprovalResults(router)
   makeGroupAdmin(router);
 
   baseRouter.use('/group-admin', router);
