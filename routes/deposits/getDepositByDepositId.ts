25fd0946eeb96a4b0266b66e24720f7a51276dde
queenslaynaila <queenslayjema@gmail.com>
2024-04-25 09:57:28 +0300

    REfactor:move types to individual folders

--- routes/deposits/getDepositByDepositId.ts
+++ routes/deposits/getDepositByDepositId.ts
@@ -2,21 +2,23 @@ import { Router } from 'express';
 import { sql } from '../../db';
 import authMiddleware from '../../middleware/authorization';
 import { HttpError } from '../../middleware/errorMiddleware';
-import { SavingInterface } from '../../types';
+import { DepositInterface } from './types';
+import { IdParamInterface } from '../../types';
 
-const  SQL_GET_SAVING_BY_ID = sql<{ id: number; user_id:number}, SavingInterface>(`
+const  SQL_GET_DEPOSIT_BY_ID = sql<{ id: number; user_id:number}, DepositInterface>(`
   SELECT * FROM savings WHERE id = :id AND user_id = :user_id
 `);
 
 export default (router: Router) => {
-  router.get<{ id: string }, SavingInterface, Record<string,never>, Record<string,never>>(
+  router.get<IdParamInterface, DepositInterface, Record<string,never>, Record<string,never>>(
     '/records/:id', 
     authMiddleware(), 
     async (req, res) => {
       const contributionsId = parseInt(req.params.id);
       const user_id= req.user!.id
-      const result = await SQL_GET_SAVING_BY_ID({ id: contributionsId,user_id })
+      const result = await SQL_GET_DEPOSIT_BY_ID({ id: contributionsId,user_id })
         .one(new HttpError(404, 'Not found'));
       return res.json(result);
     });
 };
+ 
