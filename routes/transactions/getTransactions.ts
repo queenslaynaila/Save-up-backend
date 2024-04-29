8d94f4f6f0667373131ffe5f4dc292a488f48638
queenslaynaila <queenslayjema@gmail.com>
2024-04-29 11:25:56 +0300

    Fix:casing in transactions

--- routes/transactions/getTransactions.ts
+++ routes/transactions/getTransactions.ts
@@ -12,7 +12,7 @@ const SQL_GET_TRANSACTIONS = sql<GetTransactionsInput, GetTransactionResp>(`
             NULL AS transfer_from
             d.created_at AS transaction_date
         FROM deposits d
-        WHERE d.pocket_id = :pocket_id
+        WHERE d.pocket_id = :pocketId
 
         UNION ALL
 
@@ -23,7 +23,7 @@ const SQL_GET_TRANSACTIONS = sql<GetTransactionsInput, GetTransactionResp>(`
             NULL AS transfer_from
             w.created_at AS transaction_date
         FROM withdrawals w
-        WHERE w.pocket_id = :pocket_id
+        WHERE w.pocket_id = :pocketId
 
         UNION ALL
 
@@ -34,7 +34,7 @@ const SQL_GET_TRANSACTIONS = sql<GetTransactionsInput, GetTransactionResp>(`
             t.source_pocket_id AS transfer_from
             t.created_at AS transaction_date
         FROM transfers t
-        WHERE t.destination_pocket_id = :pocket_id
+        WHERE t.destination_pocket_id = :pocketId
     ) AS transactions
 `);
 
@@ -42,29 +42,29 @@ export default (router: Router) => {
   router.get<string,IdParamInterface, GetTransactionResp[], Record<string,never>, GetTransactionQuery>(
     '/:pocket_id', 
     async (req, res) => {
-      const pocket_id  = parseInt(req.params.id);
-      const { transaction_type, from_date, to_date } = req.query;
+      const pocketId  = parseInt(req.params.id);
+      const { transactionType, fromDate, toDate } = req.query;
       const filters: string[] = [];
       const filterArgs: Record<string, string | Date> = {};
 
-      if (transaction_type) {
-        filterArgs.transaction_type = transaction_type;
-        filters.push(`transaction_type = :transaction_type`);
+      if (transactionType) {
+        filterArgs.transaction_type = transactionType;
+        filters.push(`transaction_type = :transactionType`);
       }
-      if (from_date) {
-        filterArgs.from_date = from_date;
-        filters.push(`transaction_date >= :from_date`);
+      if (fromDate) {
+        filterArgs.fromDate = fromDate;
+        filters.push(`transaction_date >= :fromDate`);
       }
-      if (to_date) {
-        filterArgs.to_date = to_date;
-        filters.push(`transaction_date <= :to_date`);
+      if (toDate) {
+        filterArgs.toDate = toDate;
+        filters.push(`transaction_date <= :toDate`);
       }
       if (filters.length > 0) {
-        filterArgs.pocket_id = pocket_id.toString();
-        filters.push(`pocket_id = :pocket_id`);
+        filterArgs.pocketId = pocketId.toString();
+        filters.push(`pocket_id = :pocketId`);
       }
 
-      const query = SQL_GET_TRANSACTIONS({ pocket_id });
+      const query = SQL_GET_TRANSACTIONS({ pocketId });
       if (filters.length > 0) query.extend(`WHERE ${filters.join(' AND ')}`, filterArgs);
       query.extend('LIMIT 15', {});
       return res.json(await query.many());
--- routes/transactions/types.ts
+++ routes/transactions/types.ts
@@ -7,25 +7,25 @@ export const enum transactionType {
 }
 
 export const getTransactionsInputSchema = z.object({
-  pocket_id: z.number()
+  pocketId: z.number()
 })
 
 export type GetTransactionsInput = z.infer<typeof getTransactionsInputSchema>;
 
 export const getTransactionRespSchema = z.object({
-  transaction_id: z.number().positive(),
-  transaction_type: z.enum([transactionType.DEPOSIT, transactionType.WITHDRAWAL, transactionType.TRANSFER]),
+  transactionId: z.number().positive(),
+  transactionType: z.enum([transactionType.DEPOSIT, transactionType.WITHDRAWAL, transactionType.TRANSFER]),
   amount: z.number(),
-  transaction_date: z.string(),
-  transfer_from: z.string() || null
+  transactionDate: z.string(),
+  transferFrom: z.string() || null
 })
 
 export type GetTransactionResp = z.infer<typeof getTransactionRespSchema>;
 
 export const getTransactionQuery = z.object({
-  transaction_type: z.enum([transactionType.DEPOSIT, transactionType.WITHDRAWAL, transactionType.TRANSFER]).optional(),
-  from_date: z.date().optional(),
-  to_date: z.date().optional()
+  transactionType: z.enum([transactionType.DEPOSIT, transactionType.WITHDRAWAL, transactionType.TRANSFER]).optional(),
+  fromDate: z.date().optional(),
+  toDate: z.date().optional()
 }) 
 
 export type GetTransactionQuery = z.infer<typeof getTransactionQuery>;
