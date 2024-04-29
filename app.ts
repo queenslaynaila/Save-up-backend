ef85b0aeb61c57c9c5d5c13d4fcf2e12e131c5be
queenslaynaila <queenslayjema@gmail.com>
2024-04-29 08:05:51 +0300

    Refactor:change goal folder to pockets

--- app.ts
+++ app.ts
@@ -8,7 +8,7 @@ import swaggerJsDoc from "swagger-jsdoc";
 import usersRoutes from './routes/users/index';
 import nextOfKinRoutes from './routes/nextOfKin/index';
 import categoriesRoutes from './routes/categories/index';
-import savingsRoutes from './routes/goals/index';
+import savingsRoutes from './routes/pockets/index';
 import expensesRoutes from './routes/expenses/index';
 import depositRoutes from './routes/deposits/index';
 import AdminRoutes from './routes/admin/index';
