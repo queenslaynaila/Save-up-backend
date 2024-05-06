7a6629849ca44f2ab8f7536ea800199b18a77988
queenslaynaila <queenslayjema@gmail.com>
2024-05-06 13:10:21 +0300

    Fix:pocket creation validation error

--- routes/pockets/createPocket.ts
+++ routes/pockets/createPocket.ts
@@ -5,18 +5,18 @@ import { validateRequest } from '../../middleware/validationMiddleware';
 import { CreatePocketInterface, PocketInterface, basePocketSchema } from './types';
 
 const SQL_CREATE_POCKET = sql<CreatePocketInterface, PocketInterface>(`
-  INSERT INTO pockets (entity_id, category_id, name, target_amount, priority, target_at, pocket_type)
-  VALUES (:entity_id, :category_id, :name, :target_amount, :priority, :target_at, :pocket_type)
+  INSERT INTO pockets (id, entity_id, category_id, name, target_amount, priority, target_at, pocket_type)
+  VALUES ((SELECT COALESCE(MAX(id), 0) + 1 FROM pockets WHERE entity_id = :entity_id), 
+          :entity_id, :category_id, :name, :target_amount, :priority, :target_at, :pocket_type)
   RETURNING id, entity_id, name, category_id, target_amount, priority, target_at, created_at, completed_at;
 `);
 
-const pocketSchema = basePocketSchema.omit({ entity_id: true })
 
 export default (router: Router) => {
   router.post<Record<string,never>, PocketInterface, CreatePocketInterface, Record<string,never>, Record<string,never>>(
     '/', 
     authMiddleware(), 
-    validateRequest(pocketSchema),
+    validateRequest(basePocketSchema),
     async (req, res) => {
       const { entity_id, category_id, name, target_amount, priority, target_at, pocket_type } = req.body;
       const newPocket = await SQL_CREATE_POCKET({
