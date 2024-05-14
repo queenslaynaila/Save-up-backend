36afb0343cd007dae53a46f305acb094d7acd18e
queenslaynaila <queenslayjema@gmail.com>
2024-05-14 11:04:41 +0300

    Fix: order of keys in foreign key ref

--- database/transfers.sql
+++ database/transfers.sql
@@ -6,8 +6,8 @@ CREATE TABLE IF NOT EXISTS transfers (
   destination_pocket_id   INT NOT NULL,
   amount                  NUMERIC(30, 2) NOT NULL,
   created_at              TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
-  FOREIGN KEY             (source_pocket_id, user_id) REFERENCES pockets(entity_id, id),
-  FOREIGN KEY             (destination_pocket_id, user_id) REFERENCES pockets(entity_id, id),
+  FOREIGN KEY             (user_id, source_pocket_id) REFERENCES pockets(entity_id, id),
+  FOREIGN KEY             (user_id, destination_pocket_id) REFERENCES pockets(entity_id, id),
   FOREIGN KEY             (user_id) REFERENCES users(id)
 );
 
