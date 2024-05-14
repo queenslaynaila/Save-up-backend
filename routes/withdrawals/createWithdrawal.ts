bd5123f96b319ea9b692120600b6d00d353be50b
queenslaynaila <queenslayjema@gmail.com>
2024-05-14 10:24:18 +0300

    Fix withdrawal

--- routes/withdrawals/createWithdrawal.ts
+++ routes/withdrawals/createWithdrawal.ts
@@ -6,37 +6,7 @@ import { WithdrawalRequest,  WithdrawalRequestInterface } from './types';
 import { MessageInterface } from '../../globalTypes/index';
 
 const SQL_CREATE_WITHDRAWAL = sql<WithdrawalRequest, Record<string, never>>(`
-  INSERT INTO withdrawals (pocket_id, id, user_id, entity_id, amount)
-  SELECT 
-    :pocket_id, 
-    COALESCE((SELECT MAX(id) + 1 FROM withdrawals WHERE pocket_id = :pocket_id), 1), 
-    :user_id, 
-    :entity_id,
-    :amount
-  FROM (
-    SELECT cumulative_amount
-    FROM transaction_logs
-    WHERE pocket_id = :pocket_id
-    ORDER BY created_at DESC
-    LIMIT 1
-  ) AS t
-  CROSS JOIN (
-    SELECT pocket_type, target_at, name
-    FROM pockets
-    WHERE id = :pocket_id
-  ) AS p
-  WHERE 
-    (
-      p.pocket_type = 'Standard Pocket' 
-      AND t.cumulative_amount >= :amount
-    )
-    OR (
-      p.pocket_type = 'Locked Pocket' 
-      AND t.cumulative_amount >= :amount 
-      AND p.target_at <= NOW()
-    )
-  RETURNING *;
-
+  SELECT withdraw_savings(:pocket_id, :user_id, :amount, :entity_id);
 `); 
 
 export default (router: Router) => {
@@ -47,11 +17,10 @@ export default (router: Router) => {
       const { pocket_id, amount} = req.body
       const user_id = req.user!.id
       await SQL_CREATE_WITHDRAWAL({ pocket_id, user_id, amount, entity_id: user_id }).one(
-        new HttpError(400, `Insufficient funds to make withdrawal of ${amount.toFixed(2)}` )
+        new HttpError(400, `Insufficient funds to make withdrawal of ${amount.toFixed(2)} or target date not met for withdrawal.` )
       );
       return res.json({
         message: `Withdrawal of amount KES ${amount.toFixed(2)} successful!`
       });     
     });
 };
-`1`
