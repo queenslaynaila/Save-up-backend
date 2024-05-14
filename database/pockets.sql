290eb00de98f74f21dcf31ba446d0bd87afceb0a
queenslaynaila <queenslayjema@gmail.com>
2024-05-14 11:23:30 +0300

    Refactor: move interest earned from pockets

--- database/pockets.sql
+++ database/pockets.sql
@@ -18,7 +18,6 @@ CREATE TABLE IF NOT EXISTS pockets (
   pocket_type             enum_pocket_types NOT NULL DEFAULT 'Standard Pocket',
   reminder_count          INT NOT NULL DEFAULT 0,
   last_reminder_sent_at   TIMESTAMP WITH TIME ZONE,
-  interest_earned         NUMERIC(30, 2) NOT NULL DEFAULT 0,
   created_at              TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
   updated_at              TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
   completed_at            TIMESTAMP WITH TIME ZONE, 
@@ -58,48 +57,3 @@ AFTER INSERT ON savings
 FOR EACH ROW
 EXECUTE FUNCTION update_pockets_status();
 
---Trigger:Calculate Interest earned by a pocket
-
-CREATE OR REPLACE FUNCTION compute_interest_earned
-RETURNS TRIGGER AS $$
-DECLARE
-      total_savings NUMERIC(30, 2);
-      pocket_rate   NUMERIC(3, 2);
-      pocket_type   enum_pocket_types;
-BEGIN
-   --Retrieve pocket type for given pocket
-    SELECT pocket_type INTO pocket_type
-    FROM pockets
-    WHERE id = NEW.pocket_id;
-
-   --Fetch rate for the pocket type
-    SELECT rate INTO pocket_rate
-    FROM interest_rates
-    WHERE pocket_type = pocket_type;
- 
-   --Compute total savings for the ggiven pocket
-    SELECT COALESCE(SUM(amount), 0) INTO total_savings
-    FROM savings
-    WHERE user_id = NEW.user_id AND saving_id = NEW.saving_id;
-
-    -- Fetch the timestamp of the last interest calculation
-    SELECT MAX(created_at) INTO last_interest_calculation
-    FROM transaction_logs
-    WHERE user_id = NEW.user_id AND pocket_id = NEW.pocket_id AND transaction_type = SAVINGS;
-    days_elapsed = DATE_PART('day', NOW() - last_interest_calculation);
- 
-    --Calculate interest earned aad update
-     NEW.interest_earned = (total_savings * pocket_rate/100 * days_elapsed) / (* 365); 
-
-    UPDATE pockets
-    SET interest_earned = NEW.interest_earned
-    WHERE id = NEW.pocket_id;
-
-    RETURN NEW;
-END
-$$ LANGUAGE plgpgsql
-
-CREATE TRIGGER enforce_compute_interest_earned
-AFTER INSERT ON savings OR INSERT ON external_savings
-FOR EACH ROW
-EXECUTE FUNCTION compute_interest_earned();
