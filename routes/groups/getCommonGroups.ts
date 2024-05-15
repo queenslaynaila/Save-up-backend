9ac2c0248b1eba0fdcf4dd6b4ad22dc657a6cabf
queenslaynaila <queenslayjema@gmail.com>
2024-05-15 12:38:24 +0300

    Fix: get common groups

--- routes/groups/getCommonGroups.ts
+++ routes/groups/getCommonGroups.ts
@@ -5,16 +5,16 @@ import { IdParamInterface } from '../../globalTypes/index';
 import { CommonGroupInterface, SharedGroupInterface  } from './types';
 
 const SQL_GET_COMMON_GROUPS = sql<SharedGroupInterface, CommonGroupInterface>(`
-  SELECT g.id AS group_id, g.group_name, g.description
-  FROM user_groups ug1
-  INNER JOIN user_groups ug2 ON ug1.group_id = ug2.group_id
-  INNER JOIN groups g ON ug1.group_id = g.id
-  WHERE ug1.user_id = :logged_in_user_id 
-  AND ug2.user_id = :user_id2
+  SELECT g.id AS group_id, g.group_name, g.description, g.created_at
+  FROM groups g
+  INNER JOIN user_groups ug1 ON g.id = ug1.group_id  
+  INNER JOIN user_groups ug2 ON g.id = ug2.group_id  
+  WHERE ug1.user_id = :logged_in_user_id AND ug2.user_id = :user_id
+  AND ug1.left_at IS NULL AND ug2.left_at IS NULL;
 `);
 
 export default (router: Router) => {
-  router.get<IdParamInterface, CommonGroupInterface[], Record<string,never>, Record<string,never>>(
+  router.get<IdParamInterface, CommonGroupInterface[],Record<string,never>, Record<string,never>>(
     '/common-groups/:id',
     authMiddleware(),
     async (req, res) => {
