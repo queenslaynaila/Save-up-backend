da4f1276ef5a3ec1c1857f995e83c82e31ee6baf
queenslaynaila <queenslayjema@gmail.com>
2024-05-16 14:56:15 +0300

    Feat:replace trigger with function

--- database/savings.sql
+++ database/savings.sql
@@ -12,8 +12,7 @@ CREATE TABLE IF NOT EXISTS savings (
                                        -- For group pockets, it represents the group member saving the money thus diff from entity_id. 
   amount                NUMERIC(30, 2) NOT NULL,
   created_at            TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
-  PRIMARY KEY           (pocket_id, id), 
-
+  PRIMARY KEY           (pocket_id, id)
 );
 
 GRANT INSERT, SELECT ON savings TO app_user;
@@ -23,6 +22,34 @@ SELECT create_distributed_table('savings', 'pocket_id');
 ALTER TABLE savings
 ADD CONSTRAINT fk_pocket_id
 FOREIGN KEY (entity_id, pocket_id) REFERENCES pockets (entity_id, id);
+
+CREATE OR REPLACE FUNCTION create_saving(user_id_arg INT, pocket_id_arg INT, saving_amount_arg NUMERIC, entity_id_arg INT)
+RETURNS TABLE (pocket_name TEXT) AS $$
+DECLARE
+    total_savings NUMERIC;
+BEGIN 
+    INSERT INTO savings (id, pocket_id, user_id, entity_id, amount)
+    SELECT COALESCE(MAX(id), 0) + 1, pocket_id_arg, user_id_arg, entity_id_arg, saving_amount_arg
+    FROM savings 
+    WHERE pocket_id = pocket_id_arg; 
+    
+    SELECT COALESCE(cumulative_amount, 0) INTO total_savings
+    FROM transaction_logs
+    WHERE pocket_id = pocket_id_arg
+    ORDER BY transaction_id DESC
+    LIMIT 1;
+	
+    IF total_savings >= (SELECT target_amount FROM pockets WHERE entity_id = entity_id_arg AND id = pocket_id_arg) THEN
+        UPDATE pockets
+        SET status = 'Completed',
+            completed_date = NOW()
+        WHERE id = pocket_id_arg;
+    END IF;
+
+    RETURN QUERY SELECT name FROM pockets WHERE id = pocket_id_arg;
+END;
+$$ LANGUAGE plpgsql;
+
 
 CREATE TABLE IF NOT EXISTS donors (
   entity_id            INT NOT NULL PRIMARY KEY,
--- routes/savings/createSaving.ts
+++ routes/savings/createSaving.ts
@@ -6,22 +6,11 @@ import { validateRequest } from '../../middleware/validationMiddleware';
 import { CreateSavingInterface, validateSavingCreationSchema } from './types';
 
 interface SavingInterface {
-  amount: number;
-  name: string;
+  create_saving: string;
 }
 
 const SQL_CREATE_SAVING = sql<CreateSavingInterface, SavingInterface>(`
-  INSERT INTO savings (id, pocket_id, user_id, entity_id, amount)
-  VALUES (
-      (SELECT COALESCE(MAX(id), 0) + 1 FROM savings WHERE pocket_id = :pocket_id),
-      :pocket_id,
-      :user_id,
-      :entity_id,
-      :amount
-  )
-  RETURNING amount, (
-      SELECT name FROM pockets WHERE id = :pocket_id
-  ) AS name;
+  SELECT create_saving(:user_id, :pocket_id, :amount, :entity_id )
 `);
 
 export default (router: Router) => {
@@ -31,12 +20,11 @@ export default (router: Router) => {
     validateRequest(validateSavingCreationSchema),
     async (req, res) => {
       const user_id= req.user!.id
-      const { pocket_id, amount  } = req.body;
+      const { pocket_id, amount } = req.body;
       const savingResult = await SQL_CREATE_SAVING({ user_id, pocket_id, amount, entity_id:user_id })
         .one(new HttpError(400, 'Unable to complete the request'));
-      const goalName = savingResult.name
-      const amountPaid = savingResult.amount
-      return res.json({ message: `Your saving of KES ${amountPaid.toFixed(2)} to ${goalName} was successful!` });
+      const goalName = savingResult.create_saving
+      return res.json({ message: `Your saving of KES ${amount.toFixed(2)} to ${goalName} was successful!` });
     });
 };
 
