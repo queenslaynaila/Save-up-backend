fd4217c73efbd63d9fa8b8300a0863950e141a8e
queenslaynaila <queenslayjema@gmail.com>
2024-05-16 10:44:14 +0300

    Refactor:change saving ditributed column topockets

--- database/savings.sql
+++ database/savings.sql
@@ -1,4 +1,9 @@
--- Table: Savings
+-- Create the 'savings' table without the distributed 
+-- pockets foreign key constraint as citus 
+-- doesnt allow a normal psql table to ref a distributed table
+-- Distribute table by user id
+-- Use alter command to add the fk constaint thereby bypasing citus
+
 CREATE TABLE IF NOT EXISTS savings (
   id                    INT NOT NULL,
   entity_id             INT NOT NULL, -- Entity ID of the user or group owning the savings.
@@ -8,13 +13,16 @@ CREATE TABLE IF NOT EXISTS savings (
   amount                NUMERIC(30, 2) NOT NULL,
   created_at            TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
   PRIMARY KEY           (pocket_id, id), 
-  FOREIGN KEY           (entity_id, pocket_id) REFERENCES pockets (entity_id, id),
-  FOREIGN KEY           (entity_id) REFERENCES entities(id),
-  FOREIGN KEY           (user_id) REFERENCES users(id)
+  FOREIGN KEY           (entity_id) REFERENCES entities(id)
 );
 
+GRANT INSERT, SELECT ON savings TO app_user;
 CREATE INDEX idx_savings_by_pocket_id ON savings(pocket_id);
 SELECT create_distributed_table('savings', 'pocket_id');
+
+ALTER TABLE savings
+ADD CONSTRAINT fk_pocket_id
+FOREIGN KEY (entity_id, pocket_id) REFERENCES pockets (entity_id, id);
 
 -- Table: External Savings 
 -- Stores contributions/ donations made by non members of the app towards an individual or group goal.
