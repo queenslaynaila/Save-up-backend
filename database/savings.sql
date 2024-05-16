1a51b8de0f8ddab5e9050839c327dd2e62e73f52
queenslaynaila <queenslayjema@gmail.com>
2024-05-16 10:55:28 +0300

    Feat: replace donor id from donors with entities

--- database/savings.sql
+++ database/savings.sql
@@ -13,7 +13,7 @@ CREATE TABLE IF NOT EXISTS savings (
   amount                NUMERIC(30, 2) NOT NULL,
   created_at            TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
   PRIMARY KEY           (pocket_id, id), 
-  FOREIGN KEY           (entity_id) REFERENCES entities(id)
+
 );
 
 GRANT INSERT, SELECT ON savings TO app_user;
@@ -24,34 +24,37 @@ ALTER TABLE savings
 ADD CONSTRAINT fk_pocket_id
 FOREIGN KEY (entity_id, pocket_id) REFERENCES pockets (entity_id, id);
 
--- Table: External Savings 
+CREATE TABLE IF NOT EXISTS donors (
+  entity_id            INT NOT NULL PRIMARY KEY,
+  full_name            TEXT NOT NULL,
+  phone_number         TEXT NOT NULL UNIQUE,
+  created_at           TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
+  updated_at           TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
+  FOREIGN KEY          (entity_id) REFERENCES entities(id)
+);
+
 -- Stores contributions/ donations made by non members of the app towards an individual or group goal.
 -- Once a non member makes a donations we create a donor account for him in donors table 
 -- so in case they donate again he doesnt have to re enter their details
 CREATE TABLE IF NOT EXISTS external_savings (
   id                    INT NOT NULL,
+  entity_id             INT NOT NULL, -- Entity ID of the donor owning the savings.
   pocket_id             INT NOT NULL,
-  donor_id              INT NOT NULL,
   amount                NUMERIC(30, 2) NOT NULL,
   show_donor_details    BOOLEAN NOT NULL DEFAULT TRUE,
   created_at            TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
   PRIMARY KEY           (pocket_id, id), 
-  FOREIGN KEY           (donor_id) REFERENCES donors(donor_id)
+  FOREIGN KEY           (entity_id) REFERENCES entities(id)
 );
 
 CREATE INDEX idx_external_savings_by_pocket_id ON external_savings(pocket_id);
 SELECT create_distributed_table('external_savings', 'pocket_id');
 
----Donors
-CREATE TABLE IF NOT EXISTS donors (
-  donor_id             INT NOT NULL PRIMARY KEY,
-  full_name            TEXT NOT NULL,
-  phone_number         TEXT NOT NULL UNIQUE,
-  created_at           TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
-  updated_at           TIMESTAMP WITH TIME ZONE DEFAULT NOW()
-);
+ALTER TABLE external_savings
+ADD CONSTRAINT fk_exsavings_pocket_id
+FOREIGN KEY (entity_id, pocket_id) REFERENCES pockets (entity_id, id);
 
-CREATE OR REPLACE FUNCTION create_external_savings(pocket_id INT, amount NUMERIC(30, 2), show_donor_details BOOLEAN, full_name, phone_number)
+CREATE OR REPLACE FUNCTION create_external_savings(pocket_id INT, amount NUMERIC(30, 2), show_donor_details BOOLEAN, full_name TEXT, phone_number TEXT)
 RETURNS VOID AS $$
 DECLARE
    entity_id INTEGER;
@@ -70,10 +73,10 @@ BEGIN
         VALUES (donor_id, full_name, phone_number);
     END IF;
 
-    INSERT INTO external_savings (id, pocket_id, donor_id, amount, show_donor_details)
-    VALUES (COALESCE((SELECT MAX(id) + 1 FROM external_savings WHERE pocket_id = pocket_id), 1), 
-            pocket_id, 
+    INSERT INTO external_savings (id, entity_id, pocket_id, amount, show_donor_details)
+    VALUES (COALESCE((SELECT MAX(id) + 1 FROM external_savings WHERE pocket_id = pocket_id), 1),
             donor_id, 
+            pocket_id, 
             amount, 
             show_donor_details
           );
