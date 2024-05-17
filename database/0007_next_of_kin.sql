68a7260fe9abf5219235b470075d999d49553258
queenslaynaila <queenslayjema@gmail.com>
2024-05-17 15:53:38 +0300

    Refcator:file name

---
+++ database/0007_next_of_kin.sql
@@ -0,0 +1,27 @@
+CREATE TYPE enum_relationship AS ENUM (
+  'Parent', 
+  'Spouse', 
+  'Sibling', 
+  'Child',
+  'Relative', 
+  'Lawyer', 
+  'Friend'
+);
+
+CREATE TABLE IF NOT EXISTS next_of_kins (
+  user_id                INT NOT NULL,
+  ex_id                  INT NOT NULL,
+  full_name              TEXT NOT NULL,
+  relationship           enum_relationship NOT NULL,
+  email                  TEXT NOT NULL,
+  phone_number           TEXT NOT NULL,
+  created_at             TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+  deleted_at             TIMESTAMP WITH TIME ZONE,
+  PRIMARY KEY            (user_id, id),
+  FOREIGN KEY            (user_id) REFERENCES users(id)
+);
+
+--Enforce uniqueness of user IDs for non-deleted records, ensuring each user can have only one valid next of kin at a time
+CREATE UNIQUE INDEX next_of_kins_user_id_key ON next_of_kins(user_id) WHERE deleted_at IS NULL;
+GRANT INSERT, SELECT ON user_contact_details TO app_user;
+SELECT create_distributed_table ('next_of_kins', 'user_id');
