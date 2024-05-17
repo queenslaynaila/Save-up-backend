b4e03338b03e19ceb36482e363e5d1344cf47aa7
queenslaynaila <queenslayjema@gmail.com>
2024-05-17 16:08:57 +0300

    Feat:  groups table

---
+++ database/00011_groups.sql
@@ -0,0 +1,12 @@
+CREATE TABLE IF NOT EXISTS groups (
+  id            SERIAL PRIMARY KEY,
+  name          TEXT NOT NULL,
+  created_by    INT NOT NULL,
+  created_at    TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+  updated_at    TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+  deleted_at    TIMESTAMP WITH TIME ZONE,
+  FOREIGN KEY   (id) REFERENCES entities(id)
+);
+
+GRANT INSERT, SELECT, UPDATE ON groups TO app_user;
+SELECT create_distributed_table('groups', 'id');
