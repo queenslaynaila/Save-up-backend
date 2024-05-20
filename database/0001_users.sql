5369a792e7fea6c43665a4cc5483c343abae4fa5
queenslaynaila <queenslayjema@gmail.com>
2024-05-20 11:28:34 +0300

    Refactor:clear unnecsary comments

--- database/0001_users.sql
+++ database/0001_users.sql
@@ -1,41 +1,37 @@
 -- Custom enums for user-related data
 CREATE TYPE enum_entity_type AS ENUM ('User', 'Group', 'Donor');
-CREATE TYPE enum_id_type AS ENUM ('National ID', 'Passport');
+CREATE TYPE enum_id_type AS ENUM ('National ID', 'Passport ID');
 CREATE TYPE enum_user_role AS ENUM ('Admin', 'User', 'Moderator');
 CREATE TYPE enum_gender AS ENUM ('Male', 'Female');
 
--- Entity Tables: Categorises all entities in the system whether User, Group or Donor
 CREATE TABLE IF NOT EXISTS entities (
   id              SERIAL PRIMARY KEY,
-  entity_type     enum_entity_type NOT NULL 
-  created_at      TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+  entity_type     enum_entity_type NOT NULL, 
+  created_at      TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()
 );
 GRANT INSERT, SELECT ON entities TO app_user;  
 SELECT create_reference_table('entities');
 
---- User Contacts: Stores contact details and identification details
 CREATE TABLE IF NOT EXISTS user_contact_details (
   id              INT PRIMARY KEY,  
   id_type         enum_id_type NOT NULL DEFAULT 'National ID',
-  id_number       TEXT NOT NULL UNIQUE,
+  id_number       TEXT NOT NULL UNIQUE CHECK (id_number ~ '^[0-9]+$'),
   phone_number    TEXT NOT NULL UNIQUE,
   created_at      TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
-  FOREIGN KEY     (id) REFERENCES entities(id),
-  CONSTRAINT      id_number_format_check CHECK (id_number ~ '^[0-9]+$')
+  FOREIGN KEY     (id) REFERENCES entities(id)
 );
 GRANT INSERT, SELECT ON user_contact_details TO app_user;
 CREATE INDEX user_contacts_phone_number_idx ON user_contact_details(phone_number);
 
--- Users Table: Store general user details
+-- Ref entities instead as a distributed table can only reference another colocated distributed table or a reference table
 CREATE TABLE IF NOT EXISTS users (
   id              INT NOT NULL PRIMARY KEY,  
   full_name       TEXT NOT NULL,
   role            enum_user_role NOT NULL DEFAULT 'User',
   gender          enum_gender,
-  pin             TEXT NOT NULL,
+  pin             TEXT NOT NULL CHECK (pin ~ '^[0-9]{4}$'),
   created_at      TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
-  FOREIGN KEY     (id) REFERENCES user_contact_details(id),
-  CONSTRAINT      pin_length_format_check CHECK (id_number ~ '^[0-9]+$')
+  FOREIGN KEY     (id) REFERENCES entities(id)
 );
 GRANT INSERT, SELECT, UPDATE ON user_contact_details TO app_user;
 SELECT create_distributed_table('users', 'id');
