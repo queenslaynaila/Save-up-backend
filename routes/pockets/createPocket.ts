96a3aaed1b1f7929cbfdf05f0e9110bda4d06b8e
queenslaynaila <queenslayjema@gmail.com>
2024-05-20 16:03:45 +0300

    Fic:create pocket

--- routes/pockets/createPocket.ts
+++ routes/pockets/createPocket.ts
@@ -5,12 +5,20 @@ import { validateRequest } from '../../middleware/validationMiddleware';
 import { CreatePocketInterface, PocketInterface, createPocketSchema } from './types';
 
 const SQL_CREATE_POCKET = sql<CreatePocketInterface, PocketInterface>(`
-  INSERT INTO pockets (id, entity_id, category_id, name, target_amount, priority, target_at, pocket_type)
-  SELECT COALESCE(MAX(id), 0) + 1, 
-        :entity_id, :category_id, :name, :target_amount, :priority, :target_at, :pocket_type
-  FROM pockets 
-  WHERE entity_id = :entity_id
-  RETURNING id, entity_id, name, category_id, target_amount, priority, target_at, created_at
+INSERT INTO pockets (entity_id, xid, category_id, name, priority, pocket_type, target_amount, target_at)
+SELECT :entity_id,
+       COALESCE(MAX(xid), 0) + 1,
+       :category_id,
+       :name,
+       :priority,
+       :pocket_type,
+       :target_amount,
+       :target_at
+FROM pockets 
+WHERE entity_id = :entity_id
+GROUP BY entity_id
+RETURNING xid, entity_id, name, category_id, target_amount, priority, target_at, created_at;
+
 `);
 
 export default (router: Router) => {
@@ -21,14 +29,15 @@ export default (router: Router) => {
     async (req, res) => {
       const { category_id, name, target_amount, priority, target_at, pocket_type } = req.body;
       const entity_id = req.body.entity_id ? req.body.entity_id : req.user!.id;
+      console.log(entity_id)
       const newPocket = await SQL_CREATE_POCKET({
         entity_id,
         category_id,
         name,
-        target_amount,
         priority,
+        pocket_type,
+        target_amount,
         target_at,
-        pocket_type
       }).one();
       return res.json(newPocket);
     });
