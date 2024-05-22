0b5150ec0646533d34e86d2d2506ef2aa06db9f0
queenslaynaila <queenslayjema@gmail.com>
2024-05-22 09:02:17 +0300

    Refactor:id to xid

--- routes/pockets/createPocket.ts
+++ routes/pockets/createPocket.ts
@@ -7,17 +7,17 @@ import { CreatePocketInterface, PocketInterface, createPocketSchema } from './ty
 const SQL_CREATE_POCKET = sql<CreatePocketInterface, PocketInterface>(`
   INSERT INTO pockets (entity_id, xid, category_id, name, priority, pocket_type, target_amount, target_at)
   SELECT :entity_id,
-        COALESCE(MAX(xid), 0) + 1,
-        :category_id,
-        :name,
-        :priority,
-        :pocket_type,
-        :target_amount,
-        :target_at
+          COALESCE(MAX(xid), 0) + 1,
+          :category_id,
+          :name,
+          :priority,
+          :pocket_type,
+          :target_amount,
+          :target_at
   FROM pockets 
   WHERE entity_id = :entity_id
   GROUP BY entity_id
-  RETURNING xid, entity_id, name, category_id, target_amount, priority, target_at, created_at;
+  RETURNING xid, entity_id, name, category_id, target_amount, priority, status, target_at, created_at;
 `);
 
 export default (router: Router) => {
@@ -28,7 +28,6 @@ export default (router: Router) => {
     async (req, res) => {
       const { category_id, name, target_amount, priority, target_at, pocket_type } = req.body;
       const entity_id = req.body.entity_id ? req.body.entity_id : req.user!.id;
-      console.log(entity_id)
       const newPocket = await SQL_CREATE_POCKET({
         entity_id,
         category_id,
