e0b7c44b88923a55495477a57129ffc796e60bbe
queenslaynaila <queenslayjema@gmail.com>
2024-05-23 13:46:57 +0300

    Fix: remove authorization from donations

--- routes/externalSaving/createExSaving.ts
+++ routes/externalSaving/createExSaving.ts
@@ -1,23 +1,26 @@
 import { Router } from 'express';
 import { sql } from '../../db';
-import authMiddleware from '../../middleware/authorization';
 import { HttpError } from '../../middleware/errorMiddleware';
-import { validateRequest } from '../../middleware/validationMiddleware';
-import {  ExternalSavingInterface, externalSavingSchema } from './types';
+//import { validateRequest } from '../../middleware/validationMiddleware';
+import {  ExternalSavingInterface} from './types';
 import { MessageInterface } from '../../globalTypes';
 
 const SQL_CREATE_SAVING = sql<ExternalSavingInterface, Record<string,never>>(`
-  SELECT create_external_savings( :pocket_id, :amount, :show_donor_details)
+  SELECT create_external_savings( 
+    :pocket_id, 
+    :amount, 
+    :show_donor_details, 
+    :full_name,
+    :phone_number
+  )
 `);
 
 export default (router: Router) => {
   router.post<Record<string,never>, MessageInterface, ExternalSavingInterface, Record<string,never>>(
     '/', 
-    authMiddleware(),
-    validateRequest(externalSavingSchema),
     async (req, res) => {
-      const { pocket_id, amount, show_donor_details } = req.body;
-      const savingResult = await SQL_CREATE_SAVING({ pocket_id, amount, show_donor_details })
+      const { pocket_id, amount, show_donor_details, full_name, phone_number } = req.body;
+      const savingResult = await SQL_CREATE_SAVING({ pocket_id, amount, show_donor_details, full_name, phone_number })
         .one(new HttpError(400, 'Unable to complete the request'));
       const amountPaid = savingResult.amount
       return res.json({ message: `Your saving of KES ${amountPaid}  was successful!` });
