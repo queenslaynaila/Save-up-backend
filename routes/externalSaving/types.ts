331dfa74e96487909a986abb9fcb5c6f6479ff28
queenslaynaila <queenslayjema@gmail.com>
2024-05-23 13:47:10 +0300

    Fix: add more donor details

--- routes/externalSaving/types.ts
+++ routes/externalSaving/types.ts
@@ -3,7 +3,9 @@ import { z } from 'zod';
 export const externalSavingSchema = z.object({
   pocket_id: z.number().int(),
   amount: z.number(), 
-  show_donor_details:z.boolean()
+  show_donor_details:z.boolean(),
+  full_name: z.string(),
+  phone_number:z.string()
 })
 
 export type ExternalSavingInterface = z.infer<typeof externalSavingSchema>
