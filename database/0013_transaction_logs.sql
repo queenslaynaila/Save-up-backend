5aa341f42293e6da52c0f74cd71a58e5903d01db
queenslaynaila <queenslayjema@gmail.com>
2024-05-23 13:45:44 +0300

    Fix: missing comma

--- database/0013_transaction_logs.sql
+++ database/0013_transaction_logs.sql
@@ -10,9 +10,9 @@ CREATE TABLE IF NOT EXISTS transaction_logs (
   amount                  NUMERIC(30, 2) NOT NULL CHECK (amount > 0),
   reference_no            TEXT NOT NULL, -- The bank or mobile transaction no
   status                  TEXT NOT NULL DEFAULT 'Pending', -- The bank or mobile transaction no
-  cumulative_amount       NUMERIC(30, 2) NOT NULL CHECK (cumulative_amount >= 0) --curent accoyt balance,
+  cumulative_amount       NUMERIC(30, 2) NOT NULL CHECK (cumulative_amount >= 0), --curent accoyt balance,
   created_at              TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
-  PRIMARY KEY             (entity_id, pocket_id, xid),
+  PRIMARY KEY             (entity_id, pocket_id, xid)
   FOREIGN KEY             (entity_id, pocket_id) REFERENCES pockets (entity_id, xid)
 );
 
