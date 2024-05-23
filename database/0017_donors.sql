f6051b7da10466e8724e717e13ad104e9cad6f69
queenslaynaila <queenslayjema@gmail.com>
2024-05-23 13:46:37 +0300

    Fix: spacing

--- database/0017_donors.sql
+++ database/0017_donors.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS donors (
-  id            INT NOT NULL PRIMARY KEY,
+  id                   INT NOT NULL PRIMARY KEY,
   full_name            TEXT NOT NULL,
   phone_number         TEXT NOT NULL UNIQUE,
   created_at           TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
