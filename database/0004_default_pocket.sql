792690f084809a46a4c4af16279fcd656083aac6
queenslaynaila <queenslayjema@gmail.com>
2024-05-27 08:19:42 +0300

    Dix primary keys

--- database/0004_default_pocket.sql
+++ database/0004_default_pocket.sql
@@ -1,15 +1,8 @@
--- Set Citus to run commands sequentially within this transaction
--- to avoid parallel DDL error due to default pkts -pkts - category rlshp
-SET LOCAL citus.multi_shard_modify_mode TO 'sequential';
-
 CREATE TABLE IF NOT EXISTS default_pockets ( 
   entity_id               INT PRIMARY KEY, 
   pocket_id               INT NOT NULL,
   FOREIGN KEY             (entity_id, pocket_id) REFERENCES pockets(entity_id, xid)
 );
-
---Reset Citus to its default behaviour
-RESET citus.multi_shard_modify_mode;
 
 GRANT INSERT, SELECT ON pockets TO app_user;
 SELECT create_distributed_table('default_pockets', 'entity_id');
