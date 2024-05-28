620d4853b1ac2cbc38fcc82939c3c296a1baa6cd
queenslaynaila <queenslayjema@gmail.com>
2024-05-28 11:43:10 +0300

    Fix: enum nae

--- database/0002_categories.sql
+++ database/0002_categories.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS categories (
   id            SERIAL PRIMARY KEY,
   name          TEXT NOT NULL,
   description   TEXT NOT NULL,
-  image_url     TEXT NOT NULL,
+  image_url     TEXT,
   created_at    TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
   deleted_at    TIMESTAMP WITH TIME ZONE
 );
