947d0555c156c0e06286d2cdf55038bc9db96406
queenslaynaila <queenslayjema@gmail.com>
2024-05-28 12:03:55 +0300

    Fix: referencing

--- database/0018_external_savings.sql
+++ database/0018_external_savings.sql
@@ -1,13 +1,13 @@
 CREATE TABLE IF NOT EXISTS external_savings (
-  id                    INT NOT NULL, 
-  pocket_id             INT NOT NULL,
+  entity_id             INT NOT NULL,
+  xid                   INT NOT NULL,
+  pocket_id             INT NOT NULL, 
   amount                NUMERIC(30, 2) NOT NULL,
   show_details          BOOLEAN NOT NULL DEFAULT TRUE,
   created_at            TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
-  PRIMARY KEY           (pocket_id, id), 
-  FOREIGN KEY           (id) REFERENCES entities(id),
-  FOREIGN KEY           (id, pocket_id) REFERENCES pockets (entity_id, xid)
+  PRIMARY KEY           (entity_id, xid),
+  FOREIGN KEY           (entity_id, pocket_id) REFERENCES pockets (entity_id, xid)
 );
 
 GRANT INSERT, SELECT ON external_savings TO app_user;
-SELECT create_distributed_table('external_savings', 'pocket_id');
+SELECT create_distributed_table('external_savings', 'entity_id');
