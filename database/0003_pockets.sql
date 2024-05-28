64277052d56363c73dd1b0153c54a36e39d10a97
queenslaynaila <queenslayjema@gmail.com>
2024-05-28 13:58:10 +0300

    Feat: create pockets default pcokets and pocket reminders in asingle transation

--- database/0003_pockets.sql
+++ database/0003_pockets.sql
@@ -35,3 +35,14 @@ CREATE TABLE IF NOT EXISTS default_pockets (
 
 GRANT INSERT, SELECT ON pockets TO app_user;
 SELECT create_distributed_table('default_pockets', 'entity_id');
+
+CREATE TABLE IF NOT EXISTS pocket_reminders ( 
+  entity_id               INT PRIMARY KEY, 
+  pocket_id               INT NOT NULL,
+  reason                  TEXT,
+  created_at              TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+  FOREIGN KEY             (entity_id, pocket_id) REFERENCES pockets(entity_id, xid)
+);
+
+GRANT INSERT, SELECT, UPDATE ON pockets TO app_user;
+SELECT create_distributed_table('pocket_reminders', 'entity_id');
