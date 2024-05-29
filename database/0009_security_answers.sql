1a55d6555679dddfe7c5589ad23d101d7634d300
queenslaynaila <queenslayjema@gmail.com>
2024-05-29 09:01:35 +0300

    Refactor: security answer creation in separate transactions

--- database/0009_security_answers.sql
+++ database/0009_security_answers.sql
@@ -1,10 +1,18 @@
+-- Step 1: Create the table with a foreign key reference to a reference table.
+-- Isolate the step to ensure that the table is created before any distribution logic is applied.
+-- As Citus can't enforce foreign key constraints from local to both a  distributed and reference tables within the same transaction.
+-- It can only enforce one of this at a time
 CREATE TABLE IF NOT EXISTS security_answers (
   user_id       INT NOT NULL,
   question_id   INT NOT NULL, 
   answer        TEXT NOT NULL,
   created_at    TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
   PRIMARY KEY   (user_id, question_id),
-  FOREIGN KEY   (question_id) REFERENCES security_questions(id),
-  FOREIGN KEY   (user_id) REFERENCES users(id)
+  FOREIGN KEY   (question_id) REFERENCES security_questions(id)
 );
+
 SELECT create_distributed_table('security_answers', 'user_id');
+
+ALTER TABLE security_answers
+ADD CONSTRAINT fk_users
+FOREIGN KEY   (user_id) REFERENCES users(id);
