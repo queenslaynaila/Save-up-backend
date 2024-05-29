00a30a6ea0a1bae0cdde68c2d01350263ca1b45f
queenslaynaila <queenslayjema@gmail.com>
2024-05-29 13:02:52 +0300

    Fix: transfer table

--- database/0020_transfers.sql
+++ database/0020_transfers.sql
@@ -1,14 +1,26 @@
+-- Step 1: Create the table with a foreign key reference to a reference table only.
+-- Isolate the step to ensure that the table is created before any distribution logic is applied.
+-- On creation the table is a local table and  As Citus can't enforce foreign key constraints from local to 
+-- both a  distributed and reference tables within the same transaction.
+-- Add the other foreign key in a separate transaction after distribution.
+
 CREATE TABLE IF NOT EXISTS transfers (
-  user_id                 INT NOT NULL,
-  id                      INT NOT NULL,
+  entity_id               INT NOT NULL,  --owner of pkt
+  xid                     INT NOT NULL,
+  user_id                 INT NOT NULL,  --user doing withdrawal
   source_pocket_id        INT NOT NULL,
   destination_pocket_id   INT NOT NULL,
   amount                  NUMERIC(30, 2) NOT NULL,
   created_at              TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
-  FOREIGN KEY             (user_id, source_pocket_id) REFERENCES pockets(entity_id, xid),
-  FOREIGN KEY             (user_id, destination_pocket_id) REFERENCES pockets(entity_id, xid),
-  FOREIGN KEY             (user_id) REFERENCES users(id)
+  PRIMARY KEY             (entity_id, xid),
+  FOREIGN KEY             (entity_id, source_pocket_id) REFERENCES pockets(entity_id, xid),
+  FOREIGN KEY             (entity_id, destination_pocket_id) REFERENCES pockets(entity_id, xid)
 );
 
 GRANT INSERT, SELECT ON transfers TO app_user;
 SELECT create_distributed_table('transfers', 'entity_id');
+
+
+ALTER TABLE  transfers
+ADD CONSTRAINT fk_users
+FOREIGN KEY (user_id) REFERENCES entities(id);
