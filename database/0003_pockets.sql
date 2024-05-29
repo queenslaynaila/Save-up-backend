8e804482d4b535c8add22b03f13882913e4a3353
queenslaynaila <queenslayjema@gmail.com>
2024-05-29 09:04:21 +0300

    Refcator: craete pockets and all its related tables in one transcations

--- database/0003_pockets.sql
+++ database/0003_pockets.sql
@@ -2,6 +2,10 @@ CREATE TYPE enum_status AS ENUM ('In Progress', 'Completed');
 CREATE TYPE enum_priority AS ENUM ('High', 'Intermediate', 'Low');
 CREATE TYPE enum_pocket_type AS ENUM ('Standard', 'Locked');
 
+-- As pockets references the reference table to categories create it and all its associated tables in one single transaction 
+-- as when there is a foreign key to a reference table, Citus 
+-- needs to perform all operations over a single connection to ensure consistency
+
 CREATE TABLE IF NOT EXISTS pockets ( 
   entity_id               INT NOT NULL, 
   xid                     INT NOT NULL,
@@ -46,3 +50,17 @@ CREATE TABLE IF NOT EXISTS pocket_reminders (
 
 SELECT create_distributed_table('pocket_reminders', 'entity_id');
 GRANT INSERT, SELECT, UPDATE ON pockets TO app_user;
+
+CREATE TABLE IF NOT EXISTS savings (
+  entity_id             INT NOT NULL, 
+  xid                   INT NOT NULL,
+  pocket_id             INT NOT NULL,
+  user_id               INT NOT NULL, 
+  amount                NUMERIC(30, 2) NOT NULL CHECK (amount > 0),
+  created_at            TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
+  PRIMARY KEY           (entity_id, xid),
+  FOREIGN KEY           (entity_id, pocket_id) REFERENCES pockets (entity_id, xid)
+);
+
+SELECT create_distributed_table('savings', 'entity_id');
+GRANT INSERT, SELECT ON savings TO app_user;
