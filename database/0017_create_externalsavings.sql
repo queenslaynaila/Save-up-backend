36ba347ab31fc1c71f7f2f76a4e2495d9249bd19
queenslaynaila <queenslayjema@gmail.com>
2024-05-29 11:43:40 +0300

    FIX: maontain consistency with variable types int insteda of integer

--- database/0017_create_externalsavings.sql
+++ database/0017_create_externalsavings.sql
@@ -1,18 +1,18 @@
 CREATE OR REPLACE FUNCTION create_external_saving(
+    entity_id        INT,
     p_pocket_id      INT, 
     p_amount         NUMERIC(30, 2), 
     p_show_details   BOOLEAN, 
-    p_full_name      TEXT, 
-    p_phone_number   TEXT
+    p_full_name      TEXT
 )
 RETURNS VOID AS $$
 DECLARE
-    v_entity_id           INTEGER;
-    v_donor_id            INTEGER;
+    v_entity_id           INT;
+    v_donor_id            INT;
     v_current_balance     NUMERIC(30, 2);
     v_reference_no        TEXT;
-    v_new_balance      NUMERIC(30, 2);
-    v_transaction_id      INTEGER;
+    v_new_balance         NUMERIC(30, 2);
+    v_transaction_id      INT;
 BEGIN
     SELECT id INTO v_donor_id
     FROM donors
@@ -42,23 +42,12 @@ BEGIN
         p_show_details
     );
     
-    SELECT 
-        COALESCE(MAX(xid) + 1, 1) AS v_transaction_id,
-        COALESCE((SELECT cumulative_amount
-                FROM transaction_logs
-                WHERE pocket_id = p_pocket_id
-                  AND entity_id = p_entity_id
-                ORDER BY xid DESC
-                LIMIT 1), 0) AS v_current_balance
-        INTO STRICT v_transaction_id, v_current_balance
-    FROM transaction_logs
-    WHERE pocket_id = p_pocket_id
-    AND entity_id = v_donor_id;
+    SELECT * FROM get_transaction_info(p_pocket_id, p_entity_id) 
+    INTO v_transaction_id, v_current_balance;
 
     v_new_balance = v_current_balance + p_amount;
     v_reference_no = 'DONATE' || left(md5(random()::text), 5);
 
-    -- Insert the transaction log
     INSERT INTO transaction_logs (
         xid, 
         pocket_id, 
@@ -84,6 +73,5 @@ $$ LANGUAGE plpgsql;
 
 GRANT EXECUTE ON FUNCTION create_external_saving(INT, NUMERIC, BOOLEAN, TEXT, TEXT) TO app_user;
 SELECT create_distributed_function(
-  'create_external_saving(INT, NUMERIC, BOOLEAN, TEXT, TEXT)', 'p_entity_id',
-   colocate_with := 'external_savings'
+  'create_external_saving(INT, INT, NUMERIC, BOOLEAN, TEXT)', 'p_entity_id'
 );
