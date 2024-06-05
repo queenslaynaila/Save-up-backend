2ef21c84a9eb43b9dfc0f054d1b8f7836dababab
queenslaynaila <queenslayjema@gmail.com>
2024-06-05 12:09:28 +0300

    Fix: withdrwals

--- routes/withdrawals/createWithdrawal.ts
+++ routes/withdrawals/createWithdrawal.ts
@@ -1,26 +1,20 @@
 import { Router } from 'express';
 import { sql } from '../../db';
 import authMiddleware from '../../middleware/authorization';
-import { HttpError } from '../../middleware/errorMiddleware';
 import { WithdrawalRequest,  WithdrawalRequestInterface } from './types';
-import { MessageInterface } from '../../globalTypes/index';
+import { StatusCodeInterface } from '../../globalTypes/index';
 
 const SQL_CREATE_WITHDRAWAL = sql<WithdrawalRequest, Record<string, never>>(`
-  SELECT withdraw_savings(:pocket_id, :user_id, :amount, :entity_id);
+  SELECT withdraw_savings(:entity_id, :pocket_id, :user_id, :amount);
 `); 
 
 export default (router: Router) => {
-  router.post<Record<string,never>, MessageInterface, WithdrawalRequestInterface, Record<string,never>>(
+  router.post<Record<string,never>, StatusCodeInterface, WithdrawalRequestInterface, Record<string,never>>(
     '/', 
     authMiddleware(),
     async (req, res) => {
-      const { pocket_id, amount} = req.body
       const user_id = req.user!.id
-      await SQL_CREATE_WITHDRAWAL({ pocket_id, user_id, amount, entity_id: user_id }).one(
-        new HttpError(400, `Insufficient funds to make withdrawal of ${amount.toFixed(2)} or target date not met for withdrawal.` )
-      );
-      return res.json({
-        message: `Withdrawal of amount KES ${amount.toFixed(2)} successful!`
-      });     
+      await SQL_CREATE_WITHDRAWAL({...req.body, entity_id: user_id, user_id }).exec();
+      res.sendStatus(201);   
     });
 };
