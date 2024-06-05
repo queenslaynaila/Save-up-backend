55ecf743c126c05bf69268152e24b4e79a06d86d
queenslaynaila <queenslayjema@gmail.com>
2024-06-05 14:45:10 +0300

    Fix: security answers

--- routes/securityAnswer/createSecurityAnswer.ts
+++ routes/securityAnswer/createSecurityAnswer.ts
@@ -4,21 +4,20 @@ import { sql } from '../../db';
 import authMiddleware from '../../middleware/authorization';
 import { validateRequest } from '../../middleware/validationMiddleware';
 import { CreateSecurityAnswerInterface, securityAnswerRequestSchema } from './types'
-import {  MessageInterface } from '../../globalTypes/index';
+import {  StatusCodeInterface } from '../../globalTypes/index';
 
 const SQL_CREATE_ANSWER = sql<CreateSecurityAnswerInterface ,Record<string,never>>(`
   SELECT create_answer (:user_id, :question_id, :answer)
 `); 
 
 export default (router: Router) => {
-  router.post<Record<string,never>, MessageInterface, CreateSecurityAnswerInterface , Record<string,never>>(
+  router.post<Record<string,never>, StatusCodeInterface, CreateSecurityAnswerInterface , Record<string,never>>(
     '/', 
     authMiddleware(), 
     validateRequest(securityAnswerRequestSchema),
     async (req, res) => {
-      const user_id = req.user!.id;
       const hashedAnswer = await bcrypt.hash(req.body.answer, 12);
-      await SQL_CREATE_ANSWER({  ...req.body, answer: hashedAnswer, user_id}).exec();
+      await SQL_CREATE_ANSWER({  ...req.body, answer: hashedAnswer, user_id:req.user!.id}).exec();
       res.sendStatus(201);
     });
 };
