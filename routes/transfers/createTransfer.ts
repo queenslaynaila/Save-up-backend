1fe6085471d433942531608b8a02f20d7e9549f0
queenslaynaila <queenslayjema@gmail.com>
2024-06-05 10:25:31 +0300

    Fix:transfer

--- routes/transfers/createTransfer.ts
+++ routes/transfers/createTransfer.ts
@@ -1,34 +1,29 @@
 import { Router } from 'express';
 import { sql } from '../../db';
 import authMiddleware from '../../middleware/authorization';
-import { HttpError } from '../../middleware/errorMiddleware';
 import { validateRequest } from '../../middleware/validationMiddleware';
 import { TransferInputInterface, TransferDepositResInterface, TransferDepositBodyInterface, transferDepositBody  } from './types'
-import { MessageInterface } from '../../globalTypes/index'
+import { StatusCodeInterface } from '../../globalTypes/index'
 
 const SQL_CREATE_TRANSFER = sql<TransferInputInterface, TransferDepositResInterface>(`
-INSERT INTO transfers (id, source_pocket_id, destination_pocket_id, amount, user_id)
-VALUES (  (SELECT COALESCE(MAX(id), 0) + 1 FROM transfers WHERE user_id = :user_id),
-          :source_pocket_id, :destination_pocket_id, :amount, :user_id
-        )
-RETURNING 
-    (SELECT name FROM pockets WHERE id = :source_pocket_id) AS source_pocket_name,
-    (SELECT name FROM pockets WHERE id = :destination_pocket_id) AS destination_pocket_name;
+  INSERT INTO transfers (id, source_pocket_id, destination_pocket_id, amount, user_id)
+  SELECT 
+      COALESCE(MAX(id), 0) + 1,
+      :source_pocket_id,
+      :destination_pocket_id,
+      :amount,
+      :user_id
+  FROM transfers
+  WHERE user_id = :user_id;
 `);
 
 export default (router: Router) => {
-  router.post<Record<string,never>, MessageInterface, TransferDepositBodyInterface, Record<string,never>>(
+  router.post<Record<string,never>, StatusCodeInterface, TransferDepositBodyInterface, Record<string,never>>(
     '/', 
     authMiddleware(),
     validateRequest(transferDepositBody),
     async (req, res) => {
-      const { source_pocket_id, destination_pocket_id, amount } = req.body
-      const user_id = req.user!.id
-      const transfer = await SQL_CREATE_TRANSFER({ source_pocket_id, destination_pocket_id, amount, user_id }).one(
-        new HttpError(400, 'Insufficient funds in source pocket for transfer.')
-      );
-      return res.json({
-        message: `Trasfer of amount KES ${amount.toFixed(2)} from pocket ${transfer.source_pocket_name} to pocket ${transfer.destination_pocket_name} successful!`
-      }); 
+      await SQL_CREATE_TRANSFER({  ...req.body, user_id:req.user!.id }).exec();
+      res.sendStatus(201);
     });
 };
