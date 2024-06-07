ab45c5461a53f1abc15a08322ecb60da8a41bc48
queenslaynaila <queenslayjema@gmail.com>
2024-06-07 16:15:36 +0300

    Refactor: ts types

--- routes/securityQuestions/getAllSecurityQuestions.ts
+++ routes/securityQuestions/getAllSecurityQuestions.ts
@@ -1,13 +1,13 @@
 import { Router } from 'express';
 import { sql } from '../../db';
-import { SecurityQuestionInterface } from './types';
+import { SecurityQuestionType } from './types';
 
-const SQL_GET_SECURITY_QUESTIONS = sql<Record<string,never>, SecurityQuestionInterface>(`
+const SQL_GET_SECURITY_QUESTIONS = sql<Record<string,never>, SecurityQuestionType>(`
   SELECT id, question FROM security_questions
 `);
 
 export default (router: Router) => {
-  router.get<Record<string,never>, SecurityQuestionInterface[], Record<string,never>, Record<string,never>>(
+  router.get<Record<string,never>, SecurityQuestionType[], Record<string,never>, Record<string,never>>(
     '/', 
     async (_, res) => {
       const securityQuestions = await SQL_GET_SECURITY_QUESTIONS({}).many();
--- routes/securityQuestions/types.ts
+++ routes/securityQuestions/types.ts
@@ -5,4 +5,4 @@ export const securityQuestionSchema = z.object({
   question: z.string()
 })
     
-export type SecurityQuestionInterface = z.infer<typeof securityQuestionSchema>;
+export type SecurityQuestionType = z.infer<typeof securityQuestionSchema>;
