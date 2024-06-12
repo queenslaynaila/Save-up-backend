72e00ff78694b6b0a49441ce1f75e55f55f68b86
queenslaynaila <queenslayjema@gmail.com>
2024-06-12 08:57:45 +0300

    REfactor: move get users  questions to security questions

--- routes/securityQuestions/getUsersQuestions.ts
+++ routes/securityQuestions/getUsersQuestions.ts
@@ -1,19 +1,21 @@
 import { Router } from 'express';
 import { sql } from '../../db';
-import { SecurityQuestionType } from '../securityQuestions/types';
-import { AnswerByUserType } from './types';
+import authMiddleware from '../../middleware/authorization';
+import { SecurityQuestions } from './types';
+import { AnswerByUserType } from '../securityAnswer/types';
 
-const SQL_GET_USER_SECURITY_QUESTIONS = sql<AnswerByUserType, SecurityQuestionType>(`
+const SQL_GET_USER_SECURITY_QUESTIONS = sql<AnswerByUserType, SecurityQuestions>(`
   SELECT security_questions.id, security_questions.question 
   FROM security_questions
   LEFT JOIN security_answers 
   ON security_questions.id = security_answers.question_id
-  AND security_answers.user_id = :user_id
+  WHERE security_answers.user_id = :user_id
 `);
 
 export default (router: Router) => {
-  router.get<Record<string,never>, SecurityQuestionType[], Record<string,never>, Record<string,never>>(
-    '/', 
+  router.get<Record<string,never>, SecurityQuestions[], Record<string,never>, Record<string,never>>(
+    '/me/', 
+    authMiddleware(),
     async (req, res) => {
       const securityQuestions = await SQL_GET_USER_SECURITY_QUESTIONS({
         user_id:req.user!.id
