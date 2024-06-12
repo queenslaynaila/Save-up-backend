781a20b6cc526976616cb330dd483f0959e7e210
queenslaynaila <queenslayjema@gmail.com>
2024-06-12 11:10:54 +0300

    Fix: primary keys

--- database/0025_group_administrators.sql
+++ database/0025_group_administrators.sql
@@ -1,12 +1,12 @@
 CREATE TABLE IF NOT EXISTS group_administrators (
-  group_id      INT NOT NULL,
   user_id       INT NOT NULL,
+  group_id      INT NOT NULL,
   created_at    TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
   revoked_at    TIMESTAMP WITH TIME ZONE,
-  PRIMARY KEY   (group_id,user_id),
+  PRIMARY KEY   (user_id, group_id),
   FOREIGN KEY   (group_id,user_id) REFERENCES group_users(group_id,user_id)
 );
 
-CREATE UNIQUE INDEX group_administrators_user_id_key ON group_administrators(group_id, user_id) WHERE revoked_at  IS NULL;
+CREATE UNIQUE INDEX group_administrators_user_id_key ON group_administrators(user_id, group_id) WHERE revoked_at  IS NULL;
 GRANT INSERT, SELECT, UPDATE ON group_administrators TO app_user;
 SELECT create_distributed_table('group_administrators', 'group_id');
--- database/0033_nomination_approvals.sql
+++ database/0033_nomination_approvals.sql
@@ -1,25 +1,16 @@
 CREATE TABLE IF NOT EXISTS nomination_approvals (
   group_id              INT NOT NULL,
-  xid                   INT NOT NULL,
   voter_id              INT NOT NULL,
   nominee_id            INT NOT NULL,
   vote                  BOOLEAN NOT NULL, 
   revoked_at            TIMESTAMP WITH TIME ZONE,
   created_at            TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
-  PRIMARY KEY           (group_id, xid),
+  PRIMARY KEY           (group_id, voter_id, nominee_id),
   FOREIGN KEY           (group_id, voter_id) REFERENCES group_users(group_id, user_id),
   FOREIGN KEY           (group_id, nominee_id) REFERENCES nominated_administrators(group_id, user_id)
 );
 
+CREATE UNIQUE INDEX nomination_approvals_voter_id_nominee_id_key
+ON nomination_approvals(group_id, voter_id, nominee_id) WHERE revoked_at IS NULL;
 GRANT SELECT, INSERT ON nomination_approvals TO app_user;
 SELECT create_distributed_table('nomination_approvals', 'group_id');
-
-CREATE TABLE IF NOT EXISTS nominated_administrators (
-  group_id           INT NOT NULL,
-  xid                INT NOT NULL,
-  user_id            INT NOT NULL,
-  created_at         TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
-  revoked_at         TIMESTAMP WITH TIME ZONE,
-  PRIMARY KEY        (group_id, xid),
-  FOREIGN KEY        (group_id, user_id) REFERENCES group_users(group_id, user_id)
-);
