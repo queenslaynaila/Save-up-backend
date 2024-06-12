a79eba2251539e2563af5c84a1134915387c2e74
queenslaynaila <queenslayjema@gmail.com>
2024-06-12 08:58:06 +0300

    Feat: add route to get users questions

--- routes/securityQuestions/index.ts
+++ routes/securityQuestions/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import getAllSecurityQuestions from './getAllSecurityQuestions';
+import getUsersQuestions from './getUsersQuestions';
 
 export default (baseRouter: Router) => {
   const router = Router();
 
   getAllSecurityQuestions(router);
+  getUsersQuestions(router);
   
   baseRouter.use('/security-questions', router)
 };
