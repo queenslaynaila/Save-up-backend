a18acdaa2132ba55d0c010c5e51e6314a928f5fd
queenslaynaila <queenslayjema@gmail.com>
2024-06-12 23:05:03 +0300

    Refcator:column names

--- database/0031_nominated_administrators.sql
+++ database/0031_nominated_administrators.sql
@@ -1,12 +1,17 @@
 CREATE TABLE IF NOT EXISTS nominated_administrators (
-  user_id            INT NOT NULL,
+  nominee_id         INT NOT NULL,
   group_id           INT NOT NULL,
+  nominator_id       INT NOT NULL,
   created_at         TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
   revoked_at         TIMESTAMP WITH TIME ZONE,
-  PRIMARY KEY        (user_id, group_id),
-  FOREIGN KEY        (group_id, user_id) REFERENCES group_users(group_id, user_id)
+  PRIMARY KEY        (nominee_id, group_id),
+  FOREIGN KEY        (group_id, nominator_id) REFERENCES group_users(group_id, user_id),
+  FOREIGN KEY        (group_id, nominee_id) REFERENCES group_users(group_id, user_id)
 );
 
-CREATE UNIQUE INDEX nominated_administrators_group_id_user_id_key ON nominated_administrators(user_id, group_id) WHERE revoked_at  IS NULL;
+CREATE UNIQUE INDEX nominated_administrators_group_id_user_id_key 
+ON nominated_administrators(nominee_id, group_id) 
+WHERE revoked_at  IS NULL;
+
 GRANT SELECT, INSERT ON nominated_administrators TO app_user;
 SELECT create_distributed_table('nominated_administrators', 'group_id');  
