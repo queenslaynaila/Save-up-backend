486e4228c991dead784181243f9825a913d10c93
queenslaynaila <queenslayjema@gmail.com>
2024-06-12 10:42:57 +0300

    Refactor: primary keys and varible names

--- database/0025_group_administrators.sql
+++ database/0025_group_administrators.sql
@@ -1,9 +1,10 @@
 CREATE TABLE IF NOT EXISTS group_administrators (
   group_id      INT NOT NULL,
+  xid           INT NOT NULL
   user_id       INT NOT NULL,
   created_at    TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
   revoked_at    TIMESTAMP WITH TIME ZONE,
-  PRIMARY KEY   (group_id,user_id),
+  PRIMARY KEY   (group_id,xid),
   FOREIGN KEY   (group_id,user_id) REFERENCES group_users(group_id,user_id)
 );
 
--- database/0032_nomination_approvals.sql
+++ database/0032_nomination_approvals.sql
@@ -1,14 +1,14 @@
 CREATE TABLE IF NOT EXISTS nomination_approvals (
   group_id              INT NOT NULL,
   xid                   INT NOT NULL
-  voter_member_id       INT NOT NULL,
-  nominated_member_id   INT NOT NULL,
+  voter_id              INT NOT NULL,
+  nominee_id            INT NOT NULL,
   vote                  BOOLEAN NOT NULL, 
   revoked_at            TIMESTAMP WITH TIME ZONE,
   created_at            TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
   PRIMARY KEY           (group_id, xid),
-  FOREIGN KEY           (group_id, voter_member_id) REFERENCES group_users(group_id, user_id),
-  FOREIGN KEY           (group_id, nominated_member_id) REFERENCES nominated_administrators(group_id, user_id)
+  FOREIGN KEY           (group_id, voter_id) REFERENCES group_users(group_id, user_id),
+  FOREIGN KEY           (group_id, nominee_id) REFERENCES nominated_administrators(group_id, user_id)
 );
 
 GRANT SELECT, INSERT ON nomination_approvals TO app_user;
