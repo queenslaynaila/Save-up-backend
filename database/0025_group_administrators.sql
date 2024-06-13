7ebb4cb7b9233f9095a5674ab207a548c33eabf9
queenslaynaila <queenslayjema@gmail.com>
2024-06-13 09:04:54 +0300

    REfactor: order of keys

--- database/0025_group_administrators.sql
+++ database/0025_group_administrators.sql
@@ -1,14 +1,14 @@
 CREATE TABLE IF NOT EXISTS group_administrators (
-  user_id       INT NOT NULL,
   group_id      INT NOT NULL,
+  user_id       INT NOT NULL,
   created_at    TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
   revoked_at    TIMESTAMP WITH TIME ZONE,
-  PRIMARY KEY   (user_id, group_id),
+  PRIMARY KEY   (group_id, user_id),
   FOREIGN KEY   (group_id,user_id) REFERENCES group_users(group_id,user_id)
 );
 
 CREATE UNIQUE INDEX group_administrators_user_id_key 
-ON group_administrators(user_id, group_id) 
+ON group_administrators(group_id, user_id) 
 WHERE revoked_at  IS NULL;
 
 GRANT INSERT, SELECT, UPDATE ON group_administrators TO app_user;
