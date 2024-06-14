5107de471f7983cab6599ee6ff1f6e5f832e5528
queenslaynaila <queenslayjema@gmail.com>
2024-06-14 12:21:06 +0300

    Feat: one valid invitation to a user per group

--- database/0027_invitations.sql
+++ database/0027_invitations.sql
@@ -1,19 +1,21 @@
 CREATE TYPE enum_invite AS ENUM ('Pending', 'Accept', 'Decline');
 
 CREATE TABLE IF NOT EXISTS invitations (   
-  group_id      INT NOT NULL,       
+  group_id      INT NOT NULL,  
+  xid           INT NOT NULL,     
   receiver_id   INT NOT NULL,
   sender_id     INT NOT NULL,
   status        enum_invite NOT NULL DEFAULT 'Pending',
   created_at    TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
   deleted_at    TIMESTAMP WITH TIME ZONE ,
-  PRIMARY KEY   (group_id, receiver_id),
-  FOREIGN KEY   (receiver_id) REFERENCES entities(id)
+  PRIMARY KEY   (group_id, xid),
+  FOREIGN KEY   (receiver_id) REFERENCES entities(id),
+  FOREIGN KEY   (sender_id) REFERENCES entities(id)
 );
 SELECT create_distributed_table('invitations', 'group_id');
 
-ALTER TABLE invitations
-ADD CONSTRAINT invitations_group_id_sender_id_fkey
-FOREIGN KEY (group_id,sender_id) REFERENCES group_administrators(group_id,user_id);
+CREATE UNIQUE INDEX invitations_group_id_receiver_id_key 
+ON invitations(group_id, receiver_id) 
+WHERE deleted_at IS NULL;
 
 GRANT INSERT, SELECT, UPDATE ON invitations TO app_user;
