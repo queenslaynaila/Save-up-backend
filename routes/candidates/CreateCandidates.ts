41425c0fe41d30376dfba432ba977c9f18cfc027
queenslaynaila <queenslayjema@gmail.com>
2024-06-19 15:56:06 +0300

    Feat: Create Candidates

---
+++ routes/candidates/CreateCandidates.ts
@@ -0,0 +1,24 @@
+import { Router } from 'express';
+import { sql } from '../../db';
+import authMiddleware from '../../middleware/authorization';
+import { validateRequest } from '../../middleware/validationMiddleware';
+import {  StatusCodeInterface } from '../../globalTypes/index';
+import { CandidateInterface, CandidateRequestBody, candidateRequestBody } from './types';
+
+const SQL_CREATE_CANDIDATES = sql<CandidateInterface , Record<string,never>>(`
+  INSERT INTO CANDIDATES (group_id, election_id, candidate_id, chosen_by)
+  VALUES (:group_id, :election_id, :candidate_id, :chosen_by);
+`);
+
+export default (router: Router) => {
+  router.post<Record<string,never>, StatusCodeInterface, CandidateRequestBody, Record<string,never>, Record<string,never>>(
+    '/',
+    validateRequest(candidateRequestBody),
+    authMiddleware(),
+    async (req, res) => {
+      const chosen_by = req.user!.id
+      await SQL_CREATE_CANDIDATES({ ...req.body, chosen_by}).exec();
+      res.sendStatus(201);
+    }
+  );
+};
