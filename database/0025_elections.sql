de2a47b457aa7978ac26730d1b083419bf2317a5
queenslaynaila <queenslayjema@gmail.com>
2024-06-19 15:55:38 +0300

    Refactor: Add chosen by row to candidates

--- database/0025_elections.sql
+++ database/0025_elections.sql
@@ -7,7 +7,7 @@ CREATE TABLE election (
   type           enum_election_type NOT NULL,
   status         enum_election_status NOT NULL DEFAULT 'Open',
   created_at     TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
-  closed_at      TIMESTAMP WITH TIME ZONE ,
+  closed_at      TIMESTAMP WITH TIME ZONE,
   PRIMARY KEY    (group_id, xid),
   FOREIGN KEY    (group_id) REFERENCES groups(id) ON DELETE RESTRICT ON UPDATE RESTRICT,
   FOREIGN KEY    (group_id, initiator_id) REFERENCES group_members(group_id,user_id)
@@ -23,12 +23,14 @@ CREATE TABLE ratifications (
   FOREIGN KEY    (group_id,user_id) REFERENCES group_members(group_id,user_id) 
     ON DELETE RESTRICT ON UPDATE RESTRICT,
   FOREIGN KEY    (group_id, election_id) REFERENCES election(group_id,xid)
+    ON DELETE RESTRICT ON UPDATE RESTRICT
 );
 
 CREATE TABLE candidates (
   group_id       INT NOT NULL,
   election_id    INT NOT NULL,
   candidate_id   INT NOT NULL,
+  chosen_by      INT NOT NULL, -- User ID of the person who chose the candidate
   created_at     TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
   PRIMARY KEY    (group_id, election_id, candidate_id),
   FOREIGN KEY    (group_id, candidate_id) REFERENCES group_members(group_id,user_id),
