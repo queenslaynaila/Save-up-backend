bb16524cf3a0268f4a70cb37eb19df101e03aef9
queenslaynaila <queenslayjema@gmail.com>
2024-06-19 11:57:48 +0300

    Feat: add prev group names table for historical goup name tracking

--- database/0023_groups.sql
+++ database/0023_groups.sql
@@ -3,9 +3,19 @@ CREATE TABLE IF NOT EXISTS groups (
   name          TEXT NOT NULL,
   creator_id    INT NOT NULL, 
   created_at    TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
-  updated_at    TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
   deleted_at    TIMESTAMP WITH TIME ZONE,
   FOREIGN KEY   (creator_id) REFERENCES entities(id)
 );
 SELECT create_distributed_table('groups', 'id');
 GRANT INSERT, SELECT, UPDATE ON groups TO app_user;
+
+CREATE TABLE IF NOT EXISTS prev_group_names (
+  group_id      INT NOT NULL,
+  xid           INT NOT NULL,
+  name          TEXT NOT NULL,
+  created_at    TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+  PRIMARY KEY   (group_id, xid),
+  FOREIGN KEY   (group_id) REFERENCES groups(id)
+);
+SELECT create_distributed_table('prev_group_names', 'group_id');
+GRANT INSERT, SELECT ON prev_group_names TO app_user;
