2fb29c6c92429b904894ed3d1033a6e1200f2dd4
queenslaynaila <queenslayjema@gmail.com>
2024-06-19 12:04:02 +0300

    feat(database): introduce group membership mgmt
    - Added 'enum_exit_reason' enum type for exit reasons in groups
    - Refactored 'group_users' to 'group_members' and introduced 'group_joins' table
    - Created 'group_lefts' table to track reasons for group exits
    - Granted permissions to 'app_user' for group management table

--- database/0024_group_users.sql
+++ database/0024_group_users.sql
@@ -1,22 +1,30 @@
--- A member can leave a group and join again
-CREATE TABLE IF NOT EXISTS group_users (
+CREATE TYPE enum_exit_reason AS ENUM ('Self removal', 'Admin removal', 'Rule violation', 'Other');
+
+CREATE TABLE IF NOT EXISTS group_members (
+  group_id      INT NOT NULL,
+  user_id       INT NOT NULL,
+  is_active     BOOLEAN NOT NULL DEFAULT TRUE,
+  PRIMARY KEY   (group_id, user_id),
+  FOREIGN KEY   (group_id) REFERENCES groups(id) ON DELETE RESTRICT ON UPDATE RESTRICT
+);
+SELECT create_distributed_table('group_members', 'group_id');
+GRANT INSERT, SELECT, UPDATE ON group_members TO app_user;
+
+CREATE TABLE IF NOT EXISTS group_joins (
   group_id      INT NOT NULL,
   user_id       INT NOT NULL,
   xid           INT NOT NULL,
   created_at    TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
-  left_at       TIMESTAMP WITH TIME ZONE,
-  PRIMARY KEY   (group_id, xid),
-  FOREIGN KEY   (user_id) REFERENCES entities(id)
+  PRIMARY KEY   (group_id, user_id, xid),
+  FOREIGN KEY   (group_id, user_id) REFERENCES group_members(group_id, user_id)
 );
 
-SELECT create_distributed_table('group_users', 'group_id');
-
-CREATE UNIQUE INDEX group_users_user_id_key 
-ON group_users(group_id, user_id) 
-WHERE left_at IS NULL;
-
-ALTER TABLE group_users
-ADD CONSTRAINT group_users_group_id_fkey  
-FOREIGN KEY (group_id) REFERENCES groups(id);
-
-GRANT INSERT, SELECT, UPDATE ON group_users TO app_user;
+CREATE TABLE IF NOT EXISTS group_lefts(
+  group_id      INT NOT NULL,
+  user_id       INT NOT NULL,
+  xid           INT NOT NULL,
+  reason        enum_exit_reason NOT NULL,
+  created_at    TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+  PRIMARY KEY   (group_id, user_id, xid),
+  FOREIGN KEY   (group_id, user_id) REFERENCES group_members(group_id, user_id)
+);
