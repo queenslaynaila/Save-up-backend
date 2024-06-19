3fe0d7d66abf4501bc73db93b2b0295f9d2e9714
queenslaynaila <queenslayjema@gmail.com>
2024-06-19 16:37:28 +0300

    Refactor: Group administrators to group admins

--- database/0025_group_administrators.sql
+++ database/0025_group_administrators.sql
@@ -1,22 +1,10 @@
---Previously revoked admin can be admin once again 
-CREATE TABLE IF NOT EXISTS group_administrators (
+CREATE TABLE IF NOT EXISTS group_admins (
   group_id      INT NOT NULL,
+  election_id   INT NOT NULL,
   user_id       INT NOT NULL,
-  xid           INT NOT NULL,
-  term_starts   TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
-  term_ends     TIMESTAMP WITH TIME ZONE,
-  PRIMARY KEY   (group_id, xid),
-  FOREIGN KEY   (group_id) REFERENCES groups(id)
+  PRIMARY KEY   (group_id, election_id, user_id),
+  FOREIGN KEY   (group_id, election_id, user_id) REFERENCES candidates (group_id, election_id, candidate_id)
 );
 
-CREATE UNIQUE INDEX group_administrators_group_id_user_id_key 
-ON group_administrators(group_id, user_id) 
-WHERE term_ends  IS NULL;
-
 SELECT create_distributed_table('group_administrators', 'group_id');
-
-ALTER TABLE group_administrators 
-ADD CONSTRAINT group_administrators_user_id_fkey  
-FOREIGN KEY (user_id) REFERENCES entities(id);
-
 GRANT INSERT, SELECT, UPDATE ON group_administrators TO app_user;
