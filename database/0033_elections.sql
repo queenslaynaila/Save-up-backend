8e5c9b6495c49be3fa4277887c5db84fda2ae2ed
queenslaynaila <queenslayjema@gmail.com>
2024-06-20 14:58:20 +0300

    Refactor: move grp admin to elections file

--- database/0033_elections.sql
+++ database/0033_elections.sql
@@ -18,16 +18,14 @@ GRANT INSERT, SELECT, UPDATE ON elections TO app_user;
 CREATE TABLE ratifications (
   group_id       INT NOT NULL,
   election_id    INT NOT NULL,
-  candidate_id   INT NOT NULL,
   user_id        INT NOT NULL,
   is_ratified    BOOLEAN NOT NULL,
   created_at     TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
   PRIMARY KEY    (group_id, election_id, user_id),
   FOREIGN KEY    (group_id,user_id) REFERENCES group_members(group_id,user_id) 
     ON DELETE RESTRICT ON UPDATE RESTRICT,
-  FOREIGN KEY    (group_id, election_id) REFERENCES election(group_id,xid)
-    ON DELETE RESTRICT ON UPDATE RESTRICT,
-  FOREIGN KEY   (group_id, election_id, candidate_id) REFERENCES candidates(group_id, election_id, candidate_id)
+  FOREIGN KEY    (group_id, election_id) REFERENCES elections(group_id,xid)
+    ON DELETE RESTRICT ON UPDATE RESTRICT
 );
 SELECT create_distributed_table('ratifications', 'group_id');
 GRANT INSERT, SELECT ON ratifications TO app_user;
@@ -40,7 +38,7 @@ CREATE TABLE candidates (
   created_at     TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
   PRIMARY KEY    (group_id, election_id, candidate_id),
   FOREIGN KEY    (group_id, candidate_id) REFERENCES group_members(group_id,user_id),
-  FOREIGN KEY    (group_id, election_id) REFERENCES election(group_id,xid) 
+  FOREIGN KEY    (group_id, election_id) REFERENCES elections(group_id,xid) 
     ON DELETE RESTRICT ON UPDATE RESTRICT
 );
 SELECT create_distributed_table('candidates', 'group_id');
@@ -58,3 +56,14 @@ CREATE TABLE ballots (
 );
 SELECT create_distributed_table('ballots', 'group_id');
 GRANT INSERT, SELECT ON ballots TO app_user;
+
+CREATE TABLE IF NOT EXISTS group_admins (
+  group_id      INT NOT NULL,
+  election_id   INT NOT NULL,
+  user_id       INT NOT NULL,
+  PRIMARY KEY   (group_id, election_id, user_id),
+  FOREIGN KEY   (group_id, election_id, user_id) REFERENCES candidates (group_id, election_id, candidate_id)
+);
+
+SELECT create_distributed_table('group_admins', 'group_id');
+GRANT INSERT, SELECT ON group_admins TO app_user;
