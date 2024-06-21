e1f562d5c44642bbdce2c5a3caf74fcadb939e39
queenslaynaila <queenslayjema@gmail.com>
2024-06-21 09:04:16 +0300

    Feat: Add function to compute ratification results for elections

--- database/0035_compute_ratification.sql
+++ database/0035_compute_ratification.sql
@@ -3,44 +3,44 @@ CREATE OR REPLACE FUNCTION compute_ratification_results(
     p_election_id    INT
 )
 RETURNS TABLE (
-    full_name    TEXT
+    full_name            TEXT,
+    ratification_status  BOOLEAN
 ) AS $$
 DECLARE
     v_total_members             INT;
     v_required_approval_count   INT;
-    approval_counts             RECORD;
+    v_approval_count            RECORD;
 BEGIN
     SELECT COUNT(*) INTO v_total_members 
     FROM group_members 
     WHERE group_id = p_group_id 
     AND is_active = TRUE;
 
-    v_required_approval_count := total_members / 2 + 1;  ---shld be more than 50%
+    v_required_approval_count := v_total_members / 2 + 1;  ---shld be more than 50%
 
     FOR approval_counts IN
-        SELECT candidate_id, COUNT(*) AS approval_count
-        FROM ratifications 
-        WHERE group_id = p_group_id 
-        AND election_id = p_election_id 
-        AND is_ratified = TRUE
-        GROUP BY candidate_id
+        SELECT COUNT(*) INTO STRICT v_approval_count 
+        FROM ratifications
+        WHERE group_id =  p_group_id  
+        AND election_id =  p_election_id
+        AND is_ratified = TRUE;
     LOOP
-        IF approval_counts.approval_count >= required_approval_count THEN
-            INSERT INTO group_admins (group_id, election_id, user_id)
-            VALUES (p_group_id, p_election_id, approval_counts.candidate_id);
-
+        IF approval_counts.v_approval_count >= v_required_approval_count THEN
             RETURN QUERY
-            SELECT full_name
-            FROM users
-            WHERE id = approval_counts.candidate_id;
+            SELECT u.full_name, TRUE
+            FROM users u
+            JOIN group_admins ga ON u.id = ga.user_id
+            WHERE ga.group_id = p_group_id
+            AND ga.election_id = p_election_id;
+        ELSE
+            RETURN QUERY
+            SELECT u.full_name, FALSE
+            FROM users u
+            JOIN group_admins ga ON u.id = ga.user_id
+            WHERE ga.group_id = p_group_id
+            AND ga.election_id = p_election_id;
         END IF;
     END LOOP;
-
-    UPDATE elections
-    SET status = 'Closed', 
-        closed_at = NOW()
-    WHERE group_id = p_group_id 
-    AND xid = p_election_id;
 END;
 $$ LANGUAGE plpgsql;
 
