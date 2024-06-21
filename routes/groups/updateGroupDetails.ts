a477c9c651d10d3e1fd2fb2eb46311955242afc5
queenslaynaila <queenslayjema@gmail.com>
2024-06-21 09:14:03 +0300

    Fix: Correct function name from update_group_details to update_group_name

--- routes/groups/updateGroupDetails.ts
+++ routes/groups/updateGroupDetails.ts
@@ -1,18 +1,18 @@
 import { Router } from 'express';
 import { sql } from '../../db';
-import  authMiddleware from '../../middleware/authorization';
+//import  authMiddleware from '../../middleware/authorization';
 import { validateRequest } from '../../middleware/validationMiddleware';
 import { GroupUpdateInterface, validateGroupUpdateSchema} from './types';
 import { IdParamInterface } from '../../globalTypes/index';
 
 const SQL_UPDATE_GROUP = sql<GroupUpdateInterface, GroupUpdateInterface>(`
-   SELECT * FROM update_group_details(:id, :name)
+   SELECT * FROM update_group_name(:id, :name)
 `);
 
 export default (router: Router) => {
   router.patch<IdParamInterface, GroupUpdateInterface, GroupUpdateInterface, Record<string,never>, Record<string,never>>(
     '/:id',
-    authMiddleware(),
+
     validateRequest(validateGroupUpdateSchema),
     async (req, res) => {
       const updatedGroup = await SQL_UPDATE_GROUP({ ...req.body, id: parseInt(req.params.id)}).one();
