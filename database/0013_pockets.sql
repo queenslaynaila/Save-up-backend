e0e15c52e7570a57d2ab8862d293ca98cd0f022d
queenslaynaila <queenslayjema@gmail.com>
2024-06-24 16:24:46 +0300

    Refactor: Remove savings table

--- database/0013_pockets.sql
+++ database/0013_pockets.sql
@@ -60,20 +60,6 @@ CREATE TABLE IF NOT EXISTS pocket_reminders (
 );
 GRANT INSERT, SELECT ON pocket_reminders TO app_user;
 
-CREATE TABLE IF NOT EXISTS savings (
-  entity_id             INT NOT NULL, -- owner of the pocket, either a group or a standard user
-  xid                   INT NOT NULL,
-  pocket_id             INT NOT NULL,
-  user_id               INT NOT NULL, -- the user depositing the cash, either a grp member or a standard user
-  amount                NUMERIC(30, 2) NOT NULL CHECK (amount > 0),
-  created_at            TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
-  PRIMARY KEY           (entity_id, xid),
-  FOREIGN KEY           (entity_id, pocket_id) REFERENCES pockets (entity_id, xid)
-);
-
-SELECT create_distributed_table('savings', 'entity_id');
-GRANT INSERT, SELECT ON savings TO app_user;
-
 CREATE TABLE IF NOT EXISTS external_savings (
   entity_id             INT NOT NULL,-- owner of the pocket
   xid                   INT NOT NULL,
