5790d2f498a610b7218551da4ad1126c6385fbb4
queenslaynaila <queenslayjema@gmail.com>
2024-06-24 08:26:02 +0300

    Fix: Omit candidate id from computeRaftification

--- routes/ratifications/types.ts
+++ routes/ratifications/types.ts
@@ -3,8 +3,8 @@ import { z } from 'zod';
 export const ratificationSchema = z.object({
   group_id:z.number(), 
   election_id:z.number(),
-  candidate_id:z.number(), 
-  user_id: z.number()
+  user_id: z.number(),
+  is_ratified: z.boolean()
 })
 
 export type RatificationInterface = z.infer<typeof ratificationSchema >
