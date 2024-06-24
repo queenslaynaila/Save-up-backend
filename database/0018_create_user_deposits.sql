7f0ce4930c270e79209f63ee6a85e3536c2b4032
queenslaynaila <queenslayjema@gmail.com>
2024-06-24 12:49:44 +0300

    Refcator: Chnage file name create_deposits to create_user_deposit

---
+++ database/0018_create_user_deposits.sql
@@ -0,0 +1,51 @@
+CREATE OR REPLACE FUNCTION create_user_deposit(
+    p_user_id      INT, 
+    p_pocket_id    INT, 
+    p_type_id      INT,
+    p_amount       NUMERIC
+)
+RETURNS VOID AS $$
+DECLARE
+    v_current_balance  NUMERIC;
+    v_transaction_id   INT;
+    v_target_amount    NUMERIC;
+    v_new_balance      NUMERIC;
+    v_reference_id     INT;
+BEGIN 
+    SELECT * FROM get_transaction_info(p_pocket_id, p_entity_id) 
+    INTO STRICT v_transaction_id, v_current_balance;
+
+    SELECT pockets.target_amount INTO STRICT v_target_amount  
+    FROM pockets 
+    WHERE entity_id = p_entity_id 
+    AND xid = p_pocket_id;
+    
+    IF v_current_balance >= v_target_amount THEN
+        UPDATE pockets
+        SET status = 'Completed'::enum_status,
+            completed_at = NOW()
+        WHERE entity_id = p_entity_id
+        AND xid = p_pocket_id;
+    END IF;
+
+    v_new_balance = v_current_balance + p_amount;
+    v_reference_no = substr(md5(random()::text), 1, 5);
+
+    PERFORM insert_transaction_log(
+        p_entity_id,
+        p_pocket_id,
+        p_type_id,
+        p_amount,
+        v_reference_id,
+        v_new_balance
+    );
+
+    INSERT INTO user_deposits (user_id, deposit_id, status)
+    VALUES (p_user_id, v_transaction_id, 'Completed');
+END;
+$$ LANGUAGE plpgsql;
+
+GRANT EXECUTE ON FUNCTION create_saving(INT, INT, INT, NUMERIC) TO app_user;
+SELECT create_distributed_function(
+  'create_saving(INT, INT, INT, NUMERIC)', 'p_entity_id'
+);
