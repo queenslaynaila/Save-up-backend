164a0cf9588bf6c67b1b85da4fe33b01027c0d0c
queenslaynaila <queenslayjema@gmail.com>
2024-06-25 14:52:36 +0300

    Refactor:Rename update user contact id to update user id

--- database/0003_update_user_id.sql
+++ database/0003_update_user_id.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE FUNCTION update_user_contact_id(
+CREATE OR REPLACE FUNCTION update_user_id(
     p_user_id           INT,
     p_new_id_type       enum_id_type,
     p_new_id_number     TEXT
