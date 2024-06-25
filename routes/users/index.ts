896fbcbb4c5ba65a5bdec6760ca3895dfb376a46
queenslaynaila <queenslayjema@gmail.com>
2024-06-25 10:18:13 +0300

    Feat: Add route for updating ID

--- routes/users/index.ts
+++ routes/users/index.ts
@@ -3,6 +3,7 @@ import createUser from './createUser';
 import login from './login';
 import getUserByCriteria from './getUserByCriteria';
 import updateUserPhoneNo from './updateUserPhoneNo';
+import updateIdNumber from './updateIdNumber';
 import updateUserRole from './updateUserRole';
 import logout from './logout';
 
@@ -14,6 +15,7 @@ export default (baseRouter: Router) => {
   getUserByCriteria(router);
   updateUserPhoneNo(router);
   updateUserRole(router);
+  updateIdNumber(router);
   logout(router);
   
   baseRouter.use('/users', router);
