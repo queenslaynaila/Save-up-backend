368199b6dc02e30a4e5038495b427c661f5e0d7f
queenslaynaila <queenslayjema@gmail.com>
2024-06-26 10:50:58 +0300

    Refactor: Re arrange function for maintanability

--- database/0031_get_group_mbrs.sql
+++ database/0031_get_group_mbrs.sql
@@ -7,26 +7,22 @@ RETURNS TABLE(
     full_name    TEXT
 ) AS $$
 DECLARE
-    rec_user RECORD;
+    v_user_role   enum_user_role;
 BEGIN
-     IF EXISTS (
-        SELECT 1
-        FROM users
-        WHERE id = p_user_id
-        AND role = 'Admin'
-    ) THEN
-        RETURN QUERY
-        SELECT * FROM get_active_group_members(p_group_id);
-    END IF;
+    SELECT role INTO STRICT v_user_role
+    FROM users
+    WHERE id = p_user_id;
 
-    IF NOT EXISTS (
-        SELECT 1
-        FROM group_members
-        WHERE user_id = p_user_id
-        AND group_id = p_group_id
-        AND is_active = TRUE;
-    ) THEN
-        RAISE EXCEPTION 'User is not a member of the group.';
+    IF v_user_role != 'Admin' THEN
+        IF IF NOT EXISTS (
+            SELECT 1
+            FROM group_members
+            WHERE user_id = p_user_id
+            AND group_id = p_group_id
+            AND is_active = TRUE
+        ) THEN
+            RAISE EXCEPTION 'User is not a member of the group.';
+        END IF;
     END IF;
 
     RETURN QUERY
