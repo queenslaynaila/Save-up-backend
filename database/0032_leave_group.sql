9eb981145135bc9b557f272f2ec379b3dca51716
queenslaynaila <queenslayjema@gmail.com>
2024-06-28 11:28:39 +0300

    Refactor: REmove grp check since members table will automatically check and remove resoans from function param

--- database/0032_leave_group.sql
+++ database/0032_leave_group.sql
@@ -1,21 +1,11 @@
 CREATE OR REPLACE FUNCTION leave_group(
     p_user_id     INT,
-    p_group_id    INT,
-    p_reason      enum_exit_reason
+    p_group_id    INT
 ) 
 RETURNS VOID AS $$
 DECLARE
   v_member_count INT;
 BEGIN
-    IF NOT EXISTS (
-        SELECT 1
-        FROM groups
-        WHERE id = p_group_id
-        AND deleted_at IS NULL
-    ) THEN
-        RAISE EXCEPTION 'Group does not exist.';
-    END IF;
-
     IF NOT EXISTS (
         SELECT 1
         FROM group_members
@@ -36,7 +26,7 @@ BEGIN
         p_group_id,
         p_user_id,
         COALESCE(MAX(xid), 0) + 1,
-        p_reason
+        'Self removal'
     FROM group_lefts
     WHERE group_id = p_group_id;
 
@@ -53,7 +43,7 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
-GRANT EXECUTE ON FUNCTION leave_group(INT, INT, enum_exit_reason) TO app_user;
+GRANT EXECUTE ON FUNCTION leave_group(INT, INT) TO app_user;
 SELECT create_distributed_function(
-  'leave_group(INT, INT, enum_exit_reason)', 'p_group_id'
+  'leave_group(INT, INT)', 'p_group_id'
 );
