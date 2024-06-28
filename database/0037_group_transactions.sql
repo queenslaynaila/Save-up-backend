c6b368db55def0ec92b5446799a10fe29b4ed661
queenslaynaila <queenslayjema@gmail.com>
2024-06-28 11:05:32 +0300

    Refactor: Rename group wihdrawal to group withdrawal requests

--- database/0037_group_transactions.sql
+++ database/0037_group_transactions.sql
@@ -7,6 +7,8 @@ CREATE TYPE enum_withdrawal_reason AS ENUM (
 'Miscellaneous'
 );
 
+CREATE TYPE enum_approval_status AS ENUM ('Approved', 'Rejected', 'Pending');
+
 CREATE TABLE IF NOT EXISTS group_deposits (
   group_id              INT NOT NULL, 
   deposit_id            INT NOT NULL,
@@ -17,16 +19,16 @@ CREATE TABLE IF NOT EXISTS group_deposits (
 GRANT INSERT, SELECT ON group_deposits TO app_user;
 SELECT create_distributed_table('group_deposits', 'group_id');
 
-CREATE TABLE IF NOT EXISTS group_withdrawals (
+CREATE TABLE IF NOT EXISTS group_withdrawal_requests (
   group_id              INT NOT NULL, 
-  withdrawal_id         INT NOT NULL,
+  xid                   INT NOT NULL,
   election_id           INT NOT NULL,
   initiator_id          INT NOT NULL,
+  pocket_id             INT NOT NULL,
+  amount                NUMERIC(30, 2) NOT NULL CHECK (amount > 0),
   reason                enum_withdrawal_reason NOT NULL,
-  status                TEXT NOT NULL,
   PRIMARY KEY           (group_id, withdrawal_id),
-  FOREIGN KEY           (group_id, election_id, initiator_id) REFERENCES group_admins (group_id, election_id, user_id),
-  FOREIGN KEY           (group_id, withdrawal_id) REFERENCES transactions (entity_id, xid)
+  FOREIGN KEY           (group_id, election_id, initiator_id) REFERENCES group_admins (group_id, election_id, user_id)
 );
 GRANT INSERT, SELECT ON group_withdrawals TO app_user;
 SELECT create_distributed_table('group_withdrawals', 'group_id');
@@ -38,7 +40,7 @@ CREATE TABLE IF NOT EXISTS group_withdrawals_recipients (
   amount                NUMERIC(30, 2) NOT NULL CHECK (amount > 0),
   PRIMARY KEY           (group_id, withdrawal_id, user_id),
   FOREIGN KEY           (group_id, user_id) REFERENCES group_members (group_id, user_id),
-  FOREIGN KEY           (group_id, withdrawal_id) REFERENCES group_withdrawals (group_id, withdrawal_id)
+  FOREIGN KEY           (group_id, withdrawal_id) REFERENCES group_withdrawals (group_id, xid)
 );
 GRANT INSERT, SELECT ON group_withdrawals_recipients TO app_user;
 SELECT create_distributed_table('group_withdrawals_recipients', 'group_id');
@@ -48,6 +50,8 @@ CREATE TABLE IF NOT EXISTS group_withdrawals_approvals (
   withdrawal_id         INT NOT NULL,
   admin_id              INT NOT NULL,
   election_id           INT NOT NULL,
+  status                enum_approval_status NOT NULL DEFAULT 'Pending',
+  reason                TEXT NOT NULL,
   created_at            TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
   PRIMARY KEY           (group_id, withdrawal_id, admin_id),
   FOREIGN KEY           (group_id, election_id, admin_id) REFERENCES group_admins (group_id, election_id, user_id),
