c4ce1b42610a2cc4b5b391a04c2c10e59cbcf54a
queenslaynaila <queenslayjema@gmail.com>
2024-07-01 10:53:38 +0300

    Feat: Implement grp funds withdrawal

---
+++ routes/groups/withdrawMoney.ts
@@ -0,0 +1,30 @@
+import { Router } from 'express';
+import { sql } from '../../db';
+import authMiddleware from '../../middleware/authorization';
+import { validateRequest } from '../../middleware/validationMiddleware';
+import { groupCreationValidation, WithdrawalRequest } from './types';
+import { IdParamInterface, StatusCodeInterface } from '../../globalTypes/index';
+
+const SQL_INITIATE_GRP_WITHDRAWAL = sql<WithdrawalRequest, Record<string, never>>(`
+    SELECT initiate_grp_withdrawal(
+       :group_id, :pocket_id, :election_id, :initiator_id, :amount, :reason, :recipients
+    )
+`);
+
+export default (router: Router) => {
+  router.post<IdParamInterface, StatusCodeInterface, WithdrawalRequest, Record<string, never>, Record<string, never>>(
+    '/withdrawal/:id', 
+    authMiddleware(),
+    validateRequest(groupCreationValidation),
+    async (req, res) => {
+      await SQL_INITIATE_GRP_WITHDRAWAL({
+        ...req.body,
+        group_id: parseInt(req.params.id),
+        initiator_id: req.user!.id,
+      }).exec()
+      res.sendStatus(201);
+    }
+  );
+};
+
+
