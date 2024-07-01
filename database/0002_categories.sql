371138f00cab6005d9f193fdb4310b32735337e5
queenslaynaila <queenslayjema@gmail.com>
2024-07-01 09:47:47 +0300

    Fix : Space error

--- database/0002_categories.sql
+++ database/0002_categories.sql
@@ -19,8 +19,7 @@ VALUES ('Food', 'All food related expenses'),
        ('Savings', 'Funds set aside for future investments or emergencies'),
        ('Debt Repayment', 'Payments towards loans, credit cards, and other debts'),
        ('Travel', 'Expenses or savings related to trips, vacations, and travel activities'),
-       ('Default Category', 'A flexible space where you can save money without assigning it to specific purposes right away. 
-       Funds saved here are readily available for future use and can be easily allocated to other pockets whenever you choose.');
+       ('Default Category', 'A flexible space where you can save money without assigning it to specific purposes right away. Funds saved here are readily available for future use and can be easily allocated to other pockets whenever you choose.');
 
 SELECT create_reference_table('categories');
 GRANT SELECT ON categories TO app_user;
