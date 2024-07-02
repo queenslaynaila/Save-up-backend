3c410f6d2d6e289bf57fe2d7262a0a659917d6a1
queenslaynaila <queenslayjema@gmail.com>
2024-07-02 15:07:28 +0300

    Fix: Add missing comma in query

--- routes/elections/createElections.ts
+++ routes/elections/createElections.ts
@@ -1,15 +1,16 @@
 import { Router } from 'express';
 import { sql } from '../../db';
 import authMiddleware from '../../middleware/authorization';
+import { validateRequest } from '../../middleware/validationMiddleware';  
 import {  StatusCodeInterface } from '../../globalTypes/index';
-import { ElectionInterface } from './types';
+import { ElectionInterface, electionValidation } from './types';
 
 const SQL_CALL_ELECTION = sql<ElectionInterface , Record<string,never>>(`
   INSERT INTO elections (group_id, xid, initiator_id, type)
   SELECT 
       :group_id,
       COALESCE(MAX(xid), 0) + 1,
-      :initiator_id
+      :initiator_id,
       :type
   FROM elections
   WHERE group_id = :group_id;
@@ -19,9 +20,9 @@ export default (router: Router) => {
   router.post<Record<string,never>, StatusCodeInterface, ElectionInterface , Record<string,never>, Record<string,never>>(
     '/',
     authMiddleware(),
+    validateRequest(electionValidation),
     async (req, res) => {
-      const initiator_id= req.user!.id
-      await SQL_CALL_ELECTION({ ...req.body, initiator_id}).exec();
+      await SQL_CALL_ELECTION({ ...req.body, initiator_id:req.user!.id}).exec();
       res.sendStatus(201);
     }
   );
