a95a3d1e13b662d6f61ffc230e2b0f394dc718e6
queenslaynaila <queenslayjema@gmail.com>
2024-07-02 09:54:55 +0300

    Refactor: Improve readability

--- middleware/errorMiddleware.ts
+++ middleware/errorMiddleware.ts
@@ -1,13 +1,9 @@
-type ErrorData = {
-  [key: string]: unknown;
-};
-
 export class HttpError extends Error {
   public readonly statusCode: number;
   public readonly errorType: string;
-  public readonly errorData: ErrorData;
+  public readonly errorData: Record<string, unknown>;
 
-  constructor(statusCode: number, errorType: string, errorData: ErrorData = {}) {
+  constructor(statusCode: number, errorType: string, errorData: Record<string, unknown> = {}) {
     super();
     this.statusCode = statusCode;
     this.errorType = errorType;
