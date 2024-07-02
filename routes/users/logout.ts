56f543487da02794c56511e13b9cfdfadaf538a2
queenslaynaila <queenslayjema@gmail.com>
2024-07-02 10:46:14 +0300

    Fix: Remove patch request to sessions since it no longer there

--- routes/users/logout.ts
+++ routes/users/logout.ts
@@ -1,24 +1,14 @@
 import { Router } from 'express';
-import { sql } from '../../db';
 import authMiddleware from '../../middleware/authorization';
 import { StatusCodeInterface } from '../../globalTypes/index';
 
-const SQL_UPDATE_SESSION = sql<{user_id: number}, Record<string,never>>(`
-  UPDATE sessions
-  SET exited_at = NOW()
-  WHERE user_id = :user_id
-  AND exited_at IS NULL
-`);
-
 export default (router: Router) => {
-  router.delete<Record<string, never>, StatusCodeInterface, Record<string, never>, Record<string, never>, Record<string, never>>(
-    '/logout',
-    authMiddleware(),
-    async (req, res) => {
-      await SQL_UPDATE_SESSION({ user_id:req.user!.id}).exec();
+  router.delete<Record<string,never>, StatusCodeInterface, Record<string,never>, Record<string,never>, Record<string,never>>(
+    '/logout', 
+    authMiddleware(), 
+    async (_req, res) => {
       res.removeHeader('authorization-token');
       res.removeHeader('refresh-token');
       res.sendStatus(204);
-    }
-  );
+    });
 };
