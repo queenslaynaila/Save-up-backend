d2e4b978432f9193d7ac1f497d96b1ac1df2c513
queenslaynaila <queenslayjema@gmail.com>
2024-07-03 08:54:21 +0300

    Feat: Add get candidates to route list

--- routes/candidates/index.ts
+++ routes/candidates/index.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import createCandidates from './createCandidates';
+import getCandidates from './getCandidates';
 
 export default (baseRouter: express.Router) => {
   const router = express.Router();
 
   createCandidates(router);
+  getCandidates(router);
   
   baseRouter.use('/candidates', router);
 };
