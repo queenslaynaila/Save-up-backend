8d5a8abfc60d017afcdb5c6b377454685a402533
queenslaynaila <queenslayjema@gmail.com>
2024-07-03 09:50:28 +0300

    Refactor: Mak ballot tabulating a get request

--- routes/ballots/computeBallot.ts
+++ routes/ballots/computeBallot.ts
@@ -2,27 +2,27 @@ import { Router } from 'express';
 import { sql } from '../../db';
 import authMiddleware from '../../middleware/authorization';
 import { validateRequest } from '../../middleware/validationMiddleware';
-import {  StatusCodeInterface } from '../../globalTypes/index';
 import {  BallotComputeInterface, 
   BallotBodyRequestInterface,
   ballotBodyRequest, 
   BallotResultInterface 
 } from './types';
 
-const SQL_CREATE_BALLOT = sql<BallotComputeInterface,  BallotResultInterface>(`
-  SELECT compute_ballot_results(:group_id, :election_id, :user_id)
+const SQL_GET_ELECTION_WINNERS = sql<BallotComputeInterface,  BallotResultInterface>(`
+  SELECT * FROM compute_ballot_results(:group_id, :election_id, :user_id)
 `);
 
 export default (router: Router) => {
-  router.post<{ id: string }, StatusCodeInterface, BallotBodyRequestInterface,  BallotResultInterface, Record<string,never>>(
-    '/:id',
+  router.get<Record<string,never>, BallotResultInterface[], BallotBodyRequestInterface, Record<string,never>>(
+    '/',
     validateRequest(ballotBodyRequest),
     authMiddleware(),
     async (req, res) => {
-      await SQL_CREATE_BALLOT({
-        ...req.body, user_id: req.user!.id, group_id: parseInt(req.params.id)
-      }).exec();
-      res.sendStatus(201);
+      const winners = await SQL_GET_ELECTION_WINNERS ({
+        ...req.body, 
+        user_id: req.user!.id
+      }).many();
+      res.json(winners);
     }
   );
 };
