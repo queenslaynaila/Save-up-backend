9a6f6bc9b4e623e1392bc073a8b2b4c8344b7c81
queenslaynaila <queenslayjema@gmail.com>
2024-07-04 12:10:47 +0300

    Refcator:Replace loop with subquery

--- database/0042_get_candidates.sql
+++ database/0042_get_candidates.sql
@@ -4,11 +4,8 @@ CREATE OR REPLACE FUNCTION get_candidates(
     p_user_id       INT
 )
 RETURNS TABLE(
-    candidate_id         INT, 
-    group_id             INT,
-    election_id          INT,
-    candidate_name       TEXT,
-    created_at           TIMESTAMP WITH TIME ZONE
+    candidate_id    INT, 
+    full_name       TEXT
 ) AS $$
 DECLARE
     v_user_role   enum_user_role;
@@ -21,25 +18,16 @@ BEGIN
     IF v_user_role != 'Admin' THEN
       PERFORM check_grp_membership(p_user_id, p_group_id);
     END IF;
-
-    FOR candidate_record IN
-        SELECT c.group_id, c.election_id, c.candidate_id, c.created_at
-        FROM candidates c
-        WHERE c.group_id = p_group_id  
-        AND c.election_id = p_election_id
-    LOOP
-        RETURN QUERY
-        SELECT 
-            candidate_record.candidate_id,
-            candidate_record.group_id,
-            candidate_record.election_id,
-            ( SELECT u.full_name FROM users u 
-              WHERE u.id = candidate_record.candidate_id
-            ) AS candidate_name,
-            candidate_record.created_at;
-    END LOOP;
-
-    RETURN;
+    
+    RETURN QUERY
+    SELECT u.id AS candidate_id, u.full_name
+    FROM users u
+    WHERE u.id IN (
+      SELECT c.candidate_id
+      FROM candidates c
+      WHERE c.group_id =  p_group_id  
+      AND c.election_id = p_election_id 
+    );
 END;
 $$ LANGUAGE plpgsql;
 
