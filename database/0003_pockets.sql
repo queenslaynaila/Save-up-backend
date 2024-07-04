c2a98ae61ee2aaa1183a0e44528c0ec5854541a6
queenslaynaila <queenslayjema@gmail.com>
2024-07-04 08:31:24 +0300

    Fix: Remove extra spacing between columns

--- database/0003_pockets.sql
+++ database/0003_pockets.sql
@@ -28,13 +28,13 @@ SELECT create_distributed_table('pockets', 'entity_id');
 GRANT INSERT, SELECT, UPDATE ON pockets TO app_user;
 
 CREATE TABLE IF NOT EXISTS pocket_type_history (
-    entity_id             INT NOT NULL,
-    pocket_id             INT NOT NULL,
-    xid                   INT NOT NULL,
-    pocket_type           enum_pocket_type NOT NULL,
-    created_at            TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
-    PRIMARY KEY           (entity_id, pocket_id, xid), 
-    FOREIGN KEY           (entity_id, pocket_id) REFERENCES pockets(entity_id, xid)
+  entity_id             INT NOT NULL,
+  pocket_id             INT NOT NULL,
+  xid                   INT NOT NULL,
+  pocket_type           enum_pocket_type NOT NULL,
+  created_at            TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+  PRIMARY KEY           (entity_id, pocket_id, xid), 
+  FOREIGN KEY           (entity_id, pocket_id) REFERENCES pockets(entity_id, xid)
 );
 SELECT create_distributed_table('pocket_type_history', 'entity_id');
 GRANT INSERT, SELECT ON pocket_type_history TO app_user;
