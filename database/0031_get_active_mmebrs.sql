67143c1134d4842b5bc0237aa5b6529e22d220fe
queenslaynaila <queenslayjema@gmail.com>
2024-07-04 11:13:31 +0300

    Refactor: Replace loop with subquery

--- database/0031_get_active_mmebrs.sql
+++ database/0031_get_active_mmebrs.sql
@@ -1,4 +1,3 @@
---Use loop to avoid repartinioning
 CREATE OR REPLACE FUNCTION get_active_group_members(
     p_group_id   INT
 )
@@ -6,21 +5,16 @@ RETURNS TABLE(
     user_id      INT, 
     full_name    TEXT
 ) AS $$
-DECLARE
-    rec_user    RECORD;
 BEGIN
-    FOR rec_user IN
+    RETURN QUERY
+    SELECT u.id AS user_id, u.full_name
+    FROM users u
+    WHERE u.id IN (
         SELECT gm.user_id
         FROM group_members gm
-        WHERE gm.group_id = p_group_id 
+        WHERE gm.group_id = p_group_id
         AND gm.is_active = TRUE
-    LOOP
-        SELECT u.id, u.full_name INTO user_id, full_name
-        FROM users u
-        WHERE u.id = rec_user.user_id;
-
-        RETURN NEXT;
-    END LOOP;
+    );
 END;
 $$ LANGUAGE plpgsql;
 
