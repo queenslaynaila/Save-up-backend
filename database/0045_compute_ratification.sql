2059330031a803878e2a94c9bddab664ac9d18b9
queenslaynaila <queenslayjema@gmail.com>
2024-07-04 13:04:56 +0300

    Refactor: Use subquery instead of loop to simplify func

--- database/0045_compute_ratification.sql
+++ database/0045_compute_ratification.sql
@@ -8,42 +8,35 @@ RETURNS TABLE (
     ratification_status  BOOLEAN
 ) AS $$
 DECLARE
-    v_total_active_members             INT;
-    v_required_approval_count   INT;
-    v_approval_count            RECORD;
+    v_total_active_members       INT;
+    v_required_approval_count    INT;
+    v_approval_count             INT;
 BEGIN
-  PERFORM check_grp_membership(p_user_id, p_group_id);
+    PERFORM check_grp_membership(p_user_id, p_group_id);
 
-    SELECT COUNT(*) INTO v_total_active_members 
+    SELECT COUNT(*) INTO STRICT v_total_active_members 
     FROM group_members 
     WHERE group_id = p_group_id 
     AND is_active = TRUE;
 
-    v_required_approval_count := v_total_active_members / 2 + 1;  ---shld be more than 50%
+    v_required_approval_count := v_total_active_members / 2 + 1;
 
-    FOR approval_counts IN
-        SELECT COUNT(*) INTO STRICT v_approval_count 
-        FROM ratifications
-        WHERE group_id =  p_group_id  
-        AND election_id =  p_election_id
-        AND is_ratified = TRUE;
-    LOOP
-        IF approval_counts.v_approval_count >= v_required_approval_count THEN
-            RETURN QUERY
-            SELECT u.full_name, TRUE
-            FROM users u
-            JOIN group_admins ga ON u.id = ga.user_id
-            WHERE ga.group_id = p_group_id
-            AND ga.election_id = p_election_id;
-        ELSE
-            RETURN QUERY
-            SELECT u.full_name, FALSE
-            FROM users u
-            JOIN group_admins ga ON u.id = ga.user_id
-            WHERE ga.group_id = p_group_id
-            AND ga.election_id = p_election_id;
-        END IF;
-    END LOOP;
+    SELECT COUNT(*) INTO STRICT v_approval_count 
+    FROM ratifications
+    WHERE group_id = p_group_id  
+    AND election_id = p_election_id
+    AND is_ratified = TRUE;
+
+    RETURN QUERY
+    SELECT u.full_name,
+           (v_approval_count >= v_required_approval_count) AS ratification_status
+    FROM users u
+    WHERE u.id IN (
+        SELECT ga.user_id
+        FROM group_admins ga
+        WHERE ga.group_id =  p_group_id 
+        AND ga.election_id = p_election_id
+    );
 END;
 $$ LANGUAGE plpgsql;
 
