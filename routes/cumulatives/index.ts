af1a3864cc461df4c22f1aa579ec9736d363e9da
queenslaynaila <queenslayjema@gmail.com>
2024-07-12 10:56:41 +0300

    Fix: file path

--- routes/cumulatives/index.ts
+++ routes/cumulatives/index.ts
@@ -1,5 +1,4 @@
 import express from 'express';
-import getTotalUserTargetGoals from './getTotalUserTargetGoals';
 import getTotalUserDeposits from './getTotalUserDeposits';
 import getTotalUserExpenditure from './getTotalUserExpenditure';
 import getTopExpenseCategories from './getTopExpenseCategories';
@@ -7,7 +6,6 @@ import getTopExpenseCategories from './getTopExpenseCategories';
 export default (baseRouter: express.Router) => {
   const router = express.Router();
   
-  getTotalUserTargetGoals(router);
   getTotalUserDeposits(router);
   getTotalUserExpenditure(router);
   getTopExpenseCategories(router);
