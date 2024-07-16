166f876701a7e73d2408ce0060fbda84c9170f03
Queenslay Jema <queenslayjema@gmail.com>
2024-07-16 10:39:22 +0300

    Feat: annotate routes for api doc generation

--- routes/categories/generateDoc.ts
+++ routes/categories/generateDoc.ts
@@ -1,65 +1,80 @@
-// import { z } from "zod";
-// import * as yaml from 'yaml';
-// import * as fs from 'fs';
-// import { OpenAPIRegistry, OpenApiGeneratorV3, extendZodWithOpenApi } from '@asteasolutions/zod-to-openapi';
-// import { createCategorySchema, categorySchema, categoriesArraySchema,  } from './types'
-// import {Method } from '../../globalTypes/index';
+// /* eslint-disable @typescript-eslint/no-unused-vars */
+// import { Router } from 'express';
+// import { z } from 'zod';
+// import { OpenAPIRegistry } from '@asteasolutions/zod-to-openapi';
+// import validateRequest from '../../middleware/validationMiddleware';
+// import authMiddleware from '../../middleware/authorization';
+// import getAllCategories from './getAllCategories';
 
-// extendZodWithOpenApi(z);
-// const registry = new OpenAPIRegistry();
-
-  
-// const getAllCategories = {
-//   method: Method.GET,
-//   path: '/categories',
-//   summary: 'Returns an array of all categories object',
-//   tags: ['Categories'],
+// type RouteDefinition = {
+//   method: 'get' | 'post' | 'patch' | 'delete';
+//   path: string;
+//   summary: string;
+//   request: {
+//     headers?: z.ZodSchema;
+//     params?: z.ZodSchema;
+//     query?: z.ZodSchema;
+//     body?: z.ZodSchema;
+//     response?: z.ZodSchema;
+//   };
 //   responses: {
-//     200: {
-//       description: 'Categories retrieved successfully',
-//       content: {
-//         'application/json': {
-//           schema: categoriesArraySchema
-//         }
-//       }
-//     },
-//     401: {
-//       description:'Access Denied.Log in.'
-//     },
-//     500: {
-//       description: 'Internal server error.'
-//     }
-//   }
+//     [status: number]: {
+//       schema? : z.ZodSchema;
+//     };
+//   };
 // };
 
+// export const createRoute = (router: Router, registry: OpenAPIRegistry,routeDef: RouteDefinition) => {
+//   const { method, path, summary, description, request, responses } = routeDef;
 
-
-// registry.registerPath(getAllCategories);
-
-// function getOpenApiDocumentation() {
-//   const generator = new OpenApiGeneratorV3(registry.definitions);
-//   generator.generateComponents();
-
-//   return generator.generateDocument({
-//     info: {
-//       version: '1.0.0',
-//       title: 'CATEGORIES API',
-//       description: 'API for managing categories',
+//   registry.registerPath({
+//     method,
+//     path,
+//     summary,
+//     description,
+//     request: {
+//       headers: request.headers? {
+//         content: {
+//           'application/json': {
+//             schema: request.body,
+//           },
+//         },
+//       }: undefined,
+//       params: request.params ? {
+//         content: {
+//           'application/json': {
+//             schema: request.body,
+//           },
+//         },
+//       }: undefined,
+//       query: request.query ? {
+//         content: {
+//           'application/json': {
+//             schema: request.body,
+//           },
+//         },
+//       }: undefined,
+//       body: request.body? {
+//         content: {
+//           'application/json': {
+//             schema: request.body,
+//           },
+//         },
+//       }: undefined
 //     },
-//     tags: [
-//       { name: 'Categories', description: 'Endpoints for managing categories' },
-//     ],
-//     openapi: ""
+//     responses: Object.fromEntries(
+//       Object.entries(responses).map(([status, { description, schema }]) => [
+//         status,
+//         {
+//           description,
+//           content: {
+//             'application/json': {
+//               schema,
+//             },
+//           },
+//         },
+//       ])
+//     ),
 //   });
-// }
+// };
 
-// function writeDocumentation() {
-//   const docs = getOpenApiDocumentation();
-//   const fileContent = yaml.stringify(docs);
-
-//   fs.writeFileSync(`${__dirname}/swagger.yml`, fileContent, {
-//     encoding: 'utf-8',
-//   });
-// }
-
-// writeDocumentation();
