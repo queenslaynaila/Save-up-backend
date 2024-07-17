a0d3d112e07f7304bf2d10c28c5a828524b62302
Queenslay Jema <queenslayjema@gmail.com>
2024-07-17 10:31:37 +0300

    Refactor: Remove header validation

--- routes/categories/getAllCategories.ts
+++ routes/categories/getAllCategories.ts
@@ -1,9 +1,7 @@
 import { Router } from 'express';
 import { sql } from '../../db';
 import authMiddleware from '../../middleware/authorization';
-import validateRequest from '../../middleware/validationMiddleware';
 import { CategoryInterface } from './types';
-import { headersSchema } from '../../globalTypes';
 
 const SQL_GET_ALL_CATEGORIES = sql<Record<string,never>, CategoryInterface>(`
   SELECT id, name, description, image_url 
@@ -15,7 +13,6 @@ export default (router: Router) => {
   router.get<Record<string,never>, CategoryInterface[], Record<string,never>, 
   Record<string,never>>(
     '/',
-    validateRequest({ headers:headersSchema }),
     authMiddleware(),
     async (_req, res) => {
       const categories = await SQL_GET_ALL_CATEGORIES({}).many();
