f1877f67119b9596bef7942ce37b73084e0532c8
Queenslay Jema <queenslayjema@gmail.com>
2024-07-17 09:49:47 +0300

    Refactor: Remove header validation Fix: Use the targeted user id instead of the logged in user id to update role Fix: Retrieve full name from user contact details instead of users Feat: Return full name and new role of updated user

--- database/0010_update_user_role.sql
+++ database/0010_update_user_role.sql
@@ -2,12 +2,15 @@ CREATE OR REPLACE FUNCTION update_user_role(
     p_target_user_id    INT,
     p_new_role          enum_user_role,
     p_admin_id          INT 
-) RETURNS TABLE (
+) 
+RETURNS TABLE (
     full_name     TEXT,
     new_role      enum_user_role
 ) AS $$
 DECLARE
+    v_full_name      TEXT;
     v_old_role       enum_user_role;
+    v_new_role       enum_user_role;
 BEGIN
     IF NOT EXISTS (
        SELECT 1
@@ -20,27 +23,27 @@ BEGIN
             ERRCODE = 'P0002';
     END IF;
 
-    SELECT users.full_name, users.role 
-    INTO STRICT full_name, v_old_role
+    SELECT user_contact_details.full_name, users.role 
+    INTO STRICT v_full_name, v_old_role
     FROM users 
-    WHERE users.id = p_user_id;
+    INNER JOIN user_contact_details
+    ON users.id = user_contact_details.id
+    WHERE users.id = p_target_user_id;
 
     INSERT INTO user_role_history(user_id, xid, role)
     SELECT 
-        p_user_id, 
+        p_target_user_id , 
         COALESCE(MAX(xid), 0) + 1, 
         v_old_role
     FROM user_role_history
-    WHERE user_id = p_user_id;
+    WHERE user_id = p_target_user_id ;
 
     UPDATE users
     SET role = p_new_role
-    WHERE id = p_user_id
-    RETURNING role INTO STRICT new_role;
+    WHERE id = p_target_user_id 
+    RETURNING role INTO STRICT v_new_role;
     
-    RETURN QUERY SELECT u.full_name, new_role
-    FROM users u
-    WHERE u.id = p_user_id;
+    RETURN QUERY SELECT v_full_name, v_new_role;
 END;
 $$ LANGUAGE plpgsql;
 
--- routes/users/updateUserRole.ts
+++ routes/users/updateUserRole.ts
@@ -3,34 +3,35 @@ import { sql } from '../../db';
 import validateRequest from '../../middleware/validationMiddleware';
 import authMiddleware from '../../middleware/authorization';
 import { convertToTitleCase } from '../../middleware/caseNormalization';
-import { UserRoleUpdateType, UserRoleParamType, 
-  userRoleParamSchema
+import { 
+  UserRoleUpdateType, 
+  UserRoleParamType, 
+  userRoleParamSchema,
+  UpdatedUser
 } from './types';
-import { UserRole, StatusCodeInterface, 
-  headersSchema 
-} from '../../globalTypes/index';
+import { UserRole } from '../../globalTypes/index';
 
-const SQL_UPDATE_ROLE = sql<UserRoleUpdateType, Record<string,never>>(`
+const SQL_UPDATE_ROLE = sql<UserRoleUpdateType, UpdatedUser>(`
   SELECT * FROM update_user_role(:targetUserId,:role, :adminId);
 `);
 
 export default (router: Router) => {
-  router.patch<UserRoleParamType, StatusCodeInterface, Record<string, never>, 
+  router.patch<UserRoleParamType, UpdatedUser, Record<string, never>, 
   Record<string, never>>(
     '/:id/:role',
     validateRequest({ 
-      headers: headersSchema, params:userRoleParamSchema
+      params:userRoleParamSchema
     }),
     authMiddleware({ roles: [UserRole.ADMIN] }),
     async (req, res) => {
       const role = convertToTitleCase(req.params.role);
       const targetUserId = req.params.id;
       
-      await SQL_UPDATE_ROLE({
+      const user = await SQL_UPDATE_ROLE({
         role, targetUserId, adminId: req.user!.id 
-      }).exec();
-  
-      res.sendStatus(204);
+      }).one();
+
+      res.json(user);
     }
   );
 };
