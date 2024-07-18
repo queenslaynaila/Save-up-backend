f080380ec29354a5b28bc81cf8f80a39b91ce4e4
Queenslay Jema <queenslayjema@gmail.com>
2024-07-18 12:20:35 +0300

    Feat: Find if user is on the app before sending an invite

--- database/0027_send_invite.sql
+++ database/0027_send_invite.sql
@@ -7,15 +7,17 @@ RETURNS VOID AS $$
 DECLARE
     v_receiver_id   INT;
 BEGIN 
-    SELECT id INTO STRICT v_receiver_id
+    SELECT id 
+    INTO STRICT v_receiver_id
     FROM user_contact_details 
     WHERE phone_number = p_phone_number; 
 
     INSERT INTO invitations (group_id, xid, sender_id, receiver_id)
-    SELECT p_group_id, 
-           COALESCE(MAX(xid), 0) + 1, 
-           p_sender_id,
-           v_receiver_id
+    SELECT 
+        p_group_id, 
+        COALESCE(MAX(xid), 0) + 1, 
+        p_sender_id,
+        v_receiver_id
     FROM invitations
     WHERE group_id = p_group_id;
 END;
--- routes/invitations/sendInvite.ts
+++ routes/invitations/sendInvite.ts
@@ -9,6 +9,13 @@ import {
 import { StatusCodeInterface } from '../../globalTypes';
 import validateRequest from '../../middleware/validationMiddleware';
 
+const SQL_CHECK_USER_EXISTENCE = sql<{ phone_number: string }, { exists: boolean }>(`
+  SELECT EXISTS (
+    SELECT 1 FROM user_contact_details 
+    WHERE phone_number = :phone_number
+  ) AS exists
+`);
+
 const SQL_SEND_INVITATION = sql<InviteInputInterface, Record<string,never>>(`
   SELECT send_invite( :group_id, :phone_number, :sender_id)
 `);
@@ -22,6 +29,15 @@ export default (router: Router) => {
     }),
     authMiddleware(),
     async (req, res) => {
+      const { exists } = await SQL_CHECK_USER_EXISTENCE({ 
+        phone_number:req.body.phone_number 
+      }).one();
+
+      if (!exists) {
+        res.sendStatus(404);
+        return;
+      }
+
       await SQL_SEND_INVITATION({
         ...req.body, sender_id:req.user!.id 
       }).exec()
