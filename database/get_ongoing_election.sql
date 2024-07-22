313d2d38b2350553e050471a3cd9c1f42545126b
Queenslay Jema <queenslayjema@gmail.com>
2024-07-22 10:39:09 +0300

    Refactor: REplace aliases with table names

--- database/get_ongoing_election.sql
+++ database/get_ongoing_election.sql
@@ -1,38 +1,53 @@
-CREATE OR REPLACE FUNCTION get_open_election_for_group(
-    p_group_id   INT,
-    p_user_id    INT
+CREATE OR REPLACE FUNCTION get_ongoing_election(
+    p_group_id       INT,
+    p_user_id        INT
 )
-RETURNS TABLE(
-    group_id        INT,
-    election_id     INT,
-    initiator_id    INT,
-    type            TEXT,
-    initiator_name  TEXT,
-    created_at      TIMESTAMP WITH TIME ZONE
+RETURNS TABLE (
+    group_id            INT,
+    election_id         INT,
+    initiator_id        INT,
+    type                TEXT,
+    created_at          TIMESTAMP WITH TIME ZONE,
+    initiator_name      TEXT
 ) AS $$
 DECLARE
-    v_initiator_name      TEXT;
-    v_group_id            INT;
-    v_election_id         INT;
-    v_initiator_id        INT;
-    v_type                TEXT;
-    v_created_at          TIMESTAMP WITH TIME ZONE;
+    v_group_id              INT;
+    v_election_id           INT;
+    v_initiator_id          INT;
+    v_type                  TEXT;
+    v_created_at            TIMESTAMP WITH TIME ZONE;
+    v_initiator_name   TEXT;
 BEGIN
     PERFORM check_grp_membership(p_group_id, p_user_id);
-    
-    SELECT e.group_id, e.xid AS election_id, e.initiator_id, e.type, e.created_at
-    INTO STRICT v_group_id, v_election_id, v_initiator_id, v_type, v_created_at
-    FROM elections e
-    WHERE e.group_id = p_group_id
-    AND e.status = 'Open'
-    ORDER BY e.xid DESC
-    LIMIT 1;
 
-    SELECT u.full_name INTO STRICT v_initiator_name
-    FROM users u
-    WHERE u.id = initiator_id;
+    SELECT 
+        elections.group_id, 
+        elections.xid AS election_id, 
+        elections.initiator_id, 
+        elections.type,
+        elections.created_at,
+        user_contact_details.full_name AS initiator_name
+    INTO STRICT 
+        v_group_id, 
+        v_election_id, 
+        v_initiator_id, 
+        v_type, 
+        v_created_at, 
+        v_initiator_name
+    FROM elections
+    JOIN user_contact_details 
+    ON elections.initiator_id = user_contact_details.id
+    WHERE elections.group_id = p_group_id
+    AND elections.status = 'Open';
 
-    RETURN NEXT;
+    RETURN QUERY 
+    SELECT 
+        v_group_id, 
+        v_election_id, 
+        v_initiator_id, 
+        v_type, 
+        v_created_at, 
+        v_initiator_name;
 END;
 $$ LANGUAGE plpgsql;
 
