9d331d2801fd466f932c7306583a26737045fb4d
Queenslay Jema <queenslayjema@gmail.com>
2024-07-22 10:40:05 +0300

    FEat: get transaction by id

---
+++ routes/groupTransactions/getTransactionById.ts
@@ -0,0 +1,36 @@
+import { Router } from 'express';
+import { sql } from '../../db';
+import authMiddleware from '../../middleware/authorization';
+import {
+  TransactionRecipients,
+  TransactionDetails,
+  TransactionInput,
+  transactionInput,
+} from './types';
+import validateRequest from '../../middleware/validationMiddleware';
+import { IdParamInterface, idParamSchema } from '../../globalTypes';
+
+const SQL_GROUP_TRANSACTIONS = sql<TransactionRecipients, TransactionDetails >(`
+  SELECT * FROM get_group_transaction_details(
+    :user_id, :group_id, :transaction_id
+);
+`);
+
+export default (router: Router) => {
+  router.get<IdParamInterface, TransactionDetails[], TransactionInput, 
+  Record<string,never>>(
+    '/:id', 
+    validateRequest({
+      params:idParamSchema, 
+      body:transactionInput
+    }),
+    authMiddleware(),
+    async (req, res) => {
+      const members = await SQL_GROUP_TRANSACTIONS({ 
+        user_id: req.user!.id, 
+        group_id: req.body.group_id, 
+        transaction_id: parseInt(req.params.id)
+      }).many();  
+      return res.json(members);
+    });
+};
