aea51273578d8ac5e8625dc249ae052466062635
Queenslay Jema <queenslayjema@gmail.com>
2024-07-22 10:37:43 +0300

    Refactor: Move code to new lines to prevent overflow

--- database/0042_get_candidates.sql
+++ database/0042_get_candidates.sql
@@ -10,24 +10,27 @@ RETURNS TABLE(
 DECLARE
     v_user_role   enum_user_role;
 BEGIN
-    SELECT role INTO STRICT v_user_role
-    FROM users
-    WHERE id = p_user_id;
+  SELECT users.role 
+  INTO STRICT v_user_role
+  FROM users
+  WHERE users.id = p_user_id;
 
-    IF v_user_role != 'Admin' THEN
-      PERFORM check_grp_membership(p_group_id, p_user_id);
-    END IF;
+  IF v_user_role != 'Admin' THEN
+    PERFORM check_grp_membership(p_group_id, p_user_id);
+  END IF;
     
-    RETURN QUERY
-    SELECT ucd.id AS candidate_id, ucd.full_name
-    FROM candidates c
-    JOIN user_contact_details ucd ON ucd.id = c.candidate_id
-    WHERE c.group_id = p_group_id  
-    AND c.election_id = p_election_id;
+  RETURN QUERY
+  SELECT user_contact_details.id AS candidate_id, 
+         user_contact_details.full_name
+  FROM candidates
+  JOIN user_contact_details 
+  ON user_contact_details.id = candidates.candidate_id
+  WHERE candidates.group_id = p_group_id  
+  AND candidates.election_id = p_election_id;
 END;
 $$ LANGUAGE plpgsql;
 
-GRANT EXECUTE ON FUNCTION get_candidates(INT, INT) TO app_user;
+GRANT EXECUTE ON FUNCTION get_candidates(INT, INT, INT) TO app_user;
 SELECT create_distributed_function(
-  'get_candidates(INT, INT)', 'p_group_id'
+  'get_candidates(INT, INT, INT)', 'p_group_id'
 );
