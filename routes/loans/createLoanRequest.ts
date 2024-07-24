2b30c44a004cc95257131734af461b0988a7a84f
Queenslay Jema <queenslayjema@gmail.com>
2024-07-24 11:44:58 +0300

    Fix: Use perform instead of select since function returns void

--- routes/loans/createLoanRequest.ts
+++ routes/loans/createLoanRequest.ts
@@ -2,20 +2,22 @@ import { Router } from 'express';
 import { sql } from '../../db';
 import authMiddleware from '../../middleware/authorization';
 import validateRequest from '../../middleware/validationMiddleware';
-import { BaseRequestLoan, RequestLoan, requestLoanSchema } from './type';
+import { BaseRequestLoan, RequestLoan, requestLoanSchema } from './types';
 
 const SQL_REQUEST_LOAN = sql<BaseRequestLoan, Record<string, never>>(`
-  SELECT * FROM request_loan(:group_id, :pocket_id, :borrower_id, :guarantor_id, :amount, :reason, :period)
+  PERFORM request_loan(:group_id, :pocket_id, :borrower_id, :guarantor_id, :amount, :reason, :period)
 `);
 
 export default (router: Router) => {
-  router.post<Record<string,never>, Record<string,never>, RequestLoan, Record<string,never>>(
+  router.post<Record<string,never>, Record<string,never>, RequestLoan,
+  Record<string,never>>(
     '/',
-    validateRequest({body:requestLoanSchema}),
+    validateRequest({ body:requestLoanSchema }),
     authMiddleware(),
     async (req, res) => {
       await SQL_REQUEST_LOAN({
-        ...req.body, borrower_id:req.user!.id
+        ...req.body,
+        borrower_id:req.user!.id
       }).exec()
       return res.sendStatus(204);
     }
