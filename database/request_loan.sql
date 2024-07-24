3f9a20566ef74293d088b37b768655b04c9d1fdb
Queenslay Jema <queenslayjema@gmail.com>
2024-07-24 10:50:05 +0300

    Feat: Add request loan

---
+++ database/request_loan.sql
@@ -0,0 +1,65 @@
+CREATE OR REPLACE FUNCTION request_loan (
+    p_group_id           INT,
+    p_pocket_id          INT,
+    p_borrower_id        INT,
+    p_guarantor_id       INT,
+    p_amount             NUMERIC(30, 2),
+    p_reason             TEXT,
+    p_repayment_period   INTERVAL
+)
+RETURNS VOID AS $$
+DECLARE
+    v_current_balance           NUMERIC(30, 2);
+    v_loan_limit                NUMERIC(30, 2);
+BEGIN
+    SELECT get_transaction_info.v_current_balance
+    INTO STRICT v_current_balance
+    FROM get_transaction_info(p_group_id, p_pocket_id);
+
+    IF v_current_balance < p_amount THEN
+        RAISE EXCEPTION USING
+            MESSAGE = 'INSUFFICIENT FUNDS',
+            ERRCODE = 'P0004';
+    END IF;
+
+    IF NOT EXISTS (
+        SELECT 1
+        FROM transactions
+        WHERE entity_id = p_guarantor_id
+          AND pocket_id = p_pocket_id
+          AND type_id = 1
+    ) THEN
+        RAISE EXCEPTION USING
+            MESSAGE = 'GUARANTOR_NO_CONTRIBUTION',
+            ERRCODE = 'P0005';
+    END IF;
+
+    SELECT calculate_loan_limit.v_loan_limit
+    INTO STRICT v_loan_limit
+    FROM calculate_loan_limit(p_pocket_id, p_borrower_id);
+
+    IF p_amount > v_loan_limit THEN
+        RAISE EXCEPTION USING
+            MESSAGE = 'ERR_EXCEEDS_LOAN_LIMIT',
+            ERRCODE = 'P0007';
+    END IF;
+
+    INSERT INTO loan_requests(group_id, xid, borrower_id, guarantor_id, pocket_id, amount, purpose, repayment_period)
+    SELECT
+        p_group_id,
+        COALESCE(MAX(xid), 0) + 1,
+        p_borrower_id,
+        p_guarantor_id,
+        p_pocket_id,
+        p_amount,
+        p_reason,
+        p_repayment_period
+    FROM loan_requests
+    WHERE group_id = p_group_id;
+END;
+$$ LANGUAGE plpgsql;
+
+GRANT EXECUTE ON FUNCTION request_loan(INT, INT, INT, INT, NUMERIC, TEXT, INTERVAL) TO app_user;
+SELECT create_distributed_function(
+    'request_loan(INT, INT, INT, INT, NUMERIC, TEXT, INTERVAL)', 'p_group_id'
+);
---
+++ routes/loans/createLoanRequest.ts
@@ -0,0 +1,23 @@
+import { Router } from 'express';
+import { sql } from '../../db';
+import authMiddleware from '../../middleware/authorization';
+import validateRequest from '../../middleware/validationMiddleware';
+import { BaseRequestLoan, RequestLoan, requestLoanSchema } from './type';
+
+const SQL_REQUEST_LOAN = sql<BaseRequestLoan, Record<string, never>>(`
+  SELECT * FROM request_loan(:group_id, :pocket_id, :borrower_id, :guarantor_id, :amount, :reason, :period)
+`);
+
+export default (router: Router) => {
+  router.post<Record<string,never>, Record<string,never>, RequestLoan, Record<string,never>>(
+    '/',
+    validateRequest({body:requestLoanSchema}),
+    authMiddleware(),
+    async (req, res) => {
+      await SQL_REQUEST_LOAN({
+        ...req.body, borrower_id:req.user!.id
+      }).exec()
+      return res.sendStatus(204);
+    }
+  );
+};
