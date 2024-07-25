5237d3f1ccb8d56c6d4ef44db5d5c9a714e533ff
Queenslay Jema <queenslayjema@gmail.com>
2024-07-25 11:42:54 +0300

    Refactor: Update transaction_logs.sql to include new transaction types and grant permissions to app_user

--- database/0017_transaction_logs.sql
+++ database/0017_transaction_logs.sql
@@ -1,18 +1,21 @@
 CREATE TYPE enum_transaction_type AS ENUM (
-  'Saving', 
-  'ExternalSaving', 
-  'Withdrawal', 
-  'TransferIn', 
-  'TransferOut', 
+  'Saving',
+  'ExternalSaving',
   'Interest',
-  'Penalty' --when a user withdraws locked funds before maturity
+  'Withdrawal',
+  'Penalty'
+  'TransferIn',
+  'TransferOut',
+  'Loan',
+  'Repayment'
 );
 
 CREATE TABLE IF NOT EXISTS transaction_types (
   id          SERIAL PRIMARY KEY,
   slug        enum_transaction_type NOT NULL,
   created_at  TIMESTAMP WITH TIME ZONE DEFAULT NOW()
-); 
+);
+GRANT SELECT ON transaction_types TO app_user;
 SELECT create_reference_table('transaction_types');
 
 CREATE TABLE IF NOT EXISTS transactions (
@@ -27,10 +30,10 @@ CREATE TABLE IF NOT EXISTS transactions (
   PRIMARY KEY             (entity_id, xid, type_id, pocket_id),
   FOREIGN KEY             (type_id) REFERENCES transaction_types(id)
 );
-
-GRANT INSERT, SELECT ON transactions TO app_user;
 SELECT create_distributed_table('transactions', 'entity_id');
 
 ALTER TABLE transactions
 ADD CONSTRAINT transactions_pocket_id_fkey  
 FOREIGN KEY  (entity_id, pocket_id) REFERENCES pockets (entity_id, xid);
+
+GRANT SELECT, INSERT ON transaction_types TO app_user;
