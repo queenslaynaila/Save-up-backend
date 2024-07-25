3d948db118f3fd4721d117d9d122cdb9185e9426
Queenslay Jema <queenslayjema@gmail.com>
2024-07-25 11:45:29 +0300

    Refactor: Update group deposit function to simplify and optimize code

--- database/0038_create_group_deposit.sql
+++ database/0038_create_group_deposit.sql
@@ -15,32 +15,25 @@ BEGIN
     v_current_balance := get_transaction_info(p_group_id, p_pocket_id);
     v_new_balance = v_current_balance + p_amount;
     v_reference_id := floor(random() * 1000000 + 1)::INT;
-
-    SELECT * FROM insert_transaction_log(
+    v_transaction_id := insert_transaction_log(
         p_group_id,
         1,
         p_pocket_id,
         v_reference_id,
         p_amount,
         v_new_balance
-    ) INTO v_transaction_id;
+    );
 
     INSERT INTO group_deposits (group_id, deposit_id, user_id)
     VALUES (p_group_id, v_transaction_id, p_user_id);
 
-    SELECT pockets.target_amount INTO v_target_amount  
-    FROM pockets 
-    WHERE entity_id = p_group_id 
-    AND xid = p_pocket_id
-    AND status = 'In Progress';
-    
-    IF v_target_amount IS NOT NULL AND v_new_balance >= v_target_amount THEN
-        UPDATE pockets
-        SET status = 'Completed'::enum_status,
-            completed_at = NOW()
-        WHERE entity_id = p_group_id
-        AND xid = p_pocket_id;
-    END IF;
+    UPDATE pockets
+    SET status = 'Completed'::enum_status,
+        completed_at = NOW()
+    WHERE entity_id = p_group_id
+      AND xid = p_pocket_id
+      AND status = 'In Progress'::enum_status
+      AND v_current_balance >= pockets.target_amount;
 END;
 $$ LANGUAGE plpgsql;
 
