7fd9ebc9e21d5758a38ce4cd201c9b7f59adb93c
Queenslay Jema <queenslayjema@gmail.com>
2024-07-25 11:44:04 +0300

    Refactor: Simplify create_transfer function and improve error handling

--- database/0021_create_transfr.sql
+++ database/0021_create_transfr.sql
@@ -7,35 +7,27 @@ CREATE OR REPLACE FUNCTION create_transfer(
 )
 RETURNS VOID AS $$
 DECLARE
-  v_source_transaction_id         INT;
   v_source_balance                NUMERIC(30, 2);
-  v_destination_transaction_id    INT;
   v_destination_balance           NUMERIC (30,2);
   v_new_source_balance            NUMERIC(30, 2);
   v_new_destination_balance       NUMERIC(30, 2);
   v_reference_id                  INT;
-BEGIN 
-  SELECT get_transaction_info.v_current_balance 
-  INTO STRICT v_current_balance 
-  FROM get_transaction_info(p_entity_id, p_source_pocket_id);
-
-  IF v_current_balance < p_amount THEN
+BEGIN
+  v_source_balance := get_transaction_info(p_entity_id, p_source_pocket_id);
+  IF v_source_balance < p_amount THEN
     RAISE EXCEPTION USING
       MESSAGE = 'ERR_INSUFFICIENT_FUNDS',
       ERRCODE = 'P0004';
   END IF;
 
-  SELECT get_transaction_info.v_current_balance 
-  INTO STRICT v_destination_balance
-  FROM get_transaction_info( p_entity_id, p_destination_pocket_id);
-
+  v_destination_balance := get_transaction_info(p_entity_id, p_destination_pocket_id);
   v_new_source_balance =  v_source_balance - p_amount;
   v_new_destination_balance = v_destination_balance + p_amount;
   v_reference_id := floor(random() * 1000000 + 1)::INT;
 
    PERFORM insert_transaction_log(
         p_user_id,
-        4,
+        6,
         p_destination_pocket_id,
         v_reference_id,
         p_amount,
@@ -44,7 +36,7 @@ BEGIN
 
     PERFORM insert_transaction_log(
        p_user_id,
-       5,
+       7,
        p_source_pocket_id,
        v_reference_id,
        p_amount,
