cf9c68b2022fd2b4c415a89966daefa433a784b6
Queenslay Jema <queenslayjema@gmail.com>
2024-07-29 08:43:22 +0300

    Refactor: Update get_ongoing_election function to improve performance and readability
    - Renamed the function from 'approve_loan' to 'get_ongoing_election' to accurately reflect its purpose.
    - Removed unnecessary parameters and added new parameters for better functionality.
    - Optimized the query by joining tables and using appropriate conditions.
    - Updated the return table structure for better clarity.
    - Granted execute permission to 'app_user' for the updated function.

--- database/0027_get_ongoing_election.sql
+++ database/0027_get_ongoing_election.sql
@@ -1,37 +1,35 @@
-CREATE OR REPLACE FUNCTION approve_loan(
-    p_group_id      INT,
-    p_request_id    INT,
-    p_admin_id      INT,
-    p_status        enum_approval_status,
-    p_reason        TEXT
+CREATE OR REPLACE FUNCTION get_ongoing_election(
+    p_group_id       INT,
+    p_user_id        INT
 )
-RETURNS VOID AS $$
-DECLARE
-    v_latest_election_id INT;
+RETURNS TABLE (
+    group_id            INT,
+    election_id         INT,
+    type                TEXT,
+    initiator_id        INT,
+    initiator_name      TEXT,
+    created_at          TIMESTAMP WITH TIME ZONE
+) AS $$
 BEGIN
-    SELECT MAX(xid)
-    INTO STRICT v_latest_election_id
-    FROM elections
-    WHERE group_id = p_group_id
-      AND status = 'Closed';
+    PERFORM check_grp_membership(p_group_id, p_user_id);
 
-    IF NOT EXISTS (
-        SELECT 1
-        FROM group_admins
-        WHERE group_id = p_group_id
-          AND election_id = v_latest_election_id
-          AND user_id = p_admin_id
-    ) THEN
-        RAISE EXCEPTION 'ERR_GROUP_NOT_ADMIN',
-            'P0002';
-    END IF;
-
-    INSERT INTO loan_admin_approvals (group_id, request_id, admin_id, status, reason)
-    VALUES (p_group_id, p_request_id, p_admin_id, p_status, p_reason);
+    RETURN QUERY
+        SELECT
+            elections.group_id,
+            elections.xid AS election_id,
+            elections.type,
+            elections.initiator_id,
+            user_contact_details.full_name AS initiator_name,
+            elections.created_at
+        FROM elections
+            JOIN user_contact_details
+                ON elections.initiator_id = user_contact_details.id
+        WHERE elections.group_id = p_group_id
+          AND elections.status = 'Open';
 END;
 $$ LANGUAGE plpgsql;
 
-GRANT EXECUTE ON FUNCTION approve_loan( INT, INT, INT, enum_approval_status, TEXT ) TO app_user;
+GRANT EXECUTE ON FUNCTION get_ongoing_election(INT, INT) TO app_user;
 SELECT create_distributed_function(
-'approve_loan(INT, INT, INT, enum_approval_status, TEXT)', 'p_group_id'
+  'get_ongoing_election(INT, INT)', 'p_group_id'
 );
