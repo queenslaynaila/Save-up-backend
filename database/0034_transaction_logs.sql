1c83ed3fe29656906bf6e51ff23498f8a7401770
Queenslay Jema <queenslayjema@gmail.com>
2024-07-29 15:32:29 +0300

    Feat: Add insert query for transaction types

--- database/0034_transaction_logs.sql
+++ database/0034_transaction_logs.sql
@@ -12,9 +12,19 @@ CREATE TYPE enum_transaction_type AS ENUM (
 
 CREATE TABLE IF NOT EXISTS transaction_types (
   id          SERIAL PRIMARY KEY,
-  slug        enum_transaction_type NOT NULL,
+  slug        TEXT NOT NULL,
   created_at  TIMESTAMP WITH TIME ZONE DEFAULT NOW()
 );
+INSERT INTO transaction_types (slug)
+VALUES  ('Saving'),
+        ('ExternalSaving'),
+        ('Interest'),
+        ('Withdrawal'),
+        ('Penalty'),
+        ('TransferIn'),
+        ('TransferOut'),
+        ('Loan'),
+        ('Repayment');
 GRANT SELECT ON transaction_types TO app_user;
 SELECT create_reference_table('transaction_types');
 
