5688edf96ae3f41ce54b606d50ac9232528e7b2c
Queenslay Jema <queenslayjema@gmail.com>
2024-07-29 08:44:25 +0300

    Refactor: Optimize get_candidates SQL function - Rename variables for better clarity. - Use INNER JOIN instead of JOIN for better consistency. - Remove unnecessary spacing and clear empty lines. - Update table aliases for better readability.

--- database/0028_get_candidates.sql
+++ database/0028_get_candidates.sql
@@ -10,23 +10,23 @@ RETURNS TABLE(
 DECLARE
     v_user_role   enum_user_role;
 BEGIN
-  SELECT users.role 
-  INTO STRICT v_user_role
-  FROM users
-  WHERE users.id = p_user_id;
+    SELECT users.role
+    INTO STRICT v_user_role
+    FROM users
+    WHERE users.id = p_user_id;
 
-  IF v_user_role != 'Admin' THEN
-    PERFORM check_grp_membership(p_group_id, p_user_id);
-  END IF;
+    IF v_user_role != 'Admin' THEN
+        PERFORM check_grp_membership(p_group_id, p_user_id);
+    END IF;
     
-  RETURN QUERY
-  SELECT user_contact_details.id AS candidate_id, 
-         user_contact_details.full_name
-  FROM candidates
-  JOIN user_contact_details 
-  ON user_contact_details.id = candidates.candidate_id
-  WHERE candidates.group_id = p_group_id  
-  AND candidates.election_id = p_election_id;
+    RETURN QUERY
+        SELECT user_contact_details.id AS candidate_id,
+               user_contact_details.full_name
+        FROM candidates
+        INNER JOIN user_contact_details
+            ON user_contact_details.id = candidates.candidate_id
+        WHERE candidates.group_id = p_group_id
+            AND candidates.election_id = p_election_id;
 END;
 $$ LANGUAGE plpgsql;
 
