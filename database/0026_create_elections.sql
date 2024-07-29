34198f7df1da65606e6c4ea89543d2a94d53b60d
Queenslay Jema <queenslayjema@gmail.com>
2024-07-29 08:42:49 +0300

    Refcator: Replace get ongoing election with create election

--- database/0026_create_elections.sql
+++ database/0026_create_elections.sql
@@ -1,57 +1,36 @@
-CREATE OR REPLACE FUNCTION get_ongoing_election(
-    p_group_id       INT,
-    p_user_id        INT
+CREATE OR REPLACE FUNCTION create_election(
+    p_group_id        INT,
+    p_initiator_id    INT,
+    p_type            enum_election_type
 )
-RETURNS TABLE (
-    group_id            INT,
-    election_id         INT,
-    initiator_id        INT,
-    type                TEXT,
-    created_at          TIMESTAMP WITH TIME ZONE,
-    initiator_name      TEXT
-) AS $$
-DECLARE
-    v_group_id              INT;
-    v_election_id           INT;
-    v_initiator_id          INT;
-    v_type                  TEXT;
-    v_created_at            TIMESTAMP WITH TIME ZONE;
-    v_initiator_name   TEXT;
+RETURNS VOID AS $$
 BEGIN
-    PERFORM check_grp_membership(p_group_id, p_user_id);
+    IF EXISTS (
+        SELECT 1
+        FROM elections
+        WHERE group_id = p_group_id
+          AND status = 'Open'
+          AND closed_at IS NULL
+    ) THEN
+        RAISE EXCEPTION USING
+            MESSAGE = 'ERR_ONGOING_ELECTION_EXISTS',
+            ERRCODE = 'P0007';
+    END IF;
 
-    SELECT 
-        elections.group_id, 
-        elections.xid AS election_id, 
-        elections.initiator_id, 
-        elections.type,
-        elections.created_at,
-        user_contact_details.full_name AS initiator_name
-    INTO STRICT 
-        v_group_id, 
-        v_election_id, 
-        v_initiator_id, 
-        v_type, 
-        v_created_at, 
-        v_initiator_name
+    INSERT INTO elections (group_id, xid, initiator_id, type)
+    SELECT
+        p_group_id,
+        COALESCE(MAX(xid), 0) + 1,
+        p_initiator_id,
+        p_type
     FROM elections
-    JOIN user_contact_details 
-    ON elections.initiator_id = user_contact_details.id
-    WHERE elections.group_id = p_group_id
-    AND elections.status = 'Open';
+    WHERE group_id = p_group_id;
 
-    RETURN QUERY 
-    SELECT 
-        v_group_id, 
-        v_election_id, 
-        v_initiator_id, 
-        v_type, 
-        v_created_at, 
-        v_initiator_name;
+    RETURN;
 END;
 $$ LANGUAGE plpgsql;
 
-GRANT EXECUTE ON FUNCTION get_open_election_for_group(INT, INT) TO app_user;
+GRANT EXECUTE ON FUNCTION create_election(INT, INT, enum_election_type) TO app_user;
 SELECT create_distributed_function(
-  'get_open_election_for_group(INT, INT)', 'p_group_id'
+    'create_election(INT, INT, enum_election_type)', 'p_group_id'
 );
