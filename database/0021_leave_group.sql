d2b4aeb55a09d09b4c8bba1ed13fc6c366d16fd0
Queenslay Jema <queenslayjema@gmail.com>
2024-07-29 08:42:17 +0300

    Refactor: Remove check memerbship simce update does that in and caluse

--- database/0021_leave_group.sql
+++ database/0021_leave_group.sql
@@ -9,40 +9,40 @@ DECLARE
     v_member_count INT;
     v_user_name TEXT;
 BEGIN
-  PERFORM check_grp_membership(p_group_id, p_user_id);
+    UPDATE group_members
+    SET is_active = FALSE
+    WHERE group_id = p_group_id
+    AND user_id = p_user_id
+    AND is_active = TRUE;
 
-  UPDATE group_members
-  SET is_active = FALSE
-  WHERE group_id = p_group_id 
-  AND user_id = p_user_id;
+    INSERT INTO group_lefts (group_id, user_id, xid, reason)
+    SELECT
+        p_group_id,
+        p_user_id,
+        COALESCE(MAX(xid), 0) + 1,
+        'Self removal'::enum_exit_reason
+    FROM group_lefts
+    WHERE group_id = p_group_id;
 
-  INSERT INTO group_lefts (group_id, user_id, xid, reason)
-  SELECT 
-    p_group_id,
-    p_user_id,
-    COALESCE(MAX(xid), 0) + 1,
-    'Self removal'::enum_exit_reason
-  FROM group_lefts
-  WHERE group_id = p_group_id;
+    SELECT COUNT(*)
+    INTO STRICT v_member_count
+    FROM group_members
+    WHERE group_id = p_group_id
+    AND is_active = TRUE;
 
-  SELECT COUNT(*) 
-  INTO STRICT v_member_count
-  FROM group_members
-  WHERE group_id = p_group_id
-  AND is_active = TRUE;
+    IF v_member_count = 0 THEN
+        UPDATE groups
+        SET deleted_at = NOW()
+        WHERE id = p_group_id
+        AND deleted_at IS NULL;
+    END IF;
 
-  IF v_member_count = 0 THEN
-    UPDATE groups
-    SET deleted_at = NOW()
-    WHERE id = p_group_id;
-  END IF;  
+    SELECT full_name
+    INTO STRICT v_user_name
+    FROM user_contact_details
+    WHERE id = p_user_id;
 
-  SELECT full_name 
-  INTO STRICT v_user_name
-  FROM user_contact_details
-  WHERE id = p_user_id;
-
-  RETURN QUERY SELECT v_user_name;
+    RETURN QUERY SELECT v_user_name;
 END;
 $$ LANGUAGE plpgsql;
 
