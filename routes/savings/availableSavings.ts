108a2634daa20d06401f0258f736462b04d43a7a
Queenslay Jema <queenslayjema@gmail.com>
2024-07-30 10:02:25 +0300

    Fix: Rewrite query with a subquery to first get the current balance per pocket

--- routes/savings/availableSavings.ts
+++ routes/savings/availableSavings.ts
@@ -7,17 +7,19 @@ import { GetByUserInterface } from '../../globalTypes';
 
 //current balance across all pockets
 const  SQL_GET_AVAILABLE_SAVINGS = sql<GetByUserInterface, Balance>(`
-  SELECT delta AS available_balance
-  FROM transactions
-  WHERE entity_id = :user_id
-  ORDER BY created_at DESC
-  LIMIT 1;
+    SELECT COALESCE(SUM(balance), 0) AS available_savings
+    FROM (
+         SELECT DISTINCT ON (pocket_id) balance
+         FROM transactions
+         WHERE entity_id = :user_id
+         ORDER BY pocket_id, created_at DESC
+    ) AS current_balance_per_pocket;
 `);
 
 export default (router: Router) => {
   router.get<Record<string,never>, Balance, Record<string,never>, 
   Record<string,never>>(
-    '/available-savings', 
+    '/current-savings',
     authMiddleware(), 
     async (req, res) => {
       const result = await SQL_GET_AVAILABLE_SAVINGS({ 
