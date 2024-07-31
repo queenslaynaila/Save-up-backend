1bd2140fc6859b5816481b08459de219b396d8b5
Queenslay Jema <queenslayjema@gmail.com>
2024-07-31 11:36:37 +0300

    Feat: Add column status to debit requests

--- database/0041_group_transactions.sql
+++ database/0041_group_transactions.sql
@@ -23,19 +23,22 @@ CREATE TABLE IF NOT EXISTS debit_requests (
     group_id              INT NOT NULL,
     xid                   INT NOT NULL,
     election_id           INT NOT NULL,
-    requestor_id          INT NOT NULL,
+    initiator_id          INT NOT NULL,
     type_id               INT NOT NULL,
     pocket_id             INT NOT NULL,
     amount                NUMERIC(30, 2) NOT NULL CHECK (amount > 0),
-    reason                enum_withdrawal_reason NOT NULL,
+    reason                TEXT NOT NULL,
+    status                enum_approval_status NOT NULL DEFAULT 'Pending',
     created_at            TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
     PRIMARY KEY           (group_id, xid),
-    FOREIGN KEY           (group_id, requestor_id) REFERENCES group_members (group_id, user_id),
+    FOREIGN KEY           (group_id, initiator_id) REFERENCES group_members (group_id, user_id),
     FOREIGN KEY           (group_id, election_id) REFERENCES elections (group_id, xid),
     FOREIGN KEY           (type_id) REFERENCES debit_types (id)
 );
 GRANT INSERT, SELECT ON debit_requests TO app_user;
 SELECT create_distributed_table('debit_requests', 'group_id');
+ALTER TABLE debit_requests
+ADD FOREIGN KEY (type_id) REFERENCES debit_types (id);
 
 CREATE TABLE IF NOT EXISTS loan_details (
     group_id          INT NOT NULL,
@@ -78,7 +81,7 @@ CREATE TABLE IF NOT EXISTS debit_approvals (
     request_id            INT NOT NULL,
     admin_id              INT NOT NULL,
     election_id           INT NOT NULL,
-    status                enum_approval_status NOT NULL DEFAULT 'Pending',
+    status                enum_approval_status NOT NULL,
     reason                TEXT NOT NULL,
     created_at            TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
     PRIMARY KEY           (group_id, request_id, admin_id),
@@ -95,8 +98,8 @@ CREATE TABLE IF NOT EXISTS disbursements (
     user_id               INT NOT NULL,
     created_at            TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
     PRIMARY KEY           (group_id, transaction_id),
-    FOREIGN KEY           (group_id, request_id) REFERENCES debit_requests (group_id, xid),
-    FOREIGN KEY           (user_id, transaction_id) REFERENCES transactions (entity_id, xid)
+    FOREIGN KEY           (group_id, request_id) REFERENCES debit_requests (group_id, xid)
+    --FOREIGN KEY           (user_id, transaction_id) REFERENCES transactions (entity_id, xid)
 );
 GRANT INSERT, SELECT ON disbursements TO app_user;
 SELECT create_distributed_table('disbursements', 'group_id');
@@ -108,7 +111,7 @@ CREATE TABLE IF NOT EXISTS loan_repayments (
     request_id            INT NOT NULL,
     created_at            TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
     PRIMARY KEY           (group_id, transaction_id),
-    FOREIGN KEY           (user_id, transaction_id) REFERENCES transactions (entity_id, xid),
+    --FOREIGN KEY           (user_id, transaction_id) REFERENCES transactions (entity_id, xid),
     FOREIGN KEY           (group_id, request_id) REFERENCES debit_requests (group_id, xid)
 );
 GRANT INSERT, SELECT ON loan_repayments TO app_user;
