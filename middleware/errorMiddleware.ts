c6fc13ef39c8bda616de5710732273d9e9c799b0
Queenslay Jema <queenslayjema@gmail.com>
2024-10-22 13:07:26 +0300

    Refactor: Simplify validation error object to avoid nesting to main error object

--- middleware/errorMiddleware.ts
+++ middleware/errorMiddleware.ts
@@ -1,9 +1,9 @@
 export class HttpError extends Error {
   public readonly status: number;
-  public readonly errors:  Record<string, unknown>;
+  public readonly errors: Record<string, unknown> | Record<string, unknown>[];
 
-  constructor(status: number,errors: Record<string, unknown> = {}) {
-    super();
+  constructor(status: number, errors: Record<string, unknown> | Record<string, unknown>[] = {}) {
+    super(); 
     this.status = status;
     this.errors = errors;
   }
