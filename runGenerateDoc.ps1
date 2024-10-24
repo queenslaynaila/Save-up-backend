dfcaae1cd87064ef44e9629701db95607289c800
Queenslay Jema <queenslayjema@gmail.com>
2024-10-24 10:14:36 +0300

    Chore: Clean dead files

--- runGenerateDoc.ps1
+++
@@ -1,7 +0,0 @@
-# Find all files named "generateDoc.ts" in the current directory and its subdirectories
-$files = Get-ChildItem -Recurse -Filter generateDoc.ts
-
-# Loop through each file and execute ts-node
-foreach ($file in $files) {
-    & npx ts-node $file.FullName
-}
