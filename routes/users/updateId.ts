19b5b628f346a6745f3caa3e56a736997393280a
Queenslay Jema <queenslayjema@gmail.com>
2024-10-29 08:49:52 +0300

    Refactor: Update updateId route to include validation for updateDetails

--- routes/users/updateId.ts
+++ routes/users/updateId.ts
@@ -4,7 +4,7 @@ import { z } from 'zod';
 import { sql } from '../../db';
 import { HttpError } from '../../middleware/errorMiddleware';
 import authMiddleware from '../../middleware/authorization';
-// import validateRequest from '../../middleware/validationMiddleware';
+import validateRequest from '../../middleware/validationMiddleware';
 import { UserId } from './schema';
 
 const updateIdSchema = z.object({
@@ -26,10 +26,10 @@ const SQL_GET_USER_PIN = sql<{ id: number }, { pin: string }>(`
 `);
 
 const SQL_UPDATE_PHONE = sql<{ phone_number: string; id: number }, { updated_phone_number: string }>(`
-  SELECT update_phone_number(:id, :phone_number)
+  SELECT * FROM update_phone_number(:id, :phone_number)
 `);
 
-const SQL_UPDATE_ID_NUMBER = sql<{ id: number; id_type: string; id_number: string }, { updated_id: string }>(`
+const SQL_UPDATE_ID_NUMBER = sql<{ id: number; id_type: string; id_number: string }, { new_id_number: string }>(`
   SELECT * FROM update_id_number(:id, :id_type, :id_number)
 `);
 
@@ -37,18 +37,19 @@ export default (router: Router) => {
   router.patch<UserId, { updated_attribute: string }, UpdateDetailsType, Record<string, never>>(
     '/:user_id',
     authMiddleware(),
+    validateRequest({ body: updateDetailsSchema }),
     async (req, res) => {
       const userIdParam = req.params.user_id;
       const id = userIdParam === 'me' ? req.user!.id : Number(userIdParam);
 
       if ('new_id_number' in req.body && 'id_type' in req.body) {
         const { id_type, new_id_number } = req.body;
-        const { updated_id } = await SQL_UPDATE_ID_NUMBER({
+        const { new_id_number: id_number } = await SQL_UPDATE_ID_NUMBER({
           id,
           id_type,
           id_number: new_id_number
         }).one();
-        return res.json({ updated_attribute: updated_id });
+        return res.json({ updated_attribute: id_number });
       }
 
       if ('new_phone_number' in req.body && 'pin' in req.body) {
