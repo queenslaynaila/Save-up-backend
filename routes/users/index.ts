ffa8a31a824b4a5fccc0db6b2bf2aac013a634ef
Queenslay Jema <queenslayjema@gmail.com>
2024-10-30 08:33:23 +0300

    Refactor: Remove unused test route from user routes

--- routes/users/index.ts
+++ routes/users/index.ts
@@ -5,13 +5,11 @@ import getUserByCriteria from './getUserByCriteria';
 import updateUserAttributes from './updateId';
 import updateUserRole from './updateUserRole';
 import logout from './logout';
-import test from './test';
 
 export default (baseRouter: Router) => {
   const router = Router();
 
   createUser(router);
-  test(router);
   login(router);
   getUserByCriteria(router);
   updateUserRole(router);
--- routes/users/test.ts
+++
@@ -1,7 +0,0 @@
-import { Router } from 'express';
-
-export default (router: Router) => {
-  router.get('/tests', async (_req, res) => {
-    res.json({ message: 'App is ready!' });
-  });
-};
