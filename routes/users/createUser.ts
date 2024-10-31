72f2d4c36b815292da4b5ed2a5da6da41ee73a99
Queenslay Jema <queenslayjema@gmail.com>
2024-10-31 11:24:49 +0300

    Refactor: Default role to user if not provided

--- routes/users/createUser.ts
+++ routes/users/createUser.ts
@@ -4,37 +4,38 @@ import { z } from 'zod';
 import { sql } from '../../db';
 import validateRequest from '../../middleware/validationMiddleware';
 import { StatusCodeInterface } from '../../globalTypes';
-import { userContactDetailsSchema, userSchema } from './schema';
 import { HttpError } from '../../middleware/errorMiddleware';
+import { userContactDetailsSchema, UserRole, userSchema } from './types';
 
-const userCreationSchema = userSchema.pick({
+export const userCreationSchema = userSchema.pick({
   pin: true,
   id_type: true,
   id_number: true,
   role: true,
   gender: true
-}).merge(userContactDetailsSchema.pick({
-  full_name: true,
-  phone_number: true
-}));
+}).extend({
+  full_name: userContactDetailsSchema.shape.full_name,
+  phone_number: userContactDetailsSchema.shape.phone_number
+});
 
 type UserCreation = z.infer<typeof userCreationSchema>;
-
 const SQL_CREATE_USER = sql<UserCreation, Record<string, never>>(`
   SELECT create_user(:id_type, :id_number, :phone_number, :role, :full_name, :gender, :pin)
 `);
 
-const userCreationSchemaWithPinAsNumber = userCreationSchema.extend({
+const userDetails = userCreationSchema.extend({
   pin: z.number()
 });
-type UserCreationWithPinAsNumber = z.infer<typeof userCreationSchemaWithPinAsNumber>;
+type UserDetails = z.infer<typeof userDetails>;
 
 export default (router: Router) => {
-  router.post<Record<string, never>, StatusCodeInterface, UserCreationWithPinAsNumber,
-  Record<string, never>>(
+  router.post<Record<string, never>, StatusCodeInterface, UserDetails, Record<string, never>>(
     '/',
     validateRequest({ body: userCreationSchema }),
     async (req, res) => {
+      if (!req.body.role) {
+        req.body.role = UserRole.USER;
+      }
       const pinHash = bcrypt.hashSync(String(req.body.pin), 12);
 
       await SQL_CREATE_USER({
