43ab8b661973fe48a288a84d40ba9c805ebd8877
Queenslay Jema <queenslayjema@gmail.com>
2024-11-04 11:14:45 +0300

    Refactor: Improve authorization middleware by enhancing token verification and error handling

--- middleware/authorization.ts
+++ middleware/authorization.ts
@@ -1,15 +1,15 @@
-/* eslint-disable consistent-return */
 import { NextFunction, Request, Response } from 'express';
-import jwt, { Secret } from 'jsonwebtoken';
+import jwt, { JwtPayload, Secret, VerifyErrors } from 'jsonwebtoken';
 import { UserRole } from '../globalTypes';
 import { HttpError } from './errorMiddleware';
-import { generateToken, verifyTokenExpiration } from './generatetoken';
+import { verifyTokenExpiration } from './generatetoken';
 
 export type User = {
   id: number;
   role: UserRole;
   step?: number;
 }
+
 declare module 'express-serve-static-core' {
   interface Request {
     user?: User;
@@ -25,40 +25,43 @@ function authMiddleware(options: AuthMiddlewareOptions = {}) {
 
   return (req: Request, res: Response, next: NextFunction) => {
     const accessToken = req.headers['authorization-token'] as string;
-    const refreshToken = req.headers['refresh-token'] as string;
 
     if (!accessToken) {
       throw new HttpError(401);
     }
 
-    if (accessToken) {
-      const accessTokenValue = accessToken.split(' ')[1];
-      const isExpired = verifyTokenExpiration(accessTokenValue);
+    const accessTokenValue = accessToken.split(' ')[1];
+    if (!accessTokenValue) {
+      throw new HttpError(401);
+    }
 
-      if (isExpired && refreshToken) {
-        const refreshTokenValue = refreshToken.split(' ')[1];
-        const decodedRefreshToken = jwt.verify(refreshTokenValue, process.env.JWT_SECRET as Secret);
-        const user = decodedRefreshToken as User;
-        const newAccessToken = generateToken(user.id, user.role, '1d');
-        const newRefreshToken = generateToken(user.id, user.role, '7d');
-        res.setHeader('authorization-token', newAccessToken);
-        res.setHeader('refresh-token', newRefreshToken);
+    const isExpired = verifyTokenExpiration(accessTokenValue);
+    if (isExpired) {
+      throw new HttpError(401);
+    }
+
+    const verifyOptions: jwt.VerifyOptions = {
+      issuer: 'saveup'
+    };
+
+    jwt.verify(
+      accessTokenValue,
+      process.env.JWT_SECRET as Secret,
+      verifyOptions,
+      (err: VerifyErrors | null, decoded: JwtPayload | string | undefined) => {
+        if (err || !decoded) {
+          throw new HttpError(401);
+        }
+
+        const user = decoded as User;
         if (roles.length && !roles.includes(user.role)) {
           throw new HttpError(403);
         }
+
         req.user = user;
         return next();
       }
-
-      const decodedAccessToken = jwt.verify(accessTokenValue, process.env.JWT_SECRET as Secret);
-      const user = decodedAccessToken as User;
-
-      if (roles.length && !roles.includes(user.role)) {
-        throw new HttpError(403);
-      }
-      req.user = user;
-      return next();
-    }
+    );
   };
 }
 
