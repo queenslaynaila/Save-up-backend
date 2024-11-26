a9f8d7f561bbc154bd5c03bc276efbfb34571ff3
Queenslay Jema <queenslayjema@gmail.com>
2024-11-26 10:42:45 +0300

    Refactor: Rename getUserByCriteria to getUserBySearchCriteria in user routes

--- routes/users/index.ts
+++ routes/users/index.ts
@@ -1,6 +1,6 @@
 import createUser from './createUser';
 import login from './login';
-import getUserByCriteria from './getUserByCriteria';
+import getUserBySearchCriteria from './getUserByCriteria';
 import updateUserAttributes from './updateAttributes';
 import updateUserRole from './updateUserRole';
 import logout from './logout';
@@ -10,7 +10,7 @@ const router = Router.getRouterInstance('/users', 'Users');
 
 createUser(router);
 login(router);
-getUserByCriteria(router);
+getUserBySearchCriteria(router);
 updateUserRole(router);
 updateUserAttributes(router);
 logout(router);
