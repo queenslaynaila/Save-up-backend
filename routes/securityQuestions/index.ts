b4a313440cd1ae11c1be975c9633f6f60a9cc5fa
Queenslay Jema <queenslayjema@gmail.com>
2024-11-26 15:07:02 +0300

    Refactor: Remove unused import and commented code in securityQuestions route

--- routes/securityQuestions/index.ts
+++ routes/securityQuestions/index.ts
@@ -1,10 +1,8 @@
 import getAllSecurityQuestions from './getAllSecurityQuestions';
-// import getUsersQuestions from './getUsersQuestions';
 import Router from '../../router';
 
 const router = Router.getRouterInstance('/security-questions', 'Security Questions');
 
 getAllSecurityQuestions(router);
-// getUsersQuestions(router);
 
 export default router;
