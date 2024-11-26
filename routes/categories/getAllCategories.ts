21ee2354716128018a194b57cf8a0720780bd68d
Queenslay Jema <queenslayjema@gmail.com>
2024-11-26 15:52:19 +0300

    Refactor: Replace baseCategorySchema with inline category schema in getAllCategories route

--- routes/categories/getAllCategories.ts
+++ routes/categories/getAllCategories.ts
@@ -1,18 +1,22 @@
 import { z } from 'zod';
-import { baseCategorySchema } from './schema';
 import { sql } from '../../db';
 import Router from '../../router';
 import authMiddleware from '../../middleware/authorization';
 
-const categorySchema = baseCategorySchema.pick({
-  id: true,
-  name: true,
-  description: true,
-  image_url: true
+const categorySchema = z.object({
+  id: z.number(),
+  name: z.string(),
+  description: z.string(),
+  image_url: z.string(),
+  created_at: z.string(),
+  deleted_at: z.string()
 });
+
 type Category = z.infer<typeof categorySchema>;
 
-export const SQL_GET_ALL_CATEGORIES = sql<Record<string, never>, Category>(`
+export const SQL_GET_ALL_CATEGORIES = sql<
+Record<string, never>,
+Pick<Category, 'id' | 'name' | 'description' | 'image_url'>>(`
   SELECT id, name, description, image_url 
   FROM categories 
   WHERE deleted_at IS NULL
@@ -24,7 +28,9 @@ const getAllCategories = (router: Router) => {
     path: '/',
     summary: 'Get list of categories',
     response: {
-      schema: z.array(categorySchema)
+      schema: z.array(
+        categorySchema.pick({ id: true, name: true, description: true, image_url: true })
+      )
     },
     middlewares: [authMiddleware()],
     handler: async (req, res) => {
