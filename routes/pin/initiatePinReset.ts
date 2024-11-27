b9d6c9ba746c19d0780907eb9af83741abb0cffc
Queenslay Jema <queenslayjema@gmail.com>
2024-11-27 15:14:30 +0300

    Refactor: Update initiatePinReset route to improve token handling and response structure

--- routes/pin/initiatePinReset.ts
+++ routes/pin/initiatePinReset.ts
@@ -1,46 +1,41 @@
 import Router from '../../router';
 import bcrypt from 'bcrypt';
 import jwt, { Secret } from 'jsonwebtoken';
+import { z } from 'zod';
 import { resetPasswordLimiter } from '../../services/rateLimit';
 import { sql } from '../../db';
 import { generateResetPin } from '../../middleware/generateResetPin';
 import { HttpError } from '../../middleware/errorMiddleware';
-import {
-  GetByPhoneInterface,
-  GetByIdInterface
-} from '../../globalTypes';
-import {
-  TokenInterface,
-  InitiatePasswordResetInterface,
-  PhoneReasonSchema
-} from './types';
+import { ResetToken } from './schema';
+import logger from '../../logger';
 
-const SQL_GET_USER = sql<GetByPhoneInterface, GetByIdInterface>(`
+const SQL_GET_USER = sql<{ phone_number: string }, { id:number }>(`
   SELECT id 
   FROM user_contact_details 
   WHERE phone_number = :phone_number;
 `);
 
-const SQL_SAVE_TOKEN = sql<InitiatePasswordResetInterface, TokenInterface>(`
+const SQL_SAVE_TOKEN = sql<Pick<ResetToken, 'user_id'| 'token' | 'reason'>, Pick<ResetToken, 'token'>>(`
   INSERT INTO reset_tokens (user_id, xid, token, reason)
   SELECT :user_id,
-          COALESCE(MAX(xid), 0) + 1,
+          COALESCE(MAX(xid), 0) + 1, 
           :token,
           :reason
   FROM reset_tokens
-  WHERE user_id = :user_id;
+  WHERE user_id = :user_id
+  RETURNING token;
 `);
 
 const initiatePinReset = (router: Router) => {
   router.route({
     method: 'post',
-    path: '/',
+    path: '/request',
     summary: 'Initiate password reset',
     response: {
       statusCode: 204
     },
     schema: {
-      body: PhoneReasonSchema
+      body: z.object({ phone_number: z.string() })
     },
     middlewares: [resetPasswordLimiter],
     handler: async (req, res) => {
@@ -53,8 +48,10 @@ const initiatePinReset = (router: Router) => {
       await SQL_SAVE_TOKEN({
         user_id: user.id,
         token: hashedResetToken,
-        reason: req.body.reason
+        reason: 'Reset'
       }).exec();
+
+      logger.info(`Reset token for user ${user.id} created and is ${resetToken}`);
 
       const step1TokenPayload = { id: user.id, step: 1 };
       const resetTokenHeader = jwt.sign(
@@ -63,7 +60,7 @@ const initiatePinReset = (router: Router) => {
         { expiresIn: '15m' }
       );
 
-      res.setHeader('reset-token', resetTokenHeader)
+      res.setHeader('Reset', resetTokenHeader)
         .sendStatus(204);
     }
   });
