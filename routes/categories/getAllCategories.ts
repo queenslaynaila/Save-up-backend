f42b562900388689d9c46efb3e66173da292a859
Queenslay Jema <queenslayjema@gmail.com>
2024-11-27 08:53:50 +0300

    Refactor: Update category schema to include only necessary fields in getAllCategories route

--- routes/categories/getAllCategories.ts
+++ routes/categories/getAllCategories.ts
@@ -7,16 +7,11 @@ const categorySchema = z.object({
   id: z.number(),
   name: z.string(),
   description: z.string(),
-  image_url: z.string(),
-  created_at: z.string(),
-  deleted_at: z.string()
+  image_url: z.string()
 });
-
 type Category = z.infer<typeof categorySchema>;
 
-export const SQL_GET_ALL_CATEGORIES = sql<
-Record<string, never>,
-Pick<Category, 'id' | 'name' | 'description' | 'image_url'>>(`
+export const SQL_GET_ALL_CATEGORIES = sql<Record<string, never>, Category>(`
   SELECT id, name, description, image_url 
   FROM categories 
   WHERE deleted_at IS NULL
@@ -28,12 +23,10 @@ const getAllCategories = (router: Router) => {
     path: '/',
     summary: 'Get list of categories',
     response: {
-      schema: z.array(
-        categorySchema.pick({ id: true, name: true, description: true, image_url: true })
-      )
+      schema: z.array(categorySchema)
     },
     middlewares: [authMiddleware()],
-    handler: async (req, res) => {
+    handler: async (_req, res) => {
       const categories = await SQL_GET_ALL_CATEGORIES({}).many();
       res.json(categories);
     }
