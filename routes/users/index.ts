b399a82ec869e545e4d17cfa238aa766f3b3cab3
Queenslay Jema <queenslayjema@gmail.com>
2024-11-28 10:43:06 +0300

    Refactor: Remove login and logout routes from users index

--- routes/users/index.ts
+++ routes/users/index.ts
@@ -1,18 +1,12 @@
 import Router from '../../router';
-import createUser from './createUser';
-import login from './login';
 import updateUserAttributes from './updateAttributes';
 import getUserBySearchCriteria from './getUserByCriteria';
 import getTransactionsForUser from './getTransactionsForUser';
-import logout from './logout';
 
 const router = Router.getRouterInstance('/users', 'Users');
 
-createUser(router);
-login(router);
 getUserBySearchCriteria(router);
 getTransactionsForUser(router);
 updateUserAttributes(router);
-logout(router);
 
 export default router;
