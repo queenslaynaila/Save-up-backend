0a3958ff4774cb5d3c6be015a426adcd3cd7035e
Queenslay Jema <queenslayjema@gmail.com>
2024-11-29 11:19:18 +0300

    Refactor: Move security answer routes to security questions for better organization and clarity

--- routes/securityAnswer/index.ts
+++ routes/securityAnswer/index.ts
@@ -1,10 +1,8 @@
 import Router from '../../router';
-import createSecurityAnswer from './createSecurityAnswer';
 import updateSecurityAnswer from './updateAnswer';
 
 const router = Router.getRouterInstance('/security-answers', 'Security Answers');
 
-createSecurityAnswer(router);
 updateSecurityAnswer(router);
 
 export default router;
--- routes/securityQuestions/index.ts
+++ routes/securityQuestions/index.ts
@@ -1,8 +1,12 @@
 import getAllSecurityQuestions from './getAllSecurityQuestions';
+import createSecurityAnswer from './createSecurityAnswer';
+import updateSecurityAnswer from './updateAnswer';
 import Router from '../../router';
 
-const router = Router.getRouterInstance('/security-questions', 'Security Questions');
+const router = Router.getRouterInstance('/security-questions', 'Security');
 
 getAllSecurityQuestions(router);
+createSecurityAnswer(router);
+updateSecurityAnswer(router);
 
 export default router;
