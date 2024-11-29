49511dc4d797bc7553e9966ca75b6ddd82efc150
Queenslay Jema <queenslayjema@gmail.com>
2024-11-29 09:25:46 +0300

    Refactor: Rename verify-security-answers route to verify-answers for clarity

--- routes/pin/verifySecurityAnswers.ts
+++ routes/pin/verifySecurityAnswers.ts
@@ -22,7 +22,7 @@ const SQL_GET_SECURITY_ANSWERS = sql<{user_id:number}, {question_id: number;answ
 const verifySecurityAnswers = (router: Router) => {
   router.route({
     method: 'get',
-    path: '/verify-security-answers',
+    path: '/verify-answers',
     summary: 'Verify security answers',
     schema: {
       body: verifyAnswerSchema.array()
