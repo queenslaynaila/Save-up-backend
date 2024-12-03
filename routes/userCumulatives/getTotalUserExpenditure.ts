72a8c34cfa22c368fcb256fcf3dc23ce7d405d5f
Queenslay Jema <queenslayjema@gmail.com>
2024-12-03 12:35:39 +0300

    Chore: Log function parts for debug

--- routes/userCumulatives/getTotalUserExpenditure.ts
+++ routes/userCumulatives/getTotalUserExpenditure.ts
@@ -1,12 +1,13 @@
+import { z } from 'zod';
 import { sql } from '../../db';
-import HttpError from '../../httpError';
 import Router from '../../router';
+import { ParsedQs } from 'qs';
 import {
   TotalExpenseInterface,
-  totalExpenseResultSchema,
   totalExpensesQuerySchema,
   UserCumulaInterface
 } from './types';
+import logger from '../../logger';
 
 const SQL_GET_TOTAL_EXPENSES = sql<UserCumulaInterface, TotalExpenseInterface>(`
   SELECT COALESCE(SUM(amount), 0) AS total_expenses
@@ -23,35 +24,41 @@ const getTotalUserExpenditure = (router: Router) => {
       query: totalExpensesQuerySchema
     },
     response: {
-      schema: totalExpenseResultSchema
+      schema: z.object({ total_expenses: z.number() })
     },
     authMiddlewareOptions: {},
     handler: async (req, res) => {
       const user_id = req.user!.id;
       const { start_date, end_date, category_id } = req.query;
+
       const filters: string[] = [];
-      const filterArgs: Record<string, string> = {};
+      const filterArgs: string | string [] | ParsedQs | ParsedQs[] = {};
+
       if (start_date) {
-        filterArgs.start_date = Array.isArray(start_date)
-          ? start_date[0] as string
-          : start_date as string;
-        filters.push('spent_at >= :start_date');
+        filterArgs.start_date = start_date;
+        filters.push('DATE(spent_at) >= :start_date');
       }
+
+      logger.info(`start date is ${start_date}`);
+
       if (end_date) {
-        filterArgs.end_date = Array.isArray(end_date)
-          ? end_date[0] as string
-          : end_date as string;
-        filters.push('spent_at <= :end_date');
+        filterArgs.end_date = end_date;
+        filters.push('DATE(spent_at) <= :end_date');
       }
+
+      logger.info(`end date is ${end_date}`);
+
       if (category_id) {
-        filterArgs.category_id = Array.isArray(category_id)
-          ? category_id[0] as string
-          : category_id as string;
+        filterArgs.category_id = category_id;
         filters.push('category_id = :categoryId');
       }
+
       const query = SQL_GET_TOTAL_EXPENSES({ user_id });
+      logger.info(`query constructed is ${query}`);
       if (filters.length > 0) query.extend(`AND ${filters.join(' AND ')}`, filterArgs);
-      res.json(await query.one(new HttpError(500)));
+
+      const { total_expenses } = await query.one();
+      res.json({ total_expenses });
     }
   });
 };
