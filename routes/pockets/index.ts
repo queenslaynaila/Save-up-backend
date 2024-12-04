e236d20d766ee4d472bff5341dd58d09e4686792
Queenslay Jema <queenslayjema@gmail.com>
2024-12-04 15:52:32 +0300

    Chore: Clear dead code

--- routes/pockets/index.ts
+++ routes/pockets/index.ts
@@ -3,14 +3,12 @@ import createPocket from './createPocket';
 import deletePocket from './deletePocket';
 import getPocketByCriteria from './getPocketByCriteria';
 import updatePocket from './updatePocket';
-import getTransactionsForPocket from './getTransactionsForPocket';
 
 const router = Router.getRouterInstance('/pockets', 'Pockets');
 
 createPocket(router);
 deletePocket(router);
 getPocketByCriteria(router);
-getTransactionsForPocket(router);
 updatePocket(router);
 
 export default router;
--- routes/users/index.ts
+++ routes/users/index.ts
@@ -1,12 +1,10 @@
 import Router from '../../router';
 import updateUserAttributes from './updateAttributes';
 import getUsersBySearchCriteria from './getUserByCriteria';
-import getTransactionsForUser from './getTransactionsForUser';
 
 const router = Router.getRouterInstance('/users', 'Users');
 
 getUsersBySearchCriteria(router);
-getTransactionsForUser(router);
 updateUserAttributes(router);
 
 export default router;
