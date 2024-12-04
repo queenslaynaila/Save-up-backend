2a46a9780b7d4a7b860071bc9fabe231342b2815
Queenslay Jema <queenslayjema@gmail.com>
2024-12-04 09:36:18 +0300

    Feat: Allow update of security answers

--- routes/securityQuestions/updateAnswer.ts
+++ routes/securityQuestions/updateAnswer.ts
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 import Router from '../../router';
 import { sql } from '../../db';
 import { verifyPin } from '../../authorization';
@@ -32,12 +33,9 @@ const updateSecurityAnswer = (router: Router) => {
     method: 'patch',
     path: '/',
     summary: 'Update security answers',
-    description: 'Allows a user to completely override their existing security answers and create new ones.\n'
-    + ' Security answers are not shown to users for privacy and security reasons. Therefore,we \n'
-    + 'assume that the user has forgotten all their previous answers. Instead of editing individual answers,\n'
-    + 'the system requires the user to submit a new set of answers to predefined security questions.\n'
-    + 'The users PIN is verified to ensure proper authorization before proceeding. \n'
-    + 'Once verified all existing answers for users are delted and new ones stored.',
+    description: 'Allows a user to completely override their existing security answers.Security answers are not shown to users for privacy and security reasons\n\n'
+    + 'Security answers are not shown to users for privacy and security reasons. Therefore,we assume that the user has forgotten all their previous answers. Instead of editing individual answers we submit a new set of answers to predefined security questions \n\n'
+    + 'The users PIN is verified to ensure proper authorization before proceeding.Once verified all existing answers for users are delted and new ones stored \n\n',
     schema: {
       body: z.object({
         pin: z.string(),
