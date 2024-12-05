727fd107cd4e407904a59c3ebc6e27671ac17cf8
Queenslay Jema <queenslayjema@gmail.com>
2024-12-05 17:21:28 +0300

    Feat: Update total transactions endpoint to retrieve user balance across all pockets

--- routes/transactions/getTotalTransactions.ts
+++ routes/transactions/getTotalTransactions.ts
@@ -4,20 +4,26 @@ import Router from '../../router';
 import { ParsedQs } from 'qs';
 import { entitySchema } from '../../types';
 
-const SQL_TRANSACTION_AGREGATES = sql<{entity_id: number}, {total: number}>(`
-  SELECT 
-    COALESCE(SUM(transactions.delta), 0) AS total
-  FROM transactions
-  JOIN transaction_types 
-    ON transactions.type_id = transaction_types.id
-  WHERE transactions.entity_id = :entity_id
+const SQL_GET_BALANCE = sql<{entity_id: number}, {total: number}>(`
+  SELECT COALESCE(SUM(balance), 0) AS balance
+  FROM (
+       SELECT DISTINCT ON (pocket_id) balance
+       FROM transactions
+       WHERE entity_id = :entity_id
+       ORDER BY pocket_id, created_at DESC
+  ) AS current_balance_per_pocket;
 `);
 
 const computeTransactionTotals = (router: Router) => {
   router.route({
     method: 'get',
     path: '/totals',
-    summary: 'Get total user/ group transactions for all transaction types expenditure',
+    summary: 'Get user balance across all pockets',
+    description: 'Calculates and retrieves the available balance for a system entity(grps,pockets). Allows optional query parameters:\n\n'
+    + '- **pocket_id**: Retrieves the current balance for a specific pocket.\n'
+    + '- **from**: Filters transactions from a specific start date.\n'
+    + '- **to**: Filters transaction up to a specific end date.\n'
+    + '- **group_id**: If provided, we get available balnce for a grp entity.',
     schema: {
       body: entitySchema,
       query: z.object({
@@ -34,7 +40,8 @@ const computeTransactionTotals = (router: Router) => {
         ]).default('Saving'),
         from: z.string(),
         to: z.string(),
-        pocket_id: z.string()
+        pocket_id: z.string(),
+        group_id: z.string()
       }).partial()
     },
     response: {
@@ -42,17 +49,11 @@ const computeTransactionTotals = (router: Router) => {
     },
     authMiddlewareOptions: {},
     handler: async (req, res) => {
-      const entity_id = req.body?.entity_id ?? req.user!.id;
-      const { type = 'Saving', pocket_id, from, to } = req.query;
+      const entity_id = Number(req.query?.group_id) ?? req.user!.id;
+      const { pocket_id, from, to } = req.query;
 
       const filters: string[] = [];
       const filterArgs: string | string [] | ParsedQs | ParsedQs[] = {};
-
-      if (type) {
-        filterArgs.type = type;
-        filters.push('transaction_types.slug = :type');
-      }
-
       if (pocket_id) {
         filterArgs.pocket_id = pocket_id;
         filters.push('transactions.pocket_id = :pocket_id');
@@ -70,7 +71,7 @@ const computeTransactionTotals = (router: Router) => {
         filters.push('DATE(transactions.created_at) <= :to');
       }
 
-      const query = SQL_TRANSACTION_AGREGATES({ entity_id });
+      const query = SQL_GET_BALANCE({ entity_id });
 
       if (filters.length > 0) query.extend(`AND ${filters.join(' AND ')}`, filterArgs);
       const { total } = await query.one();
