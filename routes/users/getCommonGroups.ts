ecf64d7ec0e7b40e9fdb77b9b09ca64192a47e67
Queenslay Jema <queenslayjema@gmail.com>
2024-12-10 09:13:29 +0300

    Refactor: Add getCommonGroups route to retrieve shared groups between users

---
+++ routes/users/getCommonGroups.ts
@@ -0,0 +1,50 @@
+import Router from '../../router';
+import { sql } from '../../db';
+import { z } from 'zod';
+import { Group, group } from '../groups/createGroups';
+
+const SQL_GET_COMMON_GROUPS = sql< { logged_in_user_id: number, peer_user_id: number }, Group>(`
+  SELECT groups.id, 
+         groups.name, 
+         groups.created_at
+  FROM groups
+  JOIN group_members
+    USING (group_id)
+  JOIN group_members AS group_members_peer
+    USING (group_id)
+  WHERE group_members.user_id = :logged_in_user_id
+    AND group_members_peer.user_id = :peer_user_id
+    AND group_members.is_active = TRUE
+    AND group_members_peer.is_active = TRUE
+    AND groups.deleted_at IS NULL;
+`);
+
+const getCommonGroups = (router:Router) => {
+  router.route({
+    method: 'get',
+    path: '/:peer_user_id/shared-groups',
+    summary: 'View common groups between two users.',
+    request: {
+      params: z.object({
+        peer_user_id: z.string()
+      })
+    },
+    response: {
+      200: {
+        schema: z.array(group)
+      }
+    },
+    authMiddlewareOptions: {},
+    handler: async (req, res) => {
+      const logged_in_user_id = req.user!.id;
+      const peer_user_id = Number(req.params.peer_user_id);
+      const commonGroups = await SQL_GET_COMMON_GROUPS({
+        logged_in_user_id,
+        peer_user_id
+      }).many();
+      return res.json(commonGroups);
+    }
+  });
+};
+
+export default getCommonGroups;
--- routes/users/index.ts
+++ routes/users/index.ts
@@ -1,10 +1,12 @@
 import Router from '../../router';
 import updateUserAttributes from './updateAttributes';
 import getUsersBySearchCriteria from './getUserByCriteria';
+import getCommonGroups from './getCommonGroups';
 
 const router = Router.getRouterInstance('/users', 'Users');
 
 getUsersBySearchCriteria(router);
 updateUserAttributes(router);
+getCommonGroups(router);
 
 export default router;
