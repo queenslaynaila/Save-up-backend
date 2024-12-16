87a82f4b6ac104e36477f92228c3219312e3e4ed
Queenslay Jema <queenslayjema@gmail.com>
2024-12-16 10:43:45 +0300

    Refactor: Simplify convertToTitleCase function by removing USER_ROLE_ENUM validation

--- caseNormalization.ts
+++ caseNormalization.ts
@@ -1,10 +1,3 @@
-import { USER_ROLE_ENUM } from './routes/users/schema';
 export const convertToTitleCase = (str: string): string => {
-  const titleCased = str.toLowerCase().replace(/\b\w/g, (c) => c.toUpperCase());
-
-  if (!USER_ROLE_ENUM.safeParse(titleCased).success) {
-    throw new Error('Invalid role');
-  }
-
-  return titleCased;
+  return str.toLowerCase().replace(/\b\w/g, (c) => c.toUpperCase());
 };
