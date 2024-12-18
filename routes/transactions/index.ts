0d6927ad8dce5c06b18dd7958c51b1d00ab6f371
Queenslay Jema <queenslayjema@gmail.com>
2024-12-18 11:13:28 +0300

    Refactor: Remove getTotalTransactions route to simplify transaction handling

--- routes/transactions/index.ts
+++ routes/transactions/index.ts
@@ -1,5 +1,4 @@
 import Router from '../../router';
-import computeTransactionTotals from './getTotalTransactions';
 import createSaving from './depositTransactions';
 import createTransfer from './transfer';
 import createWithdrawal from './withdrawal';
@@ -10,7 +9,6 @@ const router = Router.getRouterInstance('/transactions', 'Transactions');
 createSaving(router);
 createTransfer(router);
 createWithdrawal(router);
-computeTransactionTotals(router);
 getTransactions(router);
 
 export default router;
