449e86fa5d4b7b67fc8499a305027d6df8377548
Queenslay Jema <queenslayjema@gmail.com>
2024-12-18 12:29:16 +0300

    feat: Add endpoint to retrieve total savings across all pockets

--- routes/pockets/index.ts
+++ routes/pockets/index.ts
@@ -4,6 +4,7 @@ import deletePocket from './deletePocket';
 import getPocketByCriteria from './getPocketByCriteria';
 import updatePocket from './updatePocket';
 import fetchPocketBalances from './getTotalTransactions';
+import getTotalSavings from './getTotalSavings';
 
 const router = Router.getRouterInstance('/pockets', 'Pockets');
 
@@ -12,5 +13,6 @@ deletePocket(router);
 getPocketByCriteria(router);
 updatePocket(router);
 fetchPocketBalances(router);
+getTotalSavings(router);
 
 export default router;
