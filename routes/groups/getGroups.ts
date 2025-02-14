ebad9f25a37697fc57182319b8d7e56d94a1e8f2
Queenslay Jema <queenslayjema@gmail.com>
2025-02-14 10:39:34 +0300

    feat: Enhance getUserGroups to support fetching common groups between two users

--- routes/groups/getGroups.ts
+++ routes/groups/getGroups.ts
@@ -13,27 +13,40 @@ const group = groupsSchema.pick({
 });
 type Group = z.infer<typeof group>;
 
-const SQL_FETCH_USER_GROUPS = sql<{ user_id:number }, Group>(`
-  SELECT
+const SQL_FETCH_USER_GROUPS = sql<{ user_id: number; other_user_id?: number|null}, Group>(`
+  SELECT 
     groups.id, 
     groups.name, 
     groups.created_at,
     user_contact_details.full_name AS created_by
-  FROM groups 
-  LEFT JOIN group_members 
-    ON groups.id = group_members.group_id
-  LEFT JOIN user_contact_details 
-    ON groups.creator_id = user_contact_details.id
+  FROM groups
+  LEFT JOIN group_members ON groups.id = group_members.group_id
+  LEFT JOIN user_contact_details ON groups.creator_id = user_contact_details.id
   WHERE group_members.user_id = :user_id
-    AND group_members.is_active = TRUE 
-    AND groups.deleted_at IS NULL;
+    AND group_members.is_active = TRUE
+    AND groups.deleted_at IS NULL
+    -- If other_user_id is provided, filter to only common groups
+    AND (:other_user_id IS NULL 
+      OR EXISTS (
+          SELECT 1 FROM group_members 
+          WHERE group_members.group_id = groups.id 
+          AND group_members.user_id = :other_user_id 
+          AND group_members.is_active = TRUE
+      )
+    );
 `);
+
 
 const getUserGroups = (router:Router) => {
   router.route({
     method: 'get',
     path: '/',
-    summary: 'Get groups a user belongs to',
+    summary: 'Get groups a logged in user belongs to/ get common grps between 2 members',
+    request: {
+      query: z.object({
+        user_id: z.string().optional()
+      })
+    },
     response: {
       200: {
         schema: z.array(group)
@@ -42,8 +55,13 @@ const getUserGroups = (router:Router) => {
     authMiddlewareOptions: {},
     handler: async (req, res) => {
       logger.info('Fetching user groups');
+      const otherUserId = req.query.user_id 
+        ? Number(req.query.user_id) 
+        : null;
+
       const groups = await SQL_FETCH_USER_GROUPS({
-        user_id: req.user!.id
+        user_id: req.user!.id,
+        other_user_id: otherUserId
       }).many();
       return res.json(groups);
     }
