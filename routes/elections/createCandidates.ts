d90823aaaeb848a917a1c4b873e82d7988c90f78
Queenslay Jema <queenslayjema@gmail.com>
2025-02-21 11:34:13 +0300

    Refactor: enhance createCandidates route to handle election closure and nomination errors

--- routes/elections/createCandidates.ts
+++ routes/elections/createCandidates.ts
@@ -2,6 +2,7 @@ import Router from '../../router';
 import { sql } from '../../db';
 import { CandidateInterface, candidateRequestBody } from './types';
 import { z } from 'zod';
+import HttpError from '../../httpError';
 
 const SQL_CREATE_CANDIDATE = sql<CandidateInterface, Record<string, never>>(`
   SELECT create_candidate(:group_id, :election_id, :candidate_id, :user_id);
@@ -30,7 +31,17 @@ const createCandidates = (router: Router) => {
         election_id: Number(req.params.election_id),
         candidate_id: req.body.candidate_id,
         user_id: req.user!.id
-      }).exec();
+      }).exec().catch(err=>{
+        if (err.code === 'P0007') {
+          throw new HttpError(401, { message: 'ERR_ELECTION_CLOSED' });
+        }
+        if (err.code === 'P0009') {
+          throw new HttpError(401, { message: 'ERR_NOMINATION_ENDED' });
+        }
+        if (err.code === 'P0003') {
+          throw new HttpError(401, { message: 'ERR_NOMINATION_ATTEMPTS_EXHAUSTED' });
+        }
+       });;
       res.sendStatus(201);
     }
   });
