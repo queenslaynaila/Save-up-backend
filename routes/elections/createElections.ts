6ce1c562bea7edc6e4abb2997962fee8da922891
Queenslay Jema <queenslayjema@gmail.com>
2025-02-24 12:17:09 +0300

    feat: enhance create_election function to accept candidate IDs for ratification elections

--- routes/elections/createElections.ts
+++ routes/elections/createElections.ts
@@ -5,8 +5,10 @@ import { z } from 'zod';
 import HttpError from '../../httpError';
 
 const SQL_CALL_ELECTION = sql<
-ElectionInterface & {nomination_ends_at?:string}, Record<string, never>>(`
-  SELECT create_election(:group_id, :initiator_id, :type, :nomination_ends_at)
+  ElectionInterface & { nomination_ends_at?: string; candidates: number[]|null },
+  Record<string, never>
+>(`
+  SELECT create_election(:group_id, :initiator_id, :type, :nomination_ends_at, :candidates)
 `);
 
 const createElections = (router: Router) => {
@@ -17,6 +19,7 @@ const createElections = (router: Router) => {
     request: {
       body: electionValidation.extend({
         nomination_ends_at: z.string().datetime().optional(),
+        candidates: z.array(z.number()).min(1).max(3).optional(),
       })
     },
     response: {
@@ -24,12 +27,13 @@ const createElections = (router: Router) => {
     },
     authMiddlewareOptions: {},
     handler: async (req, res) => {
-      const { type, group_id} = req.body;
+      const { type, group_id, candidates} = req.body;
       await SQL_CALL_ELECTION({
         type,
         group_id,
         nomination_ends_at: req.body.nomination_ends_at 
                   ?? new Date(Date.now() + 7 * 24 * 60 * 60 * 1000).toISOString(),
+        candidates: type === 'Ratification' ? req.body.candidates ?? null : null,
         initiator_id: req.user!.id
       }).exec().catch(err => {
         if (err.code === 'P0001') {
