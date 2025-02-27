9c1162bbb008bfa4655d99720e815010e081a193
Queenslay Jema <queenslayjema@gmail.com>
2025-02-27 11:44:15 +0300

    feat: add new migration scripts

---
+++ migrations/0037_create_table_elections.sql
@@ -0,0 +1,19 @@
+CREATE TYPE enum_election_type AS ENUM ('Ballot', 'Ratification', 'Default');
+CREATE TYPE enum_election_status AS ENUM ('Open', 'Closed', 'Cancelled');
+
+CREATE TABLE IF NOT EXISTS elections (
+  group_id       INT NOT NULL,
+  xid            INT NOT NULL,
+  initiator_id   INT NOT NULL,
+  type                enum_election_type NOT NULL,
+  status              enum_election_status NOT NULL DEFAULT 'Open',
+  created_at          TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+  closed_at           TIMESTAMP WITH TIME ZONE,
+  nomination_ends_at  TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT (NOW() + INTERVAL '7 days'),
+  PRIMARY KEY    (group_id, xid),
+  FOREIGN KEY    (group_id) REFERENCES groups(id) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  FOREIGN KEY    (group_id, initiator_id) REFERENCES group_members(group_id,user_id)
+);
+
+SELECT create_distributed_table('elections', 'group_id');
+GRANT INSERT, SELECT, UPDATE ON elections TO app_user;
---
+++ migrations/0038_create_table_ratifications.sql
@@ -0,0 +1,15 @@
+CREATE TABLE IF NOT EXISTS ratifications (
+  group_id       INT NOT NULL,
+  election_id    INT NOT NULL,
+  user_id        INT NOT NULL,
+  is_ratified    BOOLEAN NOT NULL,
+  created_at     TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+  PRIMARY KEY    (group_id, election_id, user_id),
+  FOREIGN KEY    (group_id,user_id) REFERENCES group_members(group_id,user_id) 
+    ON DELETE RESTRICT ON UPDATE RESTRICT,
+  FOREIGN KEY    (group_id, election_id) REFERENCES elections(group_id,xid)
+    ON DELETE RESTRICT ON UPDATE RESTRICT
+);
+SELECT create_distributed_table('ratifications', 'group_id');
+GRANT INSERT, SELECT ON ratifications TO app_user;
+
