15fb6d65f43874da67508744c05e72df5590ad56
Queenslay Jema <queenslayjema@gmail.com>
2025-03-07 08:52:10 +0300

    Chore: re,moce dead code

--- routes/groupDeposits/index.ts
+++
@@ -1,8 +0,0 @@
-import Router from '../../router';
-import createGroupDeposit from './createGroupDeposit';
-
-const router = Router.getRouterInstance('/group-deposits', 'Group Deposits');
-
-createGroupDeposit(router);
-
-export default router;
