9170d8a9ececbd82c36ef73d442b20a5e124d249
Queenslay Jema <queenslayjema@gmail.com>
2025-03-07 17:04:58 +0300

    Refactor: use or instaed of condtional extension

--- routes/transactions/getTransactions.ts
+++ routes/transactions/getTransactions.ts
@@ -1,8 +1,8 @@
 import { z } from 'zod';
 import { sql } from '../../db';
 import Router from '../../router';
-import { transactionSchema, transactionTypeSchema } from '../pockets/schema';
 import verifyGroupMembership from '../../utils';
+import { transactionSchema, transactionTypeSchema } from './schema';
 
 const transaction = transactionSchema.pick({
   xid: true,
@@ -17,7 +17,7 @@ const transaction = transactionSchema.pick({
 });
 type Transaction = z.infer<typeof transaction>;
 
-const SQL_GET_TRANSACTIONS = sql<{ entity_id: number }, Transaction>(`
+const SQL_GET_TRANSACTIONS = sql<{ entity_id: number, pocket_id?: number, slug?: string, start_date?: string, end_date?: string, limit: number }, Transaction>(`
   SELECT 
     transactions.xid, 
     transaction_types.slug,
@@ -33,21 +33,18 @@ const SQL_GET_TRANSACTIONS = sql<{ entity_id: number }, Transaction>(`
              ON group_deposits.user_id = user_contact_details.id
            WHERE group_deposits.group_id = transactions.entity_id 
              AND group_deposits.deposit_id = transactions.xid),
-          
           (SELECT user_contact_details.full_name 
            FROM disbursements 
            JOIN user_contact_details 
              ON disbursements.user_id = user_contact_details.id
            WHERE disbursements.group_id = transactions.entity_id 
              AND disbursements.transaction_id = transactions.xid),
-          
           (SELECT user_contact_details.full_name 
            FROM group_transfers 
            JOIN user_contact_details 
              ON group_transfers.admin_id = user_contact_details.id
            WHERE group_transfers.group_id = transactions.entity_id 
              AND group_transfers.source_transaction_id = transactions.xid),
-
           (SELECT user_contact_details.full_name 
            FROM group_transfers 
            JOIN user_contact_details 
@@ -57,9 +54,7 @@ const SQL_GET_TRANSACTIONS = sql<{ entity_id: number }, Transaction>(`
         )::TEXT
       ELSE NULL::TEXT
     END AS member_name,
-
-    (SELECT pockets.name 
-      FROM transactions source_transactions
+    (SELECT pockets.name FROM transactions source_transactions
       JOIN transaction_types source_transaction_types 
         ON source_transactions.type_id = source_transaction_types.id
       JOIN pockets 
@@ -68,23 +63,26 @@ const SQL_GET_TRANSACTIONS = sql<{ entity_id: number }, Transaction>(`
       WHERE source_transactions.reference_id = transactions.reference_id 
         AND source_transaction_types.slug = 'TransferOut'
         AND source_transactions.entity_id = transactions.entity_id) AS source_pocket_name,
-
-    (SELECT pockets.name 
-    FROM transactions destination_transactions
-    JOIN transaction_types destination_transaction_types 
-      ON destination_transactions.type_id = destination_transaction_types.id
-    JOIN pockets 
-      ON destination_transactions.pocket_id = pockets.xid 
-      AND pockets.entity_id = destination_transactions.entity_id
-    WHERE destination_transactions.reference_id = transactions.reference_id 
-      AND destination_transaction_types.slug = 'TransferIn'
-      AND destination_transactions.entity_id = transactions.entity_id) AS destination_pocket_name,
-
+    (SELECT pockets.name FROM transactions destination_transactions
+      JOIN transaction_types destination_transaction_types 
+        ON destination_transactions.type_id = destination_transaction_types.id
+      JOIN pockets 
+        ON destination_transactions.pocket_id = pockets.xid 
+        AND pockets.entity_id = destination_transactions.entity_id
+      WHERE destination_transactions.reference_id = transactions.reference_id 
+        AND destination_transaction_types.slug = 'TransferIn'
+        AND destination_transactions.entity_id = transactions.entity_id) AS destination_pocket_name,
     transactions.created_at
   FROM transactions
   JOIN transaction_types 
     ON transactions.type_id = transaction_types.id
   WHERE transactions.entity_id = :entity_id
+    AND (:pocket_id::INT IS NULL OR transactions.pocket_id = :pocket_id)
+    AND (:slug::TEXT IS NULL OR transaction_types.slug = :slug)
+    AND (:start_date::DATE IS NULL OR DATE(transactions.created_at) >= :start_date)
+    AND (:end_date::DATE IS NULL OR DATE(transactions.created_at) <= :end_date)
+  ORDER BY transactions.created_at DESC
+  LIMIT :limit
 `);
 
 const getTransactions = (router: Router) => {
@@ -97,7 +95,7 @@ const getTransactions = (router: Router) => {
         entity_id: z.union([
           z.string().regex(/^[1-9]\d*$/, "Must be a positive integer string"),
           z.literal("me"), 
-        ]).default('me' )
+        ]).default('me')
       }),
       query: z.object({
         slug: transactionTypeSchema.shape.slug,
@@ -116,52 +114,18 @@ const getTransactions = (router: Router) => {
     middlewares: [verifyGroupMembership({allowModeratorAccess: true})],
     handler: async (req, res) => {
       const entity_id = Number(req.params.entity_id);
-      const { slug, pocket_id, from, to, limit = '10' } = req.query;
+      const pocket_id = Number(req.query.pocket_id);
+      const { slug, from, to, limit = '10' } = req.query;
 
-      const filters: string[] = [];
-      const filterArgs: Record<string, string | number> = {
+      const transactions = await SQL_GET_TRANSACTIONS({
         entity_id,
+        pocket_id,
+        slug,
+        start_date: from,
+        end_date: to,
         limit: Number(limit)
-      };
+      }).many();
 
-      if (pocket_id) {
-        filterArgs.pocket_id = Number(pocket_id);
-        filters.push('transactions.pocket_id = :pocket_id');
-      }
-
-      if (slug) {
-        filterArgs.slug = slug;
-        filters.push('transaction_types.slug = :slug');
-      }
-
-      if (from && to) {
-        filterArgs.start_date = from;
-        filterArgs.end_date = to;
-        filters.push('DATE(transactions.created_at) BETWEEN :start_date AND :end_date');
-      } else {
-        if (from) {
-          filterArgs.start_date = from;
-          filters.push('DATE(transactions.created_at) >= :start_date');
-        }
-
-        if (to) {
-          filterArgs.end_date = to;
-          filters.push('DATE(transactions.created_at) <= :end_date');
-        }
-      }
-
-
-      const query = SQL_GET_TRANSACTIONS({
-        entity_id
-      });
-
-      if (filters.length > 0) {
-        query.extend(`AND ${filters.join(' AND ')}`, filterArgs);
-      }
-
-      query.extend('ORDER BY transactions.created_at DESC LIMIT :limit', filterArgs);
-
-      const transactions = await query.many();
       res.json(transactions);
     }
   });
