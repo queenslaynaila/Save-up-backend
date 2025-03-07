d022db60c09c1318ed61a182cb14afe456467b24
Queenslay Jema <queenslayjema@gmail.com>
2025-03-07 08:52:39 +0300

    Refactor: Rename create_user_saving

---
+++ migrations/0051_create_fn_create_saving.mutable.sql
@@ -0,0 +1,73 @@
+CREATE OR REPLACE FUNCTION create_saving(
+  p_entity_id   INT,
+  p_user_id     INT,
+  p_pocket_id   INT,
+  p_amount      NUMERIC
+) RETURNS VOID AS $$
+DECLARE
+  v_current_balance      NUMERIC;
+  v_new_balance          NUMERIC;
+  v_reference_id         INT;
+  v_is_group             BOOLEAN;
+  v_transaction_type_id  INT;
+BEGIN
+  SELECT EXISTS (
+    SELECT 1 
+    FROM entities 
+    WHERE id = p_entity_id 
+      AND entity_type = 'Group'
+  ) INTO v_is_group;
+
+  v_current_balance := get_transaction_info(p_entity_id, p_pocket_id);
+  v_new_balance := v_current_balance + p_amount;
+  v_reference_id := floor(random() * 1000000 + 1)::INT;
+
+  UPDATE pockets
+  SET status = 'Completed'::enum_status,
+      completed_at = NOW()
+  WHERE entity_id = p_entity_id
+    AND xid = p_pocket_id
+    AND status = 'In Progress'::enum_status
+    AND v_current_balance >= pockets.target_amount;
+
+  SELECT id INTO v_transaction_type_id
+  FROM transaction_types
+  WHERE slug = 'Saving';
+
+  IF v_transaction_type_id IS NULL THEN
+    RAISE EXCEPTION USING 
+      MESSAGE = 'ERR_TRANSACTION_TYPE_NOT_FOUND',
+      ERRCODE = 'P0005';
+  END IF;
+
+  PERFORM insert_transaction_log(
+    p_entity_id,
+    v_transaction_type_id,
+    p_pocket_id,
+    v_reference_id,
+    p_amount,
+    v_new_balance
+  );
+
+  IF v_is_group THEN
+    INSERT INTO group_deposits (
+      group_id,
+      deposit_id,
+      user_id
+    ) VALUES (
+      p_entity_id,
+      v_reference_id,
+      p_user_id
+    );
+  END IF;
+END;
+$$ LANGUAGE plpgsql;
+
+GRANT EXECUTE ON FUNCTION create_saving(
+  INT, INT, INT, NUMERIC
+) TO saveup_www;
+
+SELECT create_distributed_function(
+  'create_saving(INT, INT, INT, NUMERIC)',
+  'p_entity_id'
+);
