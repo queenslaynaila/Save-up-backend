388334fedf28cf18b679921d1bde2b4ae53e604a
Queenslay Jema <queenslayjema@gmail.com>
2025-03-10 02:41:21 +0300

    Fix: Move user id search block to be last

--- routes/users/getUserByCriteria.ts
+++ routes/users/getUserByCriteria.ts
@@ -5,31 +5,31 @@ import Router from '../../router';
 import { publicUserSchema, UserWithPublicAttributes } from '../auth/login';
 import { UserRole } from './schema';
 
-const SQL_GET_USER_BY_CRITERIA = sql<Record<string, never>, UserWithPublicAttributes>(`
+const SQL_GET_USER_BY_CRITERIA = sql<Record<string, never>,UserWithPublicAttributes>(`
   SELECT 
-    users.id, 
-    users.id_type, 
+    users.id,
+    users.id_type,
     users.id_number,
-    user_contact_details.full_name, 
-    users.role, 
-    users.gender, 
-    user_contact_details.phone_number,  
+    user_contact_details.full_name,
+    users.role,
+    users.gender,
+    user_contact_details.phone_number,
     users.created_at
-  FROM 
-    users
-  LEFT JOIN 
-    user_contact_details ON users.id = user_contact_details.id
+  FROM users
+  LEFT JOIN user_contact_details 
+    ON users.id = user_contact_details.id
 `);
 
 const getUsersBySearchCriteria = (router: Router) => {
   router.route({
     method: 'get',
     path: '/search',
-    summary: 'Search for users based on various criteria.Admin/mod only',
-    description: 'This endpoint allows searching for users based on a single query criterion: phone number, ID number, or user ID.',
+    summary: 'Search for users based on various criteria.',
+    description: 'Allows searching for users based on a single query criterion: ' +
+                'phone number, ID number, or user ID.',
     request: {
       query: z.object({
-        value: z.string(),
+        value: z.string()
       })
     },
     response: {
@@ -37,30 +37,35 @@ const getUsersBySearchCriteria = (router: Router) => {
         schema: z.array(publicUserSchema)
       }
     },
-    authMiddlewareOptions: {roles: [UserRole.Enum.Admin, UserRole.Enum.Moderator]},
+    authMiddlewareOptions: {
+      roles: [UserRole.Enum.Admin, UserRole.Enum.Moderator]
+    },
     handler: async (req, res) => {
       const { value } = req.query;
+      const sanitizedValue = value.replace(/^ /, '+');
 
       const filters: string[] = [];
       const filterArgs: Record<string, string | number> = {};
 
-      if (/^\d+$/.test(value)) {
-        filters.push('users.id = :user_id');
-        filterArgs.user_id = parseInt(value, 10);
-      } else if (/^\+\d{1,4}\d{9}$/.test(value)) {
+      if (/^\+\d{10,16}$/.test(sanitizedValue)) {
         filters.push('user_contact_details.phone_number = :phone_number');
-        filterArgs.phone_number = value;
-      } else if (/^(?:\d{8}|\d{9}(\d{4})?|\d{10}|\d{13}|\d{16})$/.test(value)) {
+        filterArgs.phone_number = sanitizedValue;
+      } else if (/^(?:[A-Z]{1,2}\d{6,9}|\d{8,10}|\d{13}|\d{16})$/
+        .test(sanitizedValue)) { 
         filters.push('users.id_number = :id_number');
-        filterArgs.id_number = value;
+        filterArgs.id_number = sanitizedValue;
+      }
+       else if (/^\d+$/.test(sanitizedValue)) {
+        filters.push('users.id = :user_id');
+        filterArgs.user_id = parseInt(sanitizedValue, 10);
       } else {
         throw new HttpError(400);
       }
-
+      
       const query = SQL_GET_USER_BY_CRITERIA({});
       query.extend(`WHERE ${filters}`, filterArgs);
+      
       const users = await query.many();
-
       res.json(users);
     }
   });
