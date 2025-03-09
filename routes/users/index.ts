b5ecbd90f318667fd0bf961ce2e3b92737f546f3
Queenslay Jema <queenslayjema@gmail.com>
2025-03-10 02:53:03 +0300

    Refactor: Remove unused getUserById route and its associated logic

--- routes/users/index.ts
+++ routes/users/index.ts
@@ -1,7 +1,6 @@
 import Router from '../../router';
 import updateIdDetails from './updateIdentification';
 import getUsersBySearchCriteria from './getUserByCriteria';
-import getUsersByUserId from './getUserById';
 import updateUserRole from './updateUserRole';
 import updateUserPin from './updateUserPin';
 import updateInvites from './updateInvitation';
@@ -10,7 +9,6 @@ import getInvites from './getUserGroupInvitations';
 const router = Router.getRouterInstance('/users', 'Users');
 
 getUsersBySearchCriteria(router);
-getUsersByUserId(router);
 updateUserPin(router);
 updateInvites(router);
 updateIdDetails(router);
