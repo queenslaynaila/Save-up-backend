3bf1de3c98fd433d3ded541155621b96395e9129
Queenslay Jema <queenslayjema@gmail.com>
2025-03-10 10:09:11 +0300

    Refactor: Improve formatting and readability of update_group_name function in SQL migration

--- migrations/0045_create_fn_update_group_name.mutable.sql
+++ migrations/0045_create_fn_update_group_name.mutable.sql
@@ -1,19 +1,24 @@
 CREATE OR REPLACE FUNCTION update_group_name(
-    p_group_id INT,
-    p_user_id INT,
-    p_new_name TEXT
+    p_group_id   INT,
+    p_user_id    INT,
+    p_new_name   TEXT
 ) 
-RETURNS TABLE (name TEXT) AS $$
+RETURNS TABLE (
+    name        TEXT
+) AS $$
 DECLARE
-    v_old_name TEXT;
+    v_old_name   TEXT;
 BEGIN
-    PERFORM check_grp_membership(p_group_id, p_user_id);
-
-    SELECT groups.name INTO STRICT v_old_name
+    SELECT groups.name 
+    INTO STRICT v_old_name
     FROM groups
     WHERE id = p_group_id;
 
-    INSERT INTO prev_group_names (group_id, xid, name)
+    INSERT INTO prev_group_names (
+        group_id,
+        xid,
+        name
+    )
     SELECT 
         p_group_id,
         COALESCE(MAX(xid), 0) + 1,
@@ -21,17 +26,17 @@ BEGIN
     FROM prev_group_names
     WHERE group_id = p_group_id;
 
+    RETURN QUERY
     UPDATE groups
     SET name = p_new_name
     WHERE id = p_group_id
     AND deleted_at IS NULL
-    RETURNING groups.name INTO STRICT name;
-
-    RETURN NEXT;
+    RETURNING groups.name;
 END;
 $$ LANGUAGE plpgsql;
 
 GRANT EXECUTE ON FUNCTION update_group_name(INT, INT, TEXT) TO saveup_www;
 SELECT create_distributed_function(
-  'update_group_name(INT, INT, TEXT)', 'p_group_id'
+    'update_group_name(INT, INT, TEXT)',
+    'p_group_id'
 );
