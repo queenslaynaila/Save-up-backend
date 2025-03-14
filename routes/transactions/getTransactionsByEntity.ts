fe7b265847450c2ad43edcf44c5d1770bdad7929
Queenslay Jema <queenslayjema@gmail.com>
2025-03-14 09:03:49 +0300

    Refactor: Move transaction schemas to getTransactionsByEntity route for better organization and clarity

--- routes/transactions/getTransactionsByEntity.ts
+++ routes/transactions/getTransactionsByEntity.ts
@@ -2,8 +2,35 @@ import { z } from 'zod';
 import { sql } from '../../db';
 import Router from '../../router';
 import verifyGroupMembership from '../../utils';
-import { transactionSchema, transactionTypeSchema } from './schema';
-import logger from '../../logger';
+
+const ENUM_TRANSACTION_TYPE = z.enum([
+  'Saving',
+  'ExternalSaving',
+  'Interest',
+  'Withdrawal',
+  'Penalty',
+  'TransferIn',
+  'TransferOut',
+  'Loan',
+  'Repayment'
+]);
+
+export const transactionTypeSchema = z.object({
+  id: z.number(),
+  slug: ENUM_TRANSACTION_TYPE,
+  created_at: z.string().datetime()
+});
+
+export const transactionSchema = z.object({
+  entity_id: z.number().min(1),
+  xid: z.number().min(1),
+  type_id: z.number(),
+  pocket_id: z.number().min(1),
+  reference_id: z.number(),
+  delta: z.number().min(5),
+  balance: z.number(),
+  created_at: z.string().datetime()
+});
 
 const transaction = transactionSchema.pick({
   xid: true,
