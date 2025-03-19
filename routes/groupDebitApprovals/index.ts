86a199089ce27a0dd5ef689fecd388a524485cc2
Queenslay Jema <queenslayjema@gmail.com>
2025-03-19 16:16:42 +0300

    Refactor: Remove unused groupDebitApprovals route and its associated logic

--- routes/groupDebitApprovals/index.ts
+++
@@ -1,8 +0,0 @@
-import Router from '../../router';
-import reviewDebitRequests from './approveDebitRequest';;
-
-const router = Router.getRouterInstance('/debit-approvals', 'Debit Approvals');
-
-reviewDebitRequests(router);
-
-export default router;
