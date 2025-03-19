08014d05f16889353e18edc213a652ab66db1583
Queenslay Jema <queenslayjema@gmail.com>
2025-03-19 15:16:35 +0300

    Refactor: Enhance group membership verification and access control logic in utils

--- utils.ts
+++ utils.ts
@@ -9,16 +9,59 @@ import {
   PinResetState, 
   Role
 } from './routes/users/schema';
-import logger from './logger';
-
-const ADMIN_LIKE_ROLES: Role[] = ['Admin', 'Moderator'];
-
 declare module 'express-serve-static-core' {
   interface Request {
     user?: AuthenticatedUser;
     resetState?: PinResetState;
   }
 }
+
+const ADMIN_LIKE_ROLES: Role[] = ['Admin', 'Moderator'];
+
+const SQL_GET_ENTITY_TYPE = sql<{
+  entity_id: number;
+}, {
+  entity_type: 'Group' | 'User'
+}>(`
+  SELECT entity_type 
+  FROM entities 
+  WHERE id = :entity_id
+`);
+
+const SQL_GET_PIN = sql<
+  { user_id: number }, 
+  { pin: string }
+>(`
+  SELECT pin 
+  FROM users 
+  WHERE id = :user_id
+`);
+
+const SQL_GET_GROUP_MEMBERSHIP_STATUS = sql<{
+  entity_id: number;
+  user_id: number;
+}, {
+  is_admin_member: boolean;
+  is_member: boolean;
+}>(`
+  SELECT 
+    EXISTS (
+      SELECT 1 FROM group_members 
+      WHERE user_id = :user_id 
+        AND group_id = :entity_id
+        AND is_active = TRUE
+    ) AS is_member,
+    EXISTS (
+      SELECT 1 FROM group_admins
+      WHERE group_id = :entity_id
+        AND user_id = :user_id
+        AND election_id = (
+          SELECT MAX(xid) FROM elections 
+          WHERE group_id = :entity_id 
+            AND status = 'Closed'
+        )
+    ) AS is_admin_member
+`);
 
 export function generateToken(
   id: number,
@@ -61,21 +104,35 @@ function validateAndDecodeJwt(headerValue: string): JwtPayload {
   return decoded;
 }
 
-export function decodeEntityOrUserId(
-  req: Request<{ entity_id?: number | "me"; user_id?: number | "me" }>,
-  isOwnerOrAdminMod: boolean = false
-): number {
-  const entityId = req.params.user_id ?? req.params.entity_id;
+export function checkResetTokenValidity(requiredStep: number) {
+  return function(req: Request, _res: Response, next: NextFunction) {
+    const resetToken = req.headers.reset as string;
+    const decoded = validateAndDecodeJwt(resetToken);
 
-  if (entityId === "me" || Number(entityId) === req.user!.id) {
-    return req.user!.id; 
-  }
-  
-  if (isOwnerOrAdminMod && ADMIN_LIKE_ROLES.includes(req.user!.role)) {
-    return Number(entityId);
+    if (!decoded.id || !decoded.step) {
+      throw new HttpError(401);
+    }
+
+    req.resetState = { userId: decoded.id, step: decoded.step };
+
+    if (req.resetState.step !== requiredStep) {
+      throw new HttpError(403);
+    }
+
+    next();
+  };
+}
+
+export async function verifyPin(req: Request, _res: Response, next: NextFunction) {
+  const { pin: hashedPin } = await SQL_GET_PIN({
+    user_id: req.user!.id
+  }).one();
+
+  if (!await bcrypt.compare(req.body.pin, hashedPin)) {
+    throw new HttpError(401);
   }
 
-  throw new HttpError(403);
+  next();
 }
 
 export function authMiddleware(auth: true | Role | Role[] = true) {
@@ -102,115 +159,90 @@ export function authMiddleware(auth: true | Role | Role[] = true) {
   };
 }
 
-
-export function checkResetTokenValidity(requiredStep: number) {
-  return function(req: Request, _res: Response, next: NextFunction) {
-    const resetToken = req.headers.reset as string;
-    const decoded = validateAndDecodeJwt(resetToken);
-
-    if (!decoded.id || !decoded.step) {
-      throw new HttpError(401);
-    }
-
-    req.resetState = { userId: decoded.id, step: decoded.step };
-
-    if (req.resetState.step !== requiredStep) {
-      throw new HttpError(403);
-    }
-
-    next();
-  };
+function replaceMeWithUserId(id: number | "me" | undefined, userId: number): number {
+  return id === "me" ? userId : (id ?? userId);
 }
 
-const SQL_GET_PIN = sql<{user_id: number}, {pin: string}>(`
-  SELECT pin 
-  FROM users 
-  WHERE id = :user_id
-`);
+function hasAdminPermissions(userRole:Role, isOwnerOrAdminMod: boolean): boolean {
+  return isOwnerOrAdminMod && ADMIN_LIKE_ROLES.includes(userRole);
+}
 
-export async function verifyPin(req: Request, _res: Response, next: NextFunction) {
-  const { pin: hashedPin } = await SQL_GET_PIN({
-    user_id: req.user!.id
-  }).one();
 
-  if (!await bcrypt.compare(req.body.pin, hashedPin)) {
+function checkGroupMembership(is_member: boolean, is_admin_member: boolean, requiresGrpAdmin: boolean) {
+  if (!is_member) throw new HttpError(403);
+  if (requiresGrpAdmin && !is_admin_member) throw new HttpError(403);
+}
+
+type ExactlyOne<T, Keys extends keyof T = keyof T> = {
+  [K in Keys]: Required<Pick<T, K>> & Partial<Record<Exclude<Keys, K>, never>>;
+}[Keys];
+
+type RequestParams =
+  | ExactlyOne<{ entity_id: number | "me"; user_id?: never; group_id?: never; member_id?: never }>
+  | ExactlyOne<{ user_id: number | "me"; entity_id?: never; group_id?: never; member_id?: never }>
+  | ({ group_id: number } & { entity_id?: never; user_id?: never } & { member_id?: number | "me" });
+
+
+export async function decodeEntityAndVerifyAccess(
+  req: Request<RequestParams, any, any, any, any>,
+  isOwnerOrAdminMod: boolean = false,
+  requiresGrpAdmin: boolean = false
+): Promise<number> {
+  if (!req.user) {
     throw new HttpError(401);
   }
 
-  next();
-}
+  const userId = req.user.id
+  const role = req.user.role
+  
+  const params = req.params!; 
 
-const SQL_GET_GROUP_MEMBERSHIP_STATUS = sql<{
-  entity_id: number;
-  user_id: number;
-}, {
-  is_admin_member: boolean;
-  is_member: boolean;
-}>(`
-  SELECT 
-    EXISTS (
-      SELECT 1 FROM group_members 
-      WHERE user_id = :user_id 
-        AND group_id = :entity_id
-        AND is_active = TRUE
-    ) AS is_member,
-    EXISTS (
-      SELECT 1 FROM group_admins
-      WHERE group_id = :entity_id
-        AND user_id = :user_id
-        AND election_id = (
-          SELECT MAX(xid) FROM elections 
-          WHERE group_id = :entity_id 
-            AND status = 'Closed'
-        )
-    ) AS is_admin_member
-`);
+  params.user_id = replaceMeWithUserId(params?.user_id, userId);
+  params.member_id = replaceMeWithUserId(params?.member_id, userId);
+  params.entity_id = replaceMeWithUserId(params?.entity_id, userId);
 
-interface GroupMembershipOptions {
-  isOwnerOrAdminMod?: boolean;
-  requiresGrpAdmin?: boolean;
-}
+  if (params.user_id === userId || 
+      params.entity_id === userId
+    ){ return userId; }
 
-export default function verifyGroupMembership({
-  isOwnerOrAdminMod = false,
-  requiresGrpAdmin = false
-}: GroupMembershipOptions = {}) {
-  return async (req: Request, _res: Response, next: NextFunction) => {
-    const entityId = Number(req.params.entity_id ?? req.params.group_id)
-    const userId = req.user!.id
+  if (params.user_id) {
+    if (!hasAdminPermissions(role, isOwnerOrAdminMod)) throw new HttpError(403);
+    return params.user_id;
+  }
 
-    if (entityId === userId) {
-      return next()
-    }
-
-    if (isOwnerOrAdminMod && ADMIN_LIKE_ROLES.includes(req.user!.role)) {
-      return next();
-    }
+  if (params.group_id) {
+    if (hasAdminPermissions(role, isOwnerOrAdminMod)) return userId;
     
     const { is_admin_member, is_member } = await SQL_GET_GROUP_MEMBERSHIP_STATUS({
-      entity_id: entityId,
-      user_id: userId 
-    }).one()
+      entity_id: params.group_id,
+      user_id: userId
+    }).one();
 
-    if (!is_member) {
-      throw new HttpError(403)
-    }
-
-    if (req.params.member_id === 'me') {
-      req.params.member_id = req.user!.id.toString()
-    }
-
-    const isRequestingOtherMember = req.params.member_id && 
-                                  userId !== parseInt(req.params.member_id)
-
-    if (isRequestingOtherMember && !is_admin_member) {
-      throw new HttpError(403)
-    }
-
-    if (requiresGrpAdmin && !is_admin_member) {
-      throw new HttpError(403)
-    }
-
-    next()
+    checkGroupMembership(is_member, is_admin_member, requiresGrpAdmin);
+    return params.group_id;
   }
+
+  const entityType = await SQL_GET_ENTITY_TYPE({
+     entity_id: params.entity_id
+  }).oneFirst();
+
+  if (entityType === "User" && !hasAdminPermissions(role, isOwnerOrAdminMod)) {
+    throw new HttpError(403);
+  }
+
+  const { is_admin_member, is_member } = await SQL_GET_GROUP_MEMBERSHIP_STATUS({
+    entity_id:params.entity_id,
+    user_id: userId
+  }).one();
+
+  checkGroupMembership(is_member, is_admin_member, requiresGrpAdmin);
+
+  const isRequestingOtherMember = params.member_id !== undefined && 
+                                  userId !== params.member_id;
+
+  if (isRequestingOtherMember && !is_admin_member) {
+    throw new HttpError(403)
+  }
+ 
+  return params.entity_id;
 }
