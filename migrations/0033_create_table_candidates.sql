41aeefe6e3e87cb62726edb93c9a1576f876faa9
Queenslay Jema <queenslayjema@gmail.com>
2025-04-07 08:48:27 +0300

    Chore: Clear migrations

--- migrations/0033_create_table_candidates.sql
+++ migrations/0033_create_table_candidates.sql
@@ -2,12 +2,12 @@ CREATE TABLE IF NOT EXISTS candidates (
   group_id       INT NOT NULL,
   election_id    INT NOT NULL,
   candidate_id   INT NOT NULL,
-  chosen_by      INT NOT NULL, -- User ID of the person who chose the candidate
+  chosen_by      INT NOT NULL,
   created_at     TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
   PRIMARY KEY    (group_id, election_id, candidate_id),
   FOREIGN KEY    (group_id, candidate_id) REFERENCES group_members(group_id,user_id),
   FOREIGN KEY    (group_id, chosen_by) REFERENCES group_members(group_id,user_id),
-  FOREIGN KEY    (group_id, election_id) REFERENCES elections(group_id,xid) 
+  FOREIGN KEY    (group_id, election_id) REFERENCES elections(group_id,xid)
     ON DELETE RESTRICT ON UPDATE RESTRICT
 );
 SELECT create_distributed_table('candidates', 'group_id');
