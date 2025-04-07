fa3d1a0cb52c755ad5804f1f71f8d4b54d51b941
Queenslay Jema <queenslayjema@gmail.com>
2025-04-07 12:33:21 +0300

    Refactor: simplify function

--- migrations/0055_create_fn_ratify_election.mutable.sql
+++ migrations/0055_create_fn_ratify_election.mutable.sql
@@ -5,8 +5,7 @@ CREATE OR REPLACE FUNCTION ratify_election(
     p_is_ratified    BOOLEAN
 ) RETURNS VOID AS $$
 DECLARE
-    v_total_ratifers      INT;
-    v_total_ratifications INT;
+    v_total_members     INT;
     v_total_approvals     INT;
 BEGIN
     IF NOT EXISTS (
@@ -18,7 +17,7 @@ BEGIN
           AND closed_at IS NULL
     ) THEN
         RAISE EXCEPTION USING
-            MESSAGE = 'ERR_ELECTION_CLOSED',
+            MESSAGE = 'ERR_ELECTION_CLOSED_OR_CANCELLED',
             ERRCODE = 'P0007';
     END IF;
 
@@ -27,36 +26,28 @@ BEGIN
         election_id,
         user_id,
         is_ratified
-    ) 
+    )
     VALUES (
         p_group_id,
         p_election_id,
         p_user_id,
         p_is_ratified
-    ) 
-    ON CONFLICT (group_id, election_id, user_id) DO NOTHING;
+    );
 
-    SELECT COUNT(*) 
-    INTO v_total_ratifers
+    SELECT COUNT(*)
+    INTO v_total_members
     FROM group_members
     WHERE group_id = p_group_id
       AND is_active = TRUE;
 
-    SELECT COUNT(DISTINCT user_id) 
-    INTO v_total_ratifications
-    FROM ratifications
-    WHERE group_id = p_group_id
-      AND election_id = p_election_id;
-
-    SELECT COUNT(*) 
+    SELECT COUNT(*)
     INTO v_total_approvals
     FROM ratifications
     WHERE group_id = p_group_id
       AND election_id = p_election_id
       AND is_ratified = TRUE;
 
-
-    IF v_total_approvals >= (v_total_ratifers / 2) THEN
+    IF v_total_approvals >= (v_total_members / 2) THEN
         UPDATE elections
         SET status = 'Closed',
             closed_at = NOW()
@@ -68,13 +59,13 @@ BEGIN
             election_id,
             user_id
         )
-        SELECT 
-            c.group_id,
-            c.election_id,
-            c.candidate_id
-        FROM candidates c
-        WHERE c.group_id = p_group_id
-          AND c.election_id = p_election_id
+        SELECT
+            candidates.group_id,
+            candidates.election_id,
+            candidates.candidate_id
+        FROM candidates
+        WHERE candidates.group_id = p_group_id
+          AND candidates.election_id = p_election_id
         ON CONFLICT (group_id, election_id, user_id) DO NOTHING;
     END IF;
 END;
