e3e14a96bf696020e128036e546184f7874efcdf
Queenslay Jema <queenslayjema@gmail.com>
2025-04-07 10:43:02 +0300

    Refactor: Add loan to activities

--- routes/groups/getGroupActivities.ts
+++ routes/groups/getGroupActivities.ts
@@ -2,6 +2,7 @@ import { z } from 'zod';
 import Router from '../../router';
 import { sql } from '../../db';
 import { decodeEntityAndVerifyAccess } from '../../utils';
+import logger from "../../logger";
 
 const emptyMetadata = z.object({}).nullable();
 
@@ -14,7 +15,7 @@ const transferMetadata = z.object({
     id: z.number(),
     name: z.string()
   })
-});
+}).nullable();
 
 const withdrawalRequestMetadata = z.object({
   reason: z.string(),
@@ -23,12 +24,17 @@ const withdrawalRequestMetadata = z.object({
     name: z.string(),
     amount: z.number()
   }))
-});
+}).nullable();
 
 const withdrawalStatusMetadata = z.object({
   reason: z.string(),
   status: z.enum(['Approved', 'Rejected'])
-});
+}).nullable();
+
+const loanMetadata = z.object({
+  reason: z.string(),
+  status: z.enum(['Approved', 'Rejected', 'Pending Guarantors'])
+}).nullable();
 
 export const activitySchema = z.object({
   id: z.number(),
@@ -41,27 +47,29 @@ export const activitySchema = z.object({
   pocket_name: z.string().nullable(),
   amount: z.number().nullable(),
   type: z.enum([
-    'MemberJoined', 
-    'MemberLeft', 
-    'MemberRemoved', 
-    'Deposit', 
-    'Transfer', 
-    'WithdrawalRequest', 
-    'WithdrawalApproval', 
-    'WithdrawalRejection'
+    'MemberJoined',
+    'MemberLeft',
+    'MemberRemoved',
+    'Deposit',
+    'Transfer',
+    'WithdrawalRequest',
+    'WithdrawalApproval',
+    'WithdrawalRejection',
+    'LoanRequest'
   ]),
   metadata: z.union([
     emptyMetadata,
     transferMetadata,
     withdrawalRequestMetadata,
-    withdrawalStatusMetadata
+    withdrawalStatusMetadata,
+    loanMetadata
   ])
 });
 
 type Activity = z.infer<typeof activitySchema>;
 
 const SQL_GET_GROUP_ACTIVITIES = sql<
-{ group_id: number, size: number }, 
+{ group_id: number, size: number },
 Activity>(`
     SELECT *
     FROM (
@@ -226,25 +234,26 @@ Activity>(`
                     'reason', debit_requests.reason,
                     'recipients', COALESCE(
                         jsonb_agg(jsonb_build_object(
-                            'id', debit_recipients.recipient_id,
+                            'id', withdrawal_recipients.recipient_id,
                             'name', recipient_details.full_name,
-                            'amount', debit_recipients.amount
+                            'amount', withdrawal_recipients.amount
                         )), '[]'::JSONB
                     )
                 )::JSONB AS metadata,
                 debit_requests.created_at
             FROM debit_requests
-            LEFT JOIN debit_recipients 
-                ON debit_recipients.group_id = debit_requests.group_id
-                AND debit_recipients.request_id = debit_requests.xid
+            LEFT JOIN withdrawal_recipients 
+                ON withdrawal_recipients.group_id = debit_requests.group_id
+                AND withdrawal_recipients.request_id = debit_requests.xid
             LEFT JOIN user_contact_details AS recipient_details 
-                ON recipient_details.id = debit_recipients.recipient_id
+                ON recipient_details.id = withdrawal_recipients.recipient_id
             JOIN user_contact_details 
                 ON user_contact_details.id = debit_requests.initiator_id
             LEFT JOIN pockets 
                 ON pockets.xid = debit_requests.pocket_id
                 AND pockets.entity_id = debit_requests.group_id
             WHERE debit_requests.group_id = :group_id
+            AND debit_type = 'Withdrawal'
             GROUP BY 
               debit_requests.xid, 
               debit_requests.initiator_id, 
@@ -256,6 +265,36 @@ Activity>(`
               debit_requests.created_at
             ORDER BY debit_requests.created_at DESC
             LIMIT :size
+        )
+        
+        UNION ALL
+
+        (
+          SELECT
+            'LoanRequest' AS type,
+            debit_requests.xid AS id,
+            debit_requests.initiator_id AS actor_id,
+            user_contact_details.full_name AS actor_name,
+            NULL::INT AS target_id,
+            NULL::TEXT AS target_name,
+            debit_requests.pocket_id,
+            pockets.name AS pocket_name,
+            debit_requests.amount,
+            jsonb_build_object(
+                'reason', debit_requests.reason,
+                'status', debit_requests.status
+            )::JSONB AS metadata,
+            debit_requests.created_at
+          FROM debit_requests
+          JOIN user_contact_details
+             ON user_contact_details.id = debit_requests.initiator_id
+          LEFT JOIN pockets
+            ON pockets.xid = debit_requests.pocket_id
+            AND pockets.entity_id = debit_requests.group_id
+          WHERE debit_requests.group_id = :group_id
+            AND debit_requests.debit_type = 'Loan'
+          ORDER BY debit_requests.created_at DESC
+          LIMIT :size 
         )
 
         UNION ALL
@@ -340,7 +379,7 @@ const getGroupActivities = (router: Router) => {
       }),
       query: z.object({
         size: z.number()
-      })
+      }).partial()
     },
     response: {
         schema: z.array(activitySchema)
@@ -348,12 +387,14 @@ const getGroupActivities = (router: Router) => {
     handler: async (req, res) => {
       const groupId = await decodeEntityAndVerifyAccess(req, true);
       const size = req.query.size || 200;
-      
+
       const activities = await SQL_GET_GROUP_ACTIVITIES({
         group_id: groupId,
         size
       }).many();
-      
+
+      logger.info(`${JSON.stringify(activities)}`);
+
       res.json(activities);
     }
   });
