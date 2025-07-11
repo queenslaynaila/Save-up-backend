7fb5c0243a7ac0ee44542d1456a892c24abddf80
Queenslay Jema <queenslayjema@gmail.com>
2025-07-11 12:41:26 +0300

    Fix: Use db defined rates

--- jobs/interestProcessor.ts
+++ jobs/interestProcessor.ts
@@ -3,7 +3,6 @@ import { ENUM_POCKET_TYPE } from '../routes/pockets/schema';
 import { Job } from 'bullmq';
 import { sql } from '../db';
 import { interestCalculationQueue } from './bullConfig';
-import logger from '../logger';
 
 const pocketDataSchema = z.object({
   entity_id: z.number(),
@@ -54,17 +53,14 @@ const SQL_AWARD_INTEREST_TO_POCKET = sql<{
     SELECT process_transaction(:entity_id, :transaction_type, :pocket_id, :amount)
 `);
 
-const STANDARD_SAVINGS_RATE = 0.06;
-const LOCKED_SAVINGS_RATE = 0.08;
+const SQL_GET_ALL_INTEREST_RATES = sql<Record<string, never>, {pocket_type: 'Standard' | 'Locked', rate: number}>(`
+  SELECT pocket_type, rate FROM interest_rates
+`);
 
-export async function awardInterest(job: Job<Processor>) {
-  const { entity_id, pocket_id, pocket_type, end_of_day_balance } = job.data;
+export async function awardInterest(job: Job<Processor & {rate:number}>) {
+  const { entity_id, pocket_id, rate, end_of_day_balance } = job.data;
 
-  const annualInterestRate = pocket_type === 'Locked'
-    ? LOCKED_SAVINGS_RATE
-    : STANDARD_SAVINGS_RATE;
-
-  const dailyInterestAmount = (end_of_day_balance * annualInterestRate) / 365;
+  const dailyInterestAmount = (end_of_day_balance * rate) / 365;
   const roundedInterestAmount = Number(dailyInterestAmount.toFixed(2));
 
   if (roundedInterestAmount > 0) {
@@ -78,17 +74,20 @@ export async function awardInterest(job: Job<Processor>) {
 }
 
 export async function findEligiblePocketsAndCreateJobs() {
+  const interestRates = await SQL_GET_ALL_INTEREST_RATES({}).many();
   const eligiblePockets = await SQL_FIND_POCKETS_ELIGIBLE_FOR_INTEREST({}).many();
 
-  logger.info(`found ${eligiblePockets.length} pockets eligible for interest`);
+  const rateMap = Object.fromEntries(
+    interestRates.map(r => [r.pocket_type, Number(r.rate)])
+  );
 
   const jobCreationPromises = eligiblePockets.map(pocket => interestCalculationQueue.add(
     'calculate-pocket-interest',
     {
       entity_id: pocket.entity_id,
       pocket_id: pocket.pocket_id,
-      pocket_type: pocket.pocket_type,
-      end_of_day_balance: pocket.end_of_day_balance
+      end_of_day_balance: pocket.end_of_day_balance,
+      interest_rate: rateMap[pocket.pocket_type]
     }
   ));
 
