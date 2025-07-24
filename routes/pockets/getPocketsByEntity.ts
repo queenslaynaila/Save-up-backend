7db2751d940805f6880a71037cbc2f2dbbcb7b61
Queenslay Jema <queenslayjema@gmail.com>
2025-07-24 15:21:56 +0300

    Fix: Add donation pockets

--- routes/pockets/getPocketsByEntity.ts
+++ routes/pockets/getPocketsByEntity.ts
@@ -57,11 +57,7 @@ const SQL_GET_POCKETS = sql<PocketFilters, PocketReturn>(`
     pockets.target_at,
     pockets.created_at
   FROM pockets
-  LEFT JOIN donation_pockets 
-    ON pockets.entity_id = donation_pockets.entity_id 
-    AND pockets.xid = donation_pockets.pocket_id
-  WHERE donation_pockets.pocket_id IS NULL
-    AND pockets.deleted_at IS NULL
+  WHERE pockets.deleted_at IS NULL
     AND pockets.entity_id = :entity_id
     AND (:xid::INT IS NULL OR pockets.xid = :xid)
     AND (:category_id::INT IS NULL OR pockets.category_id = :category_id)
