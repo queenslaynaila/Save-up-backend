c7e6045030e014c787fb2c354ad2f3936fb93225
Queenslay Jema <queenslayjema@gmail.com>
2025-08-04 13:30:28 +0300

    Refactor: Remove duplicate createUser call in auth routes

--- routes/auth/index.ts
+++ routes/auth/index.ts
@@ -12,7 +12,6 @@ const router = Router.createResourceRouter('Auth');
 
 createUser(router);
 login(router);
-createUser(router);
 getRefreshToken(router);
 logout(router);
 initiatePinReset(router);
