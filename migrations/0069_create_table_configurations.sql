b7704336eb8dd19662af7aa4e9fa87b544550acb
Queenslay Jema <queenslayjema@gmail.com>
2025-08-20 11:06:57 +0300

    Refactor: Update country_configurations table schema for consistency and clarity

--- migrations/0069_create_table_configurations.sql
+++ migrations/0069_create_table_configurations.sql
@@ -1,16 +1,17 @@
 CREATE TABLE country_configurations (
-  id SERIAL PRIMARY KEY,
+  id  SERIAL PRIMARY KEY,
   country_code TEXT UNIQUE NOT NULL,     
-  country_name TEXT NOT NULL,                 
+  country_name TEXT UNIQUE NOT NULL,                 
   currency  TEXT NOT NULL,               
-  calling_code INTEGER NOT NULL,                
+  calling_code TEXT NOT NULL,                
   languages TEXT[] NOT NULL,                     
   min_deposit NUMERIC NOT NULL,
   max_deposit NUMERIC NOT NULL,
   min_withdrawal NUMERIC NOT NULL,
   max_withdrawal NUMERIC NOT NULL,
   withdrawal_charges TEXT NOT NULL,
-  created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+  created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+  deleted_at TIMESTAMP WITH TIME ZONE
 );
 
 GRANT INSERT, SELECT, UPDATE ON country_configurations TO saveup_www;
